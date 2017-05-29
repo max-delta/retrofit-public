b828186582ef189a3c17f539b0542442ebd10f13
Max Delta <Max Delta@localhost>
2017-05-29 11:45:27 -0700

    Addead minclude weak destroy, minor cleanup

---
+++ libs/core_test/minclude_weak_destroy.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+
+// Do not need type to be defined to destroy it
+#include "core/ptr/weak_ptr.h"
+namespace {
+	class ForwardedClass;
+	void test(RF::WeakPtr<ForwardedClass> && wptr)
+	{
+		wptr = nullptr;
+	}
+}
