8054501c0996f7de904f86cd18a369bf30b632e3
Max Delta <54338561+max-delta@users.noreply.github.com>
2017-06-11 19:52:08 -0700

    Fixes for other build configurations

--- libs/core_platform/win_shim.h
+++ libs/core_platform/win_shim.h
@@ -17,10 +17,17 @@ typedef unsigned int        UINT;
 typedef unsigned int        *PUINT;
 typedef wchar_t WCHAR;
 typedef const WCHAR *LPCWSTR;
+#if defined(_WIN64)
+typedef __int64 INT_PTR, *PINT_PTR;
+typedef unsigned __int64 UINT_PTR, *PUINT_PTR;
+typedef __int64 LONG_PTR, *PLONG_PTR;
+typedef unsigned __int64 ULONG_PTR, *PULONG_PTR;
+#else
 typedef int INT_PTR, *PINT_PTR;
 typedef unsigned int UINT_PTR, *PUINT_PTR;
 typedef long LONG_PTR, *PLONG_PTR;
 typedef unsigned long ULONG_PTR, *PULONG_PTR;
+#endif
 typedef UINT_PTR            WPARAM;
 typedef LONG_PTR            LPARAM;
 typedef LONG_PTR            LRESULT;
