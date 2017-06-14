ee354093cd6abf06298371e8a7b79d1e826de192
Max Delta <54338561+max-delta@users.noreply.github.com>
2017-06-13 20:13:13 -0700

    Test rendering

--- apps/retrofit/source/main.cpp
+++ apps/retrofit/source/main.cpp
@@ -3,8 +3,6 @@
 #include "PlatformUtils_win32/windowing.h"
 #include "SimpleGL/SimpleGL.h"
 
-#include "core_platform/windows_inc.h"
-
 // Window Procedure in different file
 shim::LRESULT WIN32_CALLBACK WndProc( shim::HWND hWnd, shim::UINT message, shim::WPARAM wParam, shim::LPARAM lParam );
 
@@ -15,16 +13,18 @@ int main()
 	shim::HWND hwnd = RF::platform::windowing::CreateNewWindow(640,480,WndProc);
 	RF::gfx::SimpleGL graphics;
 	graphics.AttachToWindow( hwnd );
-
+	unsigned int texture = graphics.LoadTexture( "../../data/textures/common/max_delta_32.png" );
 	graphics.SetProjectionMode( RF::gfx::SimpleGL::ProjectionMode::NDC11_11UPRIGHT );
 	graphics.SetBackgroundColor();
 	graphics.SetSurfaceSize();
 	while( RF::platform::windowing::ProcessMessages() )
 	{
 		graphics.BeginFrame();
+		graphics.DrawBillboard( texture, RF::gfx::Vector2f( -.5f, .5f ), RF::gfx::Vector2f( .5f, -.5f ), 0.f );
 		graphics.RenderFrame();
 		graphics.EndFrame();
 	}
+	graphics.UnloadTexture( texture );
 	graphics.DetachFromWindow();
 	return 0;
 }
