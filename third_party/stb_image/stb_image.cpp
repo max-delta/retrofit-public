9c3f5e7545c19988686341d7fe10687c3562a5a7
Max Delta <Max Delta@localhost>
2017-06-13 20:06:42 -0700

    Added stb_image

---
+++ third_party/stb_image/stb_image.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+
+//#define STBI_NO_STDIO
+//#define STBI_NO_SIMD
+#define STBI_NO_HDR
+#define STBI_NO_LINEAR
+
+//#define STBI_NO_JPEG
+//#define STBI_NO_PNG
+//#define STBI_NO_BMP
+#define STBI_NO_PSD
+//#define STBI_NO_TGA
+//#define STBI_NO_GIF
+#define STBI_NO_HDR
+#define STBI_NO_PIC
+#define STBI_NO_PNM
+
+#define STBI_FAILURE_USERMSG
+//#define STBI_NO_FAILURE_STRINGS
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
