6a69228e7f78e9bbffff4c1e66931d3377e86d76
Max Delta <54338561+max-delta@users.noreply.github.com>
2017-07-30 17:19:59 -0700

    Minor cleanup

--- libs/core_test/main.cpp
+++ libs/core_test/main.cpp
@@ -1,16 +1,6 @@
 #include "stdafx.h"
 #include "core/macros.h"
 
-// HACK
-// TODO: Move to seperate file and cleanup
-#include "core/ptr/ptr_ref.h"
-#include "core/ptr/default_creator.h"
-#include "core/ptr/unique_ptr.h"
-#include "core/ptr/shared_ptr.h"
-#include "core/ptr/weak_ptr.h"
-#include "core/ptr/weak_shared_ptr.h"
-#include <memory>
-
 
 namespace RF {
 ///////////////////////////////////////////////////////////////////////////////
