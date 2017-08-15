24a2020fa734d77d11f5c190edf6561f39b82211
Max Delta <54338561+max-delta@users.noreply.github.com>
2017-08-14 21:18:03 -0700

    Added arithmetic helpers

---
+++ libs/core_test/math_casts.cpp
@@ -0,0 +1,113 @@
+#include "stdafx.h"
+
+#include "core_math/math_casts.h"
+
+
+namespace RF { namespace math {
+///////////////////////////////////////////////////////////////////////////////
+
+TEST(MathCasts, IntegerTruncast_Unsigned)
+{
+	uint64_t huge = std::numeric_limits<uint64_t>::max();
+	uint64_t u64 = integer_truncast<uint64_t>( huge );
+	uint64_t u32 = integer_truncast<uint32_t>( huge );
+	uint64_t u16 = integer_truncast<uint16_t>( huge );
+	uint64_t u8 = integer_truncast<uint8_t>( huge );
+	ASSERT_EQ( u64, std::numeric_limits<uint64_t>::max() );
+	ASSERT_EQ( u32, std::numeric_limits<uint32_t>::max() );
+	ASSERT_EQ( u16, std::numeric_limits<uint16_t>::max() );
+	ASSERT_EQ( u8, std::numeric_limits<uint8_t>::max() );
+}
+
+
+
+TEST(MathCasts, IntegerTruncast_Signed)
+{
+	static_assert( std::numeric_limits<int64_t>::lowest() < 0, "" );
+
+	int64_t huge = std::numeric_limits<int64_t>::lowest();
+	int64_t s64 = integer_truncast<int64_t>( huge );
+	int64_t s32 = integer_truncast<int32_t>( huge );
+	int64_t s16 = integer_truncast<int16_t>( huge );
+	int64_t s8 = integer_truncast<int8_t>( huge );
+	ASSERT_EQ( s64, std::numeric_limits<int64_t>::lowest() );
+	ASSERT_EQ( s32, std::numeric_limits<int32_t>::lowest() );
+	ASSERT_EQ( s16, std::numeric_limits<int16_t>::lowest() );
+	ASSERT_EQ( s8, std::numeric_limits<int8_t>::lowest() );
+}
+
+
+
+TEST(MathCasts, IntegerTruncast_CrossSigned)
+{
+	static_assert( std::numeric_limits<int64_t>::lowest() < 0, "" );
+	static_assert( std::numeric_limits<uint64_t>::lowest() == 0, "" );
+
+	int64_t huge = std::numeric_limits<int64_t>::lowest();
+	uint64_t u64 = integer_truncast<uint64_t>( huge );
+	uint64_t u32 = integer_truncast<uint32_t>( huge );
+	uint64_t u16 = integer_truncast<uint16_t>( huge );
+	uint64_t u8 = integer_truncast<uint8_t>( huge );
+	ASSERT_EQ( u64, std::numeric_limits<uint64_t>::lowest() );
+	ASSERT_EQ( u32, std::numeric_limits<uint32_t>::lowest() );
+	ASSERT_EQ( u16, std::numeric_limits<uint16_t>::lowest() );
+	ASSERT_EQ( u8, std::numeric_limits<uint8_t>::lowest() );
+}
+
+
+
+TEST(MathCasts, IntegerTruncast_Float)
+{
+	{
+		constexpr float source = -1.f;
+		uint8_t val = integer_truncast<uint8_t>(source);
+		ASSERT_EQ( val, 0 );
+	}
+	{
+		constexpr float source = 0.f;
+		uint8_t val = integer_truncast<uint8_t>(source);
+		ASSERT_EQ( val, 0 );
+	}
+	{
+		constexpr float source = 0.1f;
+		uint8_t val = integer_truncast<uint8_t>(source);
+		ASSERT_EQ( val, 0 );
+	}
+	{
+		constexpr float source = 0.9f;
+		uint8_t val = integer_truncast<uint8_t>(source);
+		ASSERT_EQ( val, 1 );
+	}
+	{
+		constexpr float source = 1.1f;
+		uint8_t val = integer_truncast<uint8_t>(source);
+		ASSERT_EQ( val, 1 );
+	}
+	{
+		constexpr float source = std::numeric_limits<float>::max();
+		uint8_t val = integer_truncast<uint8_t>(source);
+		ASSERT_EQ( val, std::numeric_limits<uint8_t>::max() );
+	}
+	{
+		constexpr float source = std::numeric_limits<float>::infinity();
+		uint8_t val = integer_truncast<uint8_t>(source);
+		ASSERT_EQ( val, std::numeric_limits<uint8_t>::max() );
+	}
+	{
+		// Hmm...
+		static_assert( std::numeric_limits<uint8_t>::quiet_NaN() == 0, "" );
+		constexpr float source = std::numeric_limits<float>::quiet_NaN();
+		uint8_t val = integer_truncast<uint8_t>(source);
+		ASSERT_EQ( val, std::numeric_limits<uint8_t>::quiet_NaN() );
+	}
+	{
+		// Uh... sure, I guess?
+		static_assert( std::numeric_limits<uint8_t>::signaling_NaN() == 0, "" );
+		constexpr float source = std::numeric_limits<float>::signaling_NaN();
+		uint8_t val = integer_truncast<uint8_t>(source);
+		ASSERT_EQ( val, std::numeric_limits<uint8_t>::signaling_NaN() );
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+}}
