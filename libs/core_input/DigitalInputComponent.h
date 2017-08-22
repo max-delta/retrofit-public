f71b9e842218f3759a662dfc0c2bd47dee68dfab
Max Delta <Max Delta@localhost>
2017-08-21 21:17:28 -0700

    More sketching, getting close...

--- libs/core_input/DigitalInputComponent.h
+++ libs/core_input/DigitalInputComponent.h
@@ -71,6 +71,8 @@ public:
 	virtual PhysicalCode GetMaxPhysicalCode() const = 0;
 	virtual LogicalCode GetMaxLogicalCode() const = 0;
 
+	virtual std::u16string GetLogicalName( LogicalCode code ) const = 0;
+
 	virtual PinState GetCurrentPhysicalState( PhysicalCode code ) const = 0;
 	virtual PinState GetPreviousPhysicalState( PhysicalCode code ) const = 0;
 	virtual PinState GetCurrentLogicalState( LogicalCode code ) const = 0;
@@ -128,6 +130,8 @@ public:
 
 	virtual SignalIndex GetMaxSignalIndex() const = 0;
 
+	virtual std::u16string GetSignalName( SignalIndex signalIndex ) const = 0;
+
 	virtual SignalValue GetCurrentSignalValue( SignalIndex signalIndex ) const = 0;
 	virtual SignalValue GetPreviousSignalValue( SignalIndex signalIndex ) const = 0;
 };
@@ -137,9 +141,55 @@ public:
 class InputDevice
 {
 public:
-	DigitalInputComponent* m_DigitalComponent;
-	AnalogInputComponent* m_AnalogComponent;
-	TextInputComponent* m_TextComponent;
+	void OnTick();
+	virtual ~InputDevice() = default;
+
+	UniquePtr<DigitalInputComponent> const m_DigitalComponent;
+	UniquePtr<AnalogInputComponent> const m_AnalogComponent;
+	UniquePtr<TextInputComponent> const m_TextComponent;
+
+protected:
+	InputDevice() = delete;
+	InputDevice(
+		UniquePtr<DigitalInputComponent> && digital,
+		UniquePtr<AnalogInputComponent> && analog,
+		UniquePtr<TextInputComponent> && text );
+};
+
+
+
+class NullInputDevice : public InputDevice
+{
+public:
+	NullInputDevice();
+};
+class WndProcInputDevice : public InputDevice
+{
+public:
+	WndProcInputDevice();
+	// Easiest, best behavior. If you don't use for text entry and cursor
+	//  control, it's probably because you're an idiot...
+	// NOTE: Raw input for mouse movement will be flipped on and processed as
+	//  seperate relative analog input in here. Responsible developers will
+	//  avoid using this unless they've got a reasonable case where they want
+	//  to break support for most user input devices (for example, if you are
+	//  an FPS that really only works with relative positioning devices, and
+	//  also have a community that demands sub-pixel DPI motion)
+};
+class DirectInputDevice : public InputDevice
+{
+public:
+	DirectInputDevice();
+	// Most compatible behavior, but basically mandates an input remapping gui,
+	//  so your users can map whatever the fuck the '_Rxz2' axis is supposed to
+	//  mean on their uber-joystick from the 90s
+};
+class XInputDevice : public InputDevice
+{
+public:
+	XInputDevice();
+	// XBox 360 controller, and absolutely nothing else. Great for shitty PC
+	//  ports or generally looking like a budget indie studio...
 };
 
 #error WIP, still just sketching out ideas
