158e4dcfbbff3484427aa9b0e838928cfa9d1508
Max Delta <54338561+max-delta@users.noreply.github.com>
2017-09-16 14:32:17 -0700

    Framepack editor WIP

--- apps/retrofit/source/FramePackEditor.h
+++ apps/retrofit/source/FramePackEditor.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <stdint.h>
+
 
 namespace RF {
 ///////////////////////////////////////////////////////////////////////////////
@@ -6,11 +8,28 @@ namespace RF {
 class FramePackEditor
 {
 	//
+	// Enums
+private:
+	enum class MasterMode : uint8_t
+	{
+		Meta = 0,
+		Texture,
+		Colliders
+	};
+
+
+	//
 	// Public methods
 public:
 	void Init();
 	void Process();
 	void Render();
+
+
+	//
+	// Private data
+private:
+	MasterMode m_MasterMode;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
