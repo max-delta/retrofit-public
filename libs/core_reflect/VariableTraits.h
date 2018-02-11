12c95bb71b3d310ef8869224be30df75eddcbab0
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-02-11 13:12:25 -0800

    Variable traits and tests

---
+++ libs/core_reflect/VariableTraits.h
@@ -0,0 +1,43 @@
+#pragma once
+
+namespace RF { namespace reflect {
+///////////////////////////////////////////////////////////////////////////////
+
+enum class VariableType : uint8_t
+{
+	Invalid = 0,
+	FreeStanding,
+	Member
+};
+
+
+
+// Un-defined, requires a specialization to match
+template<typename Variable>
+struct VariableTraits;
+
+
+
+// Free-standing variable
+template<typename Variable>
+struct VariableTraits<Variable*>
+{
+	static constexpr VariableType kVariableType = VariableType::FreeStanding;
+	static constexpr bool kIsConst = std::is_const<Variable>::value;
+	using VariableType = Variable;
+};
+
+
+
+// Member variable
+template<typename Class, typename Member>
+struct VariableTraits<Member Class::*>
+{
+	static constexpr VariableType kVariableType = VariableType::Member;
+	static constexpr bool kIsConst = std::is_const<Member>::value;
+	using ClassType = Class;
+	using VariableType = Member;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+}}
