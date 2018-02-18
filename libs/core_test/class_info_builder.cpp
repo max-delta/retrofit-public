52b84b8eb026e80ef3debb75fd014480070fda4f
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-02-17 19:59:24 -0800

    Initial ClassInfoBuilder work

---
+++ libs/core_test/class_info_builder.cpp
@@ -0,0 +1,245 @@
+#include "stdafx.h"
+
+#include "core_reflect/ClassInfoBuilder.h"
+
+
+namespace RF { namespace reflect {
+///////////////////////////////////////////////////////////////////////////////
+
+namespace details{
+
+// NOTE: Static member variables not allowed in function-scope classes
+struct ClassWithStaticMemberVariables
+{
+	static double static_d;
+	static double const static_cd;
+	static thread_local int16_t thread_i;
+	static thread_local int16_t const thread_ci;
+	static constexpr uint8_t constexpr_u = 0;
+};
+double ClassWithStaticMemberVariables::static_d;
+double const ClassWithStaticMemberVariables::static_cd = 0.0;
+thread_local int16_t ClassWithStaticMemberVariables::thread_i;
+thread_local int16_t const ClassWithStaticMemberVariables::thread_ci = 0;
+
+}
+
+
+
+TEST( ReflectBuilder, ClassInfos )
+{
+	// Virtual table changes alignment on some compilers, such as with a
+	//  pointer to the table stored in the instances
+	constexpr size_t kAlignmentConstraintOfPolymorphism = sizeof( void* );
+
+	{
+		// Empty
+		struct Test
+		{
+			//
+		};
+		ClassInfo classInfo;
+		builder::CreateClassInfo<Test>( classInfo );
+		ASSERT_TRUE( classInfo.mIsPolymorphic == false );
+		ASSERT_TRUE( classInfo.mIsAbstract == false );
+		ASSERT_TRUE( classInfo.mIsDefaultConstructible == true );
+		ASSERT_TRUE( classInfo.mIsCopyConstructible == true );
+		ASSERT_TRUE( classInfo.mIsMoveConstructible == true );
+		ASSERT_TRUE( classInfo.mIsDestructible == true );
+		ASSERT_TRUE( classInfo.mHasVirtualDestructor == false );
+		ASSERT_TRUE( classInfo.mMinimumAlignment == 1 );
+	}
+	{
+		// Virtual
+		struct Test
+		{
+			virtual void Virt()
+			{
+			}
+		};
+		ClassInfo classInfo;
+		builder::CreateClassInfo<Test>( classInfo );
+		ASSERT_TRUE( classInfo.mIsPolymorphic == true );
+		ASSERT_TRUE( classInfo.mIsAbstract == false );
+		ASSERT_TRUE( classInfo.mIsDefaultConstructible == true );
+		ASSERT_TRUE( classInfo.mIsCopyConstructible == true );
+		ASSERT_TRUE( classInfo.mIsMoveConstructible == true );
+		ASSERT_TRUE( classInfo.mIsDestructible == true );
+		ASSERT_TRUE( classInfo.mHasVirtualDestructor == false );
+		ASSERT_TRUE( classInfo.mMinimumAlignment == kAlignmentConstraintOfPolymorphism );
+	}
+	{
+		// Abstract
+		struct Test
+		{
+			virtual void Virt() = 0;
+		};
+		ClassInfo classInfo;
+		builder::CreateClassInfo<Test>( classInfo );
+		ASSERT_TRUE( classInfo.mIsPolymorphic == true );
+		ASSERT_TRUE( classInfo.mIsAbstract == true );
+		ASSERT_TRUE( classInfo.mIsDefaultConstructible == false );
+		ASSERT_TRUE( classInfo.mIsCopyConstructible == false );
+		ASSERT_TRUE( classInfo.mIsMoveConstructible == false );
+		ASSERT_TRUE( classInfo.mIsDestructible == false );
+		ASSERT_TRUE( classInfo.mHasVirtualDestructor == false );
+		ASSERT_TRUE( classInfo.mMinimumAlignment == kAlignmentConstraintOfPolymorphism );
+	}
+	{
+		// Correct destructor
+		struct Test
+		{
+			virtual void Virt()
+			{
+			}
+			virtual ~Test() = default;
+		};
+		ClassInfo classInfo;
+		builder::CreateClassInfo<Test>( classInfo );
+		ASSERT_TRUE( classInfo.mIsPolymorphic == true );
+		ASSERT_TRUE( classInfo.mIsAbstract == false );
+		ASSERT_TRUE( classInfo.mIsDefaultConstructible == true );
+		ASSERT_TRUE( classInfo.mIsCopyConstructible == true );
+		ASSERT_TRUE( classInfo.mIsMoveConstructible == true );
+		ASSERT_TRUE( classInfo.mIsDestructible == true );
+		ASSERT_TRUE( classInfo.mHasVirtualDestructor == true );
+		ASSERT_TRUE( classInfo.mMinimumAlignment == kAlignmentConstraintOfPolymorphism );
+	}
+	{
+		// Deleted functionality
+		struct Test
+		{
+			Test() = delete;
+			Test( Test const& ) = delete;
+			Test( Test && ) = delete;
+			~Test() = delete;
+		};
+		ClassInfo classInfo;
+		builder::CreateClassInfo<Test>( classInfo );
+		ASSERT_TRUE( classInfo.mIsPolymorphic == false );
+		ASSERT_TRUE( classInfo.mIsAbstract == false );
+		ASSERT_TRUE( classInfo.mIsDefaultConstructible == false );
+		ASSERT_TRUE( classInfo.mIsCopyConstructible == false );
+		ASSERT_TRUE( classInfo.mIsMoveConstructible == false );
+		ASSERT_TRUE( classInfo.mIsDestructible == false );
+		ASSERT_TRUE( classInfo.mHasVirtualDestructor == false );
+		ASSERT_TRUE( classInfo.mMinimumAlignment == 1 );
+	}
+}
+
+
+
+TEST( ReflectBuilder, FreeStandingVariableInfos )
+{
+	using Test = details::ClassWithStaticMemberVariables;
+
+	{
+		FreeStandingVariableInfo varInfo;
+		builder::CreateFreeStandingVariableInfo( varInfo, Test::static_d );
+		ASSERT_TRUE( varInfo.mAddress == &Test::static_d );
+		ASSERT_TRUE( varInfo.mMutable == true );
+		ASSERT_TRUE( varInfo.mSize == sizeof( Test::static_d ) );
+		ASSERT_TRUE( varInfo.mValueType == Value::Type::Double );
+	}
+	{
+		FreeStandingVariableInfo varInfo;
+		builder::CreateFreeStandingVariableInfo( varInfo, Test::static_cd );
+		ASSERT_TRUE( varInfo.mAddress == &Test::static_cd );
+		ASSERT_TRUE( varInfo.mMutable == false );
+		ASSERT_TRUE( varInfo.mSize == sizeof( Test::static_cd ) );
+		ASSERT_TRUE( varInfo.mValueType == Value::Type::Double );
+	}
+	{
+		FreeStandingVariableInfo varInfo;
+		builder::CreateFreeStandingVariableInfo( varInfo, Test::thread_i );
+		ASSERT_TRUE( varInfo.mAddress == &Test::thread_i );
+		ASSERT_TRUE( varInfo.mMutable == true );
+		ASSERT_TRUE( varInfo.mSize == sizeof( Test::thread_i ) );
+		ASSERT_TRUE( varInfo.mValueType == Value::Type::Int16 );
+	}
+	{
+		FreeStandingVariableInfo varInfo;
+		builder::CreateFreeStandingVariableInfo( varInfo, Test::thread_ci );
+		ASSERT_TRUE( varInfo.mAddress == &Test::thread_ci );
+		ASSERT_TRUE( varInfo.mMutable == false );
+		ASSERT_TRUE( varInfo.mSize == sizeof( Test::thread_ci ) );
+		ASSERT_TRUE( varInfo.mValueType == Value::Type::Int16 );
+	}
+	{
+		FreeStandingVariableInfo varInfo;
+		builder::CreateFreeStandingVariableInfo( varInfo, Test::constexpr_u );
+		ASSERT_TRUE( varInfo.mAddress == &Test::constexpr_u );
+		ASSERT_TRUE( varInfo.mMutable == false );
+		ASSERT_TRUE( varInfo.mSize == sizeof( Test::constexpr_u ) );
+		ASSERT_TRUE( varInfo.mValueType == Value::Type::UInt8 );
+	}
+}
+
+
+
+TEST( ReflectBuilder, MemberVariableInfos )
+{
+	struct Test
+	{
+		float instance_f;
+		float const instance_cf;
+	};
+
+	{
+		MemberVariableInfo varInfo;
+		builder::CreateMemberVariableInfo( varInfo, &Test::instance_f );
+		ASSERT_TRUE( varInfo.mOffset == offsetof( Test, instance_f ) );
+		ASSERT_TRUE( varInfo.mMutable == true );
+		ASSERT_TRUE( varInfo.mSize == sizeof( Test::instance_f ) );
+		ASSERT_TRUE( varInfo.mValueType == Value::Type::Float );
+	}
+	{
+		MemberVariableInfo varInfo;
+		builder::CreateMemberVariableInfo( varInfo, &Test::instance_cf );
+		ASSERT_TRUE( varInfo.mOffset == offsetof( Test, instance_cf ) );
+		ASSERT_TRUE( varInfo.mMutable == false );
+		ASSERT_TRUE( varInfo.mSize == sizeof( Test::instance_cf ) );
+		ASSERT_TRUE( varInfo.mValueType == Value::Type::Float );
+	}
+}
+
+
+
+TEST( ReflectBuilder, FreeStandingFunctionInfos )
+{
+	struct Test
+	{
+		static void v_call_v( void )
+		{
+			return;
+		}
+	};
+
+	{
+		FreeStandingFunctionInfo funcInfo;
+		builder::CreateFreeStandingFunctionInfo( funcInfo, Test::v_call_v );
+		ASSERT_TRUE( funcInfo.mAddress == Test::v_call_v );
+	}
+}
+
+
+
+TEST( ReflectBuilder, MemberFunctionInfos )
+{
+	struct Test
+	{
+		void v_call_v( void )
+		{
+			return;
+		}
+	};
+
+	{
+		MemberFunctionInfo funcInfo;
+		builder::CreateMemberFunctionInfo( funcInfo, &Test::v_call_v );
+		// TODO?
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+}}
