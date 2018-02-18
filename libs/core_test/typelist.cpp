200717039281b7a55df333345259202f8a955bc4
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-02-17 17:42:18 -0800

    TypeList additions

--- libs/core_test/typelist.cpp
+++ libs/core_test/typelist.cpp
@@ -11,6 +11,8 @@ TEST( TypeList, Empty )
 	// Should not cause compilation errors
 	using tl_empty = TypeList<>;
 	static_assert( tl_empty::kNumTypes == 0, "Unexpected size" );
+	static_assert( tl_empty::Contains<void*>::value == false, "Unexpected type presence" );
+	static_assert( tl_empty::FindIndex<void*>::value == -1, "Unexpected type presence" );
 }
 
 
@@ -20,7 +22,11 @@ TEST( TypeList, Single )
 	// Should not cause logic errors in non-list usage
 	using tl_f = TypeList<float>;
 	static_assert( tl_f::kNumTypes == 1, "Unexpected size" );
-	static_assert( std::is_same<tl_f::ByIndex<0>::Type, float>::value, "Unexpected type" );
+	static_assert( std::is_same<tl_f::ByIndex<0>::type, float>::value, "Unexpected type" );
+	static_assert( tl_f::Contains<void*>::value == false, "Unexpected type presence" );
+	static_assert( tl_f::Contains<float>::value == true, "Unexpected type presence" );
+	static_assert( tl_f::FindIndex<void*>::value == -1, "Unexpected type presence" );
+	static_assert( tl_f::FindIndex<float>::value == 0, "Unexpected type presence" );
 }
 
 
@@ -29,9 +35,17 @@ TEST( TypeList, Multiple )
 {
 	using tl_ifc = TypeList<int, float, char>;
 	static_assert( tl_ifc::kNumTypes == 3, "Unexpected size" );
-	static_assert( std::is_same<tl_ifc::ByIndex<0>::Type, int>::value, "Unexpected type" );
-	static_assert( std::is_same<tl_ifc::ByIndex<1>::Type, float>::value, "Unexpected type" );
-	static_assert( std::is_same<tl_ifc::ByIndex<2>::Type, char>::value, "Unexpected type" );
+	static_assert( std::is_same<tl_ifc::ByIndex<0>::type, int>::value, "Unexpected type" );
+	static_assert( std::is_same<tl_ifc::ByIndex<1>::type, float>::value, "Unexpected type" );
+	static_assert( std::is_same<tl_ifc::ByIndex<2>::type, char>::value, "Unexpected type" );
+	static_assert( tl_ifc::Contains<void*>::value == false, "Unexpected type presence" );
+	static_assert( tl_ifc::Contains<int>::value == true, "Unexpected type presence" );
+	static_assert( tl_ifc::Contains<float>::value == true, "Unexpected type presence" );
+	static_assert( tl_ifc::Contains<char>::value == true, "Unexpected type presence" );
+	static_assert( tl_ifc::FindIndex<void*>::value == -1, "Unexpected type presence" );
+	static_assert( tl_ifc::FindIndex<int>::value == 0, "Unexpected type presence" );
+	static_assert( tl_ifc::FindIndex<float>::value == 1, "Unexpected type presence" );
+	static_assert( tl_ifc::FindIndex<char>::value == 2, "Unexpected type presence" );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
