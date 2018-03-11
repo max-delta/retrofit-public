7793a007b9fde3eaf09a36c0a6b0d534a62546fb
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-03-11 15:17:42 -0700

    Minor commenting

--- libs/Logging/Logging.h
+++ libs/Logging/Logging.h
@@ -55,24 +55,29 @@ namespace details {
 // For extremely verbose logging that could significantly impact performance or
 //  generate extraordinary amounts of data unless under controlled inspection
 // Examples: Token is read from file, node is traversed
+// Expected user action: Examine alongside debugger
 #define RFLOG_TRACE(CONTEXT, CATEGORY, ...) ___RFLOG_IMPL( CONTEXT, CATEGORY, ::RF::logging::RF_SEV_TRACE, __VA_ARGS__ )
 
 // For verbose logging that is too noisy to be of value to anyone besides the
 //  owner of the code
 // Examples: Texture information after loading, duration of a long operation
+// Expected user action: Examine against expectations
 #define RFLOG_DEBUG(CONTEXT, CATEGORY, ...) ___RFLOG_IMPL( CONTEXT, CATEGORY, ::RF::logging::RF_SEV_DEBUG, __VA_ARGS__ )
 
 // For general informational messages that are useful to a wide audience
 // Examples: File is loaded, entity is created/destroyed
+// Expected user action: Ignore, occasionally cross-reference against errors
 #define RFLOG_INFO(CONTEXT, CATEGORY, ...) ___RFLOG_IMPL( CONTEXT, CATEGORY, ::RF::logging::RF_SEV_INFO, __VA_ARGS__ )
 
 // For a failure or quirk that is undesirable, but benign and safely handled
 // Examples: Texture is unreasonably large, expensive action has no effect
+// Expected user action: Cleanup when practical
 #define RFLOG_WARNING(CONTEXT, CATEGORY, ...) ___RFLOG_IMPL( CONTEXT, CATEGORY, ::RF::logging::RF_SEV_WARNING, __VA_ARGS__ )
 
 // For a failure that is unreliably handled, or diverts significantly from
 //  expected program flow, likely with detrimental observable side-effects
 // Examples: Can't load asset, character's head is missing
+// Expected user action: Fix when practical
 #define RFLOG_ERROR(CONTEXT, CATEGORY, ...) ___RFLOG_IMPL( CONTEXT, CATEGORY, ::RF::logging::RF_SEV_ERROR | ::RF::logging::RF_SEV_CALLSTACK_VALUABLE, __VA_ARGS__ )
 
 // For an extraordinary class of failures that may constitute emergencies in a
@@ -80,6 +85,7 @@ namespace details {
 //  even if the code can continue normally
 // NOTE: Assume this is enabled in retail builds
 // Examples: Significant datacenter outage, improper monetary transaction
+// Expected user action: Pagers go off, panic
 #define RFLOG_CRITICAL(CONTEXT, CATEGORY, ...) ___RFLOG_IMPL( CONTEXT, CATEGORY, ::RF::logging::RF_SEV_CRITICAL, __VA_ARGS__ )
 
 // For a catastrophic and unrecoverable error, where continuing the process
@@ -87,14 +93,17 @@ namespace details {
 //  diagnose if execution was allowed to continue
 // NOTE: Assume this is enabled in retail builds
 // Examples: Memory corruption detected, critical system failed to initialize
+// Expected user action: Investigate and fix
 #define RFLOG_FATAL(CONTEXT, CATEGORY, ...) ___RFLOG_ABORT_IMPL( CONTEXT, CATEGORY, ::RF::logging::RF_SEV_ERROR | ::RF::logging::RF_SEV_CALLSTACK_VALUABLE | ::RF::logging::RF_SEV_UNRECOVERABLE, __VA_ARGS__ )
 
 // For a major event that signifies a meaningful landmark in the application
 // Examples: Player dies, level transition, cutscene, quit to main menu
+// Expected user action: Ignore, occasionally use as a guide when viewing logs
 #define RFLOG_MILESTONE(CONTEXT, CATEGORY, ...) ___RFLOG_IMPL( CONTEXT, CATEGORY, ::RF::logging::RF_SEV_MILESTONE, __VA_ARGS__ )
 
 // For errors that a user should be made immediately aware of
 // Examples: Can't load asset, asset is invalid
+// Expected user action: Investigate and fix
 #define RFLOG_NOTIFY(CONTEXT, CATEGORY, ...) ___RFLOG_IMPL( CONTEXT, CATEGORY, ::RF::logging::RF_SEV_ERROR | ::RF::logging::RF_SEV_USER_ATTENTION_REQUESTED, __VA_ARGS__ )
 #define RFLOG_TEST_AND_NOTIFY(TEST, CONTEXT, CATEGORY, ...) ___RFLOG_TEST_IMPL( TEST, ___RFLOG_IMPL( CONTEXT, CATEGORY, ::RF::logging::RF_SEV_ERROR | ::RF::logging::RF_SEV_USER_ATTENTION_REQUESTED, __VA_ARGS__ ) )
 
