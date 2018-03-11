c26e58af7b629f475b2b86c6092517f6d4803cda
Max Delta <Max Delta@localhost>
2018-03-10 18:36:09 -0800

    Added core logging router

---
+++ libs/core_logging/LoggingRouter.h
@@ -0,0 +1,123 @@
+#pragma once
+#include "core/macros.h"
+
+#include <vector>
+#include <shared_mutex>
+#include <array>
+#include <stdint.h>
+
+
+namespace RF { namespace logging {
+///////////////////////////////////////////////////////////////////////////////
+
+class LoggingRouter
+{
+	RF_NO_COPY( LoggingRouter );
+
+	//
+	// Forwards
+public:
+	struct LogEvent;
+	struct HandlerDefinition;
+
+
+	//
+	// Types and constants
+public:
+	using CategoryKey = char const*;
+	using SeverityMask = uint64_t;
+	using SeverityStrings = std::array<char const*, 64>;
+	using HandlerID = uint64_t;
+	static constexpr HandlerID kInvalidHandlerID = 0;
+	using HandlerFunc = void(*)( LoggingRouter const&, LogEvent const&, va_list args );
+	static constexpr size_t kMaxContextLen = 512;
+	using LogContextBuffer = std::array<char, kMaxContextLen>;
+private:
+	static constexpr HandlerID kInitialHandlerID = kInvalidHandlerID + 1;
+	using ReaderWriterMutex = std::shared_mutex;
+	using ReaderLock = std::shared_lock<std::shared_mutex>;
+	using WriterLock = std::unique_lock<std::shared_mutex>;
+	using HandlerDefinitionByID = std::pair<HandlerID, HandlerDefinition>;
+	using HandlerDefinitionsByPriority = std::vector<HandlerDefinitionByID>;
+
+
+	//
+	// Structs
+public:
+	struct LogEvent
+	{
+		// Values not gauranteed to be consistent across builds
+		CategoryKey mCategoryKey;
+
+		// May have multiple severities set
+		SeverityMask mSeverityMask;
+
+		// Must be consumed immediately, pointers are not permanent
+		char const* mTransientContextString;
+		char const* mTransientMessageFormatString;
+	};
+
+	struct HandlerDefinition
+	{
+		// May recieve additional severities on multi-severity events
+		SeverityMask mSupportedSeverities;
+
+		// Must support multi-threading
+		HandlerFunc mHandlerFunc;
+	};
+
+
+	//
+	// Public methods
+public:
+	LoggingRouter();
+	LoggingRouter( LoggingRouter && ) = default;
+	LoggingRouter& operator=( LoggingRouter && ) = default;
+
+	template<typename Context>
+	void Log( Context context, CategoryKey categoryKey, SeverityMask severityMask, char const* format, ... ) const;
+	void Log( nullptr_t /*context*/, CategoryKey categoryKey, SeverityMask severityMask, char const* format, ... ) const;
+
+	// Control which handlers are able to handle log events
+	// NOTE: Handlers are run in the order they are registered
+	HandlerID RegisterHandler( HandlerDefinition const& handlerDefinition );
+	bool UnregisterHandler( HandlerID handlerID );
+
+	char const* GetStringFromCategoryKey( CategoryKey categoryKey ) const;
+
+	// Severity bits can have names, defined by immutable strings
+	// NOTE: This means that for practical purposes these strings must be
+	//  literals whose memory is tied to the lifetime of the executable
+	void SetSeverityString( SeverityMask singleBit, char const* immutableString );
+	SeverityStrings GetSeverityStrings() const;
+
+
+	//
+	// Private methods
+private:
+	void LogInternal( char const* context, CategoryKey categoryKey, SeverityMask severityMask, char const* format, va_list args ) const;
+
+
+	//
+	// Private data
+private:
+	mutable ReaderWriterMutex mMultiReaderSingleWriterLock;
+	HandlerID mHandlerIDGenerator;
+	HandlerDefinitionsByPriority mHandlerDefinitionsByPriority;
+	SeverityStrings mSeverityStrings;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+// Specialize this to add support for your context
+// EXAMPLE:
+//  template<> void WriteContextString(
+//   MyClass const& context,
+//   LoggingRouter::LogContextBuffer buffer );
+template<typename Context>
+void WriteContextString( Context const& context, LoggingRouter::LogContextBuffer buffer );
+
+///////////////////////////////////////////////////////////////////////////////
+}}
+
+#include "LoggingRouter.inl"
---
+++ libs/core_test/logging_router.cpp
@@ -0,0 +1,179 @@
+#include "stdafx.h"
+
+#include "core_logging/LoggingRouter.h"
+
+
+namespace RF { namespace logging {
+///////////////////////////////////////////////////////////////////////////////
+
+namespace details {
+
+static size_t gCount1, gCount2, gCount3, gCountAll;
+
+void ClearGlobalCounts()
+{
+	gCount1 = gCount2 = gCount3 = gCountAll = 0;
+}
+
+TEST( Logging, Details_ClearGlobalCounts )
+{
+	gCount1 = gCount2 = gCount3 = gCountAll = 47;
+	ClearGlobalCounts();
+	ASSERT_EQ( details::gCount1, 0 );
+	ASSERT_EQ( details::gCount2, 0 );
+	ASSERT_EQ( details::gCount3, 0 );
+	ASSERT_EQ( details::gCountAll, 0 );
+}
+
+void Handler1( LoggingRouter const&, LoggingRouter::LogEvent const&, va_list )
+{
+	gCount1++;
+}
+void Handler2( LoggingRouter const&, LoggingRouter::LogEvent const&, va_list )
+{
+	gCount2++;
+}
+void Handler3( LoggingRouter const&, LoggingRouter::LogEvent const&, va_list )
+{
+	gCount3++;
+}
+void HandlerAll( LoggingRouter const&, LoggingRouter::LogEvent const&, va_list )
+{
+	gCountAll++;
+}
+
+void SetupStandardRouter( LoggingRouter& router )
+{
+	router.SetSeverityString( 0x1, "1" );
+	router.SetSeverityString( 0x2, "2" );
+	router.SetSeverityString( 0x4, "3" );
+
+	LoggingRouter::HandlerDefinition handlerDef;
+	handlerDef.mSupportedSeverities = 0x1;
+	handlerDef.mHandlerFunc = Handler1;
+	router.RegisterHandler( handlerDef );
+	handlerDef.mSupportedSeverities = 0x2;
+	handlerDef.mHandlerFunc = Handler2;
+	router.RegisterHandler( handlerDef );
+	handlerDef.mSupportedSeverities = 0x4;
+	handlerDef.mHandlerFunc = Handler3;
+	router.RegisterHandler( handlerDef );
+	handlerDef.mSupportedSeverities = 0x7;
+	handlerDef.mHandlerFunc = HandlerAll;
+	router.RegisterHandler( handlerDef );
+}
+
+}
+
+
+
+TEST( Logging, UnsetNullLog )
+{
+	LoggingRouter router;
+	router.Log( nullptr, "", 1, "" );
+	router.Log( nullptr, "", 1, "" );
+	router.Log( nullptr, "", 1, "" );
+}
+
+
+
+TEST( Logging, UnsetTrashVarArgsToNowhere )
+{
+	// Handlers will probably fall over, but router shouldn't, since it
+	//  shouldn't be unpacking messages
+	LoggingRouter router;
+	router.Log( nullptr, "", 1, "%s%s%s", 0x1 );
+	router.Log( nullptr, "", 1, "%s%s%s", 0x1 );
+	router.Log( nullptr, "", 1, "%s%s%s", 0x1 );
+}
+
+
+
+TEST( Logging, Registration )
+{
+	LoggingRouter router;
+
+	struct Invalid
+	{
+		static void InvalidFunc( LoggingRouter const&, LoggingRouter::LogEvent const&, va_list )
+		{
+			GTEST_FAIL();
+		}
+	};
+
+	LoggingRouter::HandlerDefinition handlerDef;
+	handlerDef.mSupportedSeverities = 0x1;
+	handlerDef.mHandlerFunc = Invalid::InvalidFunc;
+
+	LoggingRouter::HandlerID const id1 = router.RegisterHandler( handlerDef );
+	ASSERT_NE( id1, LoggingRouter::kInvalidHandlerID );
+	LoggingRouter::HandlerID const id2 = router.RegisterHandler( handlerDef );
+	ASSERT_NE( id2, LoggingRouter::kInvalidHandlerID );
+	ASSERT_NE( id1, id2 );
+
+	router.UnregisterHandler( id1 );
+	router.UnregisterHandler( id2 );
+
+	// This should have no effect since the handlers should be unregistered
+	router.Log( nullptr, "", 1, "%s%s%s", 0x1 );
+}
+
+
+
+TEST( Logging, Basics )
+{
+	LoggingRouter router;
+	details::SetupStandardRouter( router );
+
+	details::ClearGlobalCounts();
+	router.Log( nullptr, "", 1, "%s%s%s", 0x1 );
+	ASSERT_EQ( details::gCount1, 1 );
+	ASSERT_EQ( details::gCount2, 0 );
+	ASSERT_EQ( details::gCount3, 0 );
+	ASSERT_EQ( details::gCountAll, 1 );
+
+	details::ClearGlobalCounts();
+	router.Log( nullptr, "", 2, "%s%s%s", 0x1 );
+	ASSERT_EQ( details::gCount1, 0 );
+	ASSERT_EQ( details::gCount2, 1 );
+	ASSERT_EQ( details::gCount3, 0 );
+	ASSERT_EQ( details::gCountAll, 1 );
+
+	details::ClearGlobalCounts();
+	router.Log( nullptr, "", 4, "%s%s%s", 0x1 );
+	ASSERT_EQ( details::gCount1, 0 );
+	ASSERT_EQ( details::gCount2, 0 );
+	ASSERT_EQ( details::gCount3, 1 );
+	ASSERT_EQ( details::gCountAll, 1 );
+}
+
+
+
+TEST( Logging, GetStringFromCategoryKey )
+{
+	LoggingRouter router;
+
+	constexpr LoggingRouter::CategoryKey key = "literal";
+	char const* string = router.GetStringFromCategoryKey( key );
+	ASSERT_EQ( string, key );
+}
+
+
+
+TEST( Logging, SeverityStrings )
+{
+	LoggingRouter router;
+	details::SetupStandardRouter( router );
+
+	LoggingRouter::SeverityStrings severityStrings = router.GetSeverityStrings();
+	ASSERT_TRUE( severityStrings.at( 0 ) != nullptr );
+	ASSERT_TRUE( severityStrings.at( 1 ) != nullptr );
+	ASSERT_TRUE( severityStrings.at( 2 ) != nullptr );
+	for( size_t i = 3; i < severityStrings.size(); i++ )
+	{
+		ASSERT_TRUE( severityStrings.at( i ) == nullptr );
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+}}
