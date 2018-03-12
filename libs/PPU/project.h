25795f3a21399643c20630822a2c9cd2c653fd79
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-03-11 18:33:12 -0700

    Replaced a bunch of obvious trash with actual logging

--- libs/PPU/project.h
+++ libs/PPU/project.h
@@ -1,7 +1,11 @@
 #pragma once
+#include "Logging/Constants.h"
 
 #ifdef PPU_EXPORTS
 #define PPU_API __declspec(dllexport)
 #else
 #define PPU_API __declspec(dllimport)
 #endif
+
+constexpr char const* RFCAT_PPU = "PPU";
+constexpr uint64_t RFCAT_PPU_SEV_WHITELIST = -1;
