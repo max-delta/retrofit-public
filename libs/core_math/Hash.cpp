e4fca0a52e4bb3ed4571a529d20a1b7a2c9ce908
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-03-17 10:17:00 -0700

    Added dynamic filtering for logging

--- libs/core_math/Hash.cpp
+++ libs/core_math/Hash.cpp
@@ -6,6 +6,40 @@
 namespace RF { namespace math {
 ///////////////////////////////////////////////////////////////////////////////
 
+HashVal64 DirectHash::operator() ( HashVal64 const& key ) const
+{
+	return key;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+HashVal64 NullTerminatedStringHash::operator()( char const * const buffer ) const
+{
+	if( buffer == nullptr )
+	{
+		return StableHashBytes( nullptr, 0 );
+	}
+
+	size_t length = 0;
+	while( buffer[length] != '\0' )
+	{
+		length++;
+	}
+
+	return StableHashBytes( buffer, length );
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+HashVal64 StableHashBytes( std::nullptr_t, size_t )
+{
+	// Reduced from non-null case
+	constexpr HashVal64 kFNV_offset_basis = 14695981039346656037ull;
+	return kFNV_offset_basis;
+}
+
+
+
 HashVal64 StableHashBytes( void const* buffer, size_t length )
 {
 	// Don't change the algorithm! Code may take dependencies on this behavior
--- libs/core_test/math_hash.cpp
+++ libs/core_test/math_hash.cpp
@@ -6,6 +6,32 @@
 namespace RF { namespace math {
 ///////////////////////////////////////////////////////////////////////////////
 
+TEST( Hash, DirectHasher )
+{
+	struct DirectHash hasher;
+	ASSERT_EQ( hasher( 0 ), 0 );
+	ASSERT_EQ( hasher( 1 ), 1 );
+	ASSERT_EQ( hasher( 2 ), 2 );
+	ASSERT_EQ( hasher( 3 ), 3 );
+	ASSERT_EQ( hasher( std::numeric_limits<HashVal64>::max() ), std::numeric_limits<HashVal64>::max() );
+}
+
+
+
+TEST( Hash, NullTerminatedStringHasher )
+{
+	// This hashing function better not change! Strings hashes need to be able
+	//  to be relied on across platforms and over time
+	struct NullTerminatedStringHash hasher;
+	ASSERT_EQ( hasher( nullptr ), 14695981039346656037ull );
+	ASSERT_EQ( hasher( "" ), 14695981039346656037ull ); // NOTE: Same as null
+	ASSERT_EQ( hasher( "\0" ), 14695981039346656037ull ); // NOTE: Same as empty
+	ASSERT_EQ( hasher( "\1" ), 12638152016183539244ull );
+	ASSERT_EQ( hasher( "abc" ), 16654208175385433931ull );
+}
+
+
+
 TEST(Hash, StableHashBytes)
 {
 	// This hashing function better not change! The stable hash should be able
