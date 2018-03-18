51807081975e3500140b4037057a9a8d9bdc7f83
Max Delta <Max Delta@localhost>
2018-03-17 18:28:52 -0700

    Sketch up for exporter interface and a diagnostic exporter

--- libs/Serialization/project.h
+++ libs/Serialization/project.h
@@ -1,7 +1,11 @@
 #pragma once
+#include "Logging/Constants.h"
 
 #ifdef SERIALIZATION_EXPORTS
 #define SERIALIZATION_API __declspec(dllexport)
 #else
 #define SERIALIZATION_API __declspec(dllimport)
 #endif
+
+constexpr char const* RFCAT_SERIALIZATION = "Serialization";
+constexpr uint64_t RFCAT_SERIALIZATION_SEV_WHITELIST = -1;
