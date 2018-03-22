870d7f88733b335edcb501fc6b181ab2ca30371b
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-03-21 22:33:48 -0700

    Added type list reverse, append, prepend

--- libs/core_test/typelist.cpp
+++ libs/core_test/typelist.cpp
@@ -13,6 +13,7 @@ TEST( TypeList, Empty )
 	static_assert( tl_empty::kNumTypes == 0, "Unexpected size" );
 	static_assert( tl_empty::Contains<void*>::value == false, "Unexpected type presence" );
 	static_assert( tl_empty::FindIndex<void*>::value == -1, "Unexpected type presence" );
+	static_assert( tl_empty::Reverse<>::type::kNumTypes == 0, "Unexpected size" );
 }
 
 
@@ -27,6 +28,7 @@ TEST( TypeList, Single )
 	static_assert( tl_f::Contains<float>::value == true, "Unexpected type presence" );
 	static_assert( tl_f::FindIndex<void*>::value == -1, "Unexpected type presence" );
 	static_assert( tl_f::FindIndex<float>::value == 0, "Unexpected type presence" );
+	static_assert( std::is_same<tl_f::Reverse<>::type::ByIndex<0>::type, float>::value, "Unexpected type" );
 	static_assert( std::is_same<tl_f::Split<0>::latter::ByIndex<0>::type, float>::value, "Unexpected type" );
 	static_assert( tl_f::Split<1>::latter::kNumTypes == 0, "Unexpected size" );
 }
@@ -48,6 +50,9 @@ TEST( TypeList, Multiple )
 	static_assert( tl_ifc::FindIndex<int>::value == 0, "Unexpected type presence" );
 	static_assert( tl_ifc::FindIndex<float>::value == 1, "Unexpected type presence" );
 	static_assert( tl_ifc::FindIndex<char>::value == 2, "Unexpected type presence" );
+	static_assert( std::is_same<tl_ifc::Reverse<>::type::ByIndex<0>::type, char>::value, "Unexpected type" );
+	static_assert( std::is_same<tl_ifc::Reverse<>::type::ByIndex<1>::type, float>::value, "Unexpected type" );
+	static_assert( std::is_same<tl_ifc::Reverse<>::type::ByIndex<2>::type, int>::value, "Unexpected type" );
 	static_assert( std::is_same<tl_ifc::Split<0>::latter::ByIndex<0>::type, int>::value, "Unexpected type" );
 	static_assert( std::is_same<tl_ifc::Split<0>::latter::ByIndex<1>::type, float>::value, "Unexpected type" );
 	static_assert( std::is_same<tl_ifc::Split<0>::latter::ByIndex<2>::type, char>::value, "Unexpected type" );
@@ -55,6 +60,22 @@ TEST( TypeList, Multiple )
 	static_assert( std::is_same<tl_ifc::Split<1>::latter::ByIndex<1>::type, char>::value, "Unexpected type" );
 	static_assert( std::is_same<tl_ifc::Split<2>::latter::ByIndex<0>::type, char>::value, "Unexpected type" );
 	static_assert( tl_ifc::Split<3>::latter::kNumTypes == 0, "Unexpected size" );
+}
+
+
+
+TEST( TypeList, Add )
+{
+	using tl_ifc = TypeList<int, float, char>;
+	using tl_empty = TypeList<>;
+
+	// Append/prepend on empty should be identical
+	static_assert( std::is_same<tl_empty::Append<float>::type, tl_empty::Prepend<float>::type>::value, "Unexpected type" );
+
+	using tl_added_f = tl_empty::Append<float>::type;
+	using tl_added_if = tl_added_f::Prepend<int>::type;
+	using tl_added_ifc = tl_added_if::Append<char>::type;
+	static_assert( std::is_same<tl_ifc, tl_added_ifc>::value, "Unexpected type" );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
