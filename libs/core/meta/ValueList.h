8fe720755c2168f0a6d86160739f61bffe54b288
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-03-22 21:46:54 -0700

    Added append, prepend, reverse, split for value lists

--- libs/core/meta/ValueList.h
+++ libs/core/meta/ValueList.h
@@ -5,6 +5,39 @@ namespace RF {
 ///////////////////////////////////////////////////////////////////////////////
 
 template<typename ValueTypeT, ValueTypeT... Values>
+struct ValueList;
+
+// These MUST be fully outside of the class, or MSVC2017 compiler crashes
+//  with an internal compilation error
+namespace typelist_details
+{
+template<size_t FirstIndexOfSecondSegment, typename RemainingValueListType>
+struct ExternalAccessSplitKeepLatter;
+
+// Zero-th case
+template<typename ValueTypeT>
+struct ExternalAccessSplitKeepLatter<0, ValueList<ValueTypeT> >
+{
+	using latter = ValueList<ValueTypeT>;
+};
+
+// Zero-th case
+template<typename ValueTypeT, ValueTypeT CurrentValue, ValueTypeT... RemainingValues>
+struct ExternalAccessSplitKeepLatter<0, ValueList<ValueTypeT, CurrentValue, RemainingValues...> >
+{
+	using latter = ValueList<ValueTypeT, CurrentValue, RemainingValues...>;
+};
+
+// N-th case
+template<typename ValueTypeT, size_t FirstIndexOfSecondSegment, ValueTypeT CurrentValue, ValueTypeT... RemainingValues>
+struct ExternalAccessSplitKeepLatter<FirstIndexOfSecondSegment, ValueList<ValueTypeT, CurrentValue, RemainingValues...> >
+{
+	static_assert( FirstIndexOfSecondSegment - 1 <= sizeof...( RemainingValues ), "Attempting to split past the end of type list" );
+	using latter = typename ExternalAccessSplitKeepLatter< FirstIndexOfSecondSegment - 1, ValueList<ValueTypeT, RemainingValues...> >::latter;
+};
+}
+
+template<typename ValueTypeT, ValueTypeT... Values>
 struct ValueList
 {
 	using ValueType = ValueTypeT;
@@ -24,9 +57,19 @@ struct ValueList
 	template<ValueType Value>
 	struct FindIndex;
 
-	//// Used to split a list into 2 segments
-	//template<size_t LastIndexOfFirstSegment>
-	//struct Split;
+	// Used to add to a list
+	template<ValueType Value>
+	struct Prepend;
+	template<ValueType Value>
+	struct Append;
+
+	// Used to reverse a list
+	template<int Unused = 0>
+	struct Reverse;
+
+	// Used to split a list into 2 segments
+	template<size_t FirstIndexOfSecondSegment>
+	struct Split;
 
 
 private:
@@ -39,16 +82,20 @@ private:
 	struct ExternalAccessContains;
 	template<ValueType Value, size_t CurrentIndex, typename ValueListType>
 	struct ExternalAccessFindIndex;
-	//template<size_t LastIndexOfFirstSegment, typename PrecedingValueListType, typename RemainingValueListType>
-	//struct ExternalAccessSplit;
+	template<ValueType Value, typename ValueListType>
+	struct ExternalAccessPrepend;
+	template<ValueType Value, typename ValueListType>
+	struct ExternalAccessAppend;
+	template<typename ValueListType>
+	struct ExternalAccessReverse;
 
 	// 0->N-th case
 	// NOTE: This COULD be recursive to explicitly prevent value array
 	//  creation, but that crashes the MSVC2017 compiler
-	template<size_t Index, ValueType... Values>
-	struct ExternalAccessByIndex<Index, ValueList<ValueType, Values...> >
+	template<size_t Index, ValueType... ValuesT>
+	struct ExternalAccessByIndex<Index, ValueList<ValueType, ValuesT...> >
 	{
-		static constexpr ValueType values[] = { Values... };
+		static constexpr ValueType values[] = { ValuesT... };
 		static constexpr ValueType value = values[Index];
 	};
 
@@ -82,6 +129,36 @@ private:
 		static constexpr int64_t value = kIsCurrent ? CurrentIndex : ExternalAccessFindIndex<Value, CurrentIndex + 1, ValueList<ValueType, RemainingValues...> >::value;
 	};
 
+	// Single case
+	template<ValueType Value, ValueType... ExistingValues>
+	struct ExternalAccessPrepend<Value, ValueList<ValueType, ExistingValues...> >
+	{
+		using type = ValueList<ValueType, Value, ExistingValues...>;
+	};
+
+	// Single case
+	template<ValueType Value, ValueType... ExistingValues>
+	struct ExternalAccessAppend<Value, ValueList<ValueType, ExistingValues...> >
+	{
+		using type = ValueList<ValueType, ExistingValues..., Value>;
+	};
+
+	// Zero-th case
+	template<>
+	struct ExternalAccessReverse<ValueList<ValueType> >
+	{
+		using type = ValueList<ValueType>;
+	};
+
+	// N-th case
+	template<ValueType CurrentValue, ValueType... RemainingValues>
+	struct ExternalAccessReverse<ValueList<ValueType, CurrentValue, RemainingValues...> >
+	{
+		using RemainingTypeList = ValueList<ValueType, RemainingValues...>;
+		using RemainingTypeListReversed = typename ExternalAccessReverse<RemainingTypeList>::type;
+		using type = typename ExternalAccessAppend<CurrentValue, RemainingTypeListReversed>::type;
+	};
+
 
 public:
 	// Implemented as external
@@ -105,12 +182,37 @@ public:
 		//
 	};
 
-	//// Implemented as external
-	//template<size_t LastIndexOfFirstSegment>
-	//struct Split : ExternalAccessSplit< LastIndexOfFirstSegment, 0, ValueList<ValueType>, ValueList<ValueType, Values...> >
-	//{
-	//	//
-	//};
+	// Implemented as external
+	template<ValueType Value>
+	struct Prepend : ExternalAccessPrepend<Value, ValueList<ValueType, Values...> >
+	{
+		//
+	};
+
+	// Implemented as external
+	template<ValueType Value>
+	struct Append : ExternalAccessAppend<Value, ValueList<ValueType, Values...> >
+	{
+		//
+	};
+
+	// Implemented as external
+	template<int Unused>
+	struct Reverse : ExternalAccessReverse< ValueList<ValueType, Values...> >
+	{
+		//
+	};
+
+	// Implemented as external
+	template<size_t FirstIndexOfSecondSegment>
+	struct Split
+	{
+		static constexpr size_t kReversedLastIndexOfFirstSegment = kNumValues - FirstIndexOfSecondSegment;
+		using ReversedList = typename ExternalAccessReverse< ValueList<ValueType, Values...> >::type;
+		using SplitReversedList = typename typelist_details::ExternalAccessSplitKeepLatter< kReversedLastIndexOfFirstSegment, ReversedList >::latter;
+		using former = typename ExternalAccessReverse< SplitReversedList >::type;
+		using latter = typename typelist_details::ExternalAccessSplitKeepLatter< FirstIndexOfSecondSegment, ValueList<ValueType, Values...> >::latter;
+	};
 };
 
 ///////////////////////////////////////////////////////////////////////////////
