2f1e521d120e74099853b6fc2da97a5a09c1ba44
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-03-22 21:56:28 -0700

    Minor cleanup

--- libs/core_test/valuelist.cpp
+++ libs/core_test/valuelist.cpp
@@ -9,13 +9,13 @@ namespace RF {
 TEST( ValueList, Empty )
 {
 	// Should not cause compilation errors
-	using tl_empty = ValueList<int>;
-	static_assert( tl_empty::kNumValues == 0, "Unexpected size" );
-	static_assert( tl_empty::Contains<5>::value == false, "Unexpected value presence" );
-	static_assert( tl_empty::FindIndex<5>::value == -1, "Unexpected value presence" );
-	static_assert( tl_empty::Reverse<>::type::kNumValues == 0, "Unexpected size" );
-	static_assert( tl_empty::Split<0>::former::kNumValues == 0, "Unexpected size" );
-	static_assert( tl_empty::Split<0>::latter::kNumValues == 0, "Unexpected size" );
+	using vl_empty = ValueList<int>;
+	static_assert( vl_empty::kNumValues == 0, "Unexpected size" );
+	static_assert( vl_empty::Contains<5>::value == false, "Unexpected value presence" );
+	static_assert( vl_empty::FindIndex<5>::value == -1, "Unexpected value presence" );
+	static_assert( vl_empty::Reverse<>::type::kNumValues == 0, "Unexpected size" );
+	static_assert( vl_empty::Split<0>::former::kNumValues == 0, "Unexpected size" );
+	static_assert( vl_empty::Split<0>::latter::kNumValues == 0, "Unexpected size" );
 }
 
 
@@ -24,14 +24,14 @@ TEST( ValueList, UnorthodoxValueTypes )
 {
 	// While ill-advised, should not cause compilation errors
 	struct Unused;
-	using tl_empty = ValueList<Unused*>;
-	static_assert( tl_empty::kNumValues == 0, "Unexpected size" );
+	using vl_empty = ValueList<Unused*>;
+	static_assert( vl_empty::kNumValues == 0, "Unexpected size" );
 	#ifndef __INTELLISENSE__ // Intellisense falls over in MSVC2017
-		static_assert( tl_empty::Contains<reinterpret_cast<Unused*>( 5 )>::value == false, "Unexpected value presence" );
-		static_assert( tl_empty::FindIndex<reinterpret_cast<Unused*>( 5 )>::value == -1, "Unexpected value presence" );
-		static_assert( tl_empty::Reverse<>::type::kNumValues == 0, "Unexpected size" );
-		static_assert( tl_empty::Split<0>::former::kNumValues == 0, "Unexpected size" );
-		static_assert( tl_empty::Split<0>::latter::kNumValues == 0, "Unexpected size" );
+		static_assert( vl_empty::Contains<reinterpret_cast<Unused*>( 5 )>::value == false, "Unexpected value presence" );
+		static_assert( vl_empty::FindIndex<reinterpret_cast<Unused*>( 5 )>::value == -1, "Unexpected value presence" );
+		static_assert( vl_empty::Reverse<>::type::kNumValues == 0, "Unexpected size" );
+		static_assert( vl_empty::Split<0>::former::kNumValues == 0, "Unexpected size" );
+		static_assert( vl_empty::Split<0>::latter::kNumValues == 0, "Unexpected size" );
 	#endif
 }
 
@@ -40,70 +40,70 @@ TEST( ValueList, UnorthodoxValueTypes )
 TEST( ValueList, Single )
 {
 	// Should not cause logic errors in non-list usage
-	using tl_f = ValueList<int, 5>;
-	static_assert( tl_f::kNumValues == 1, "Unexpected size" );
-	static_assert( tl_f::ByIndex<0>::value == 5, "Unexpected value" );
-	static_assert( tl_f::Contains<3>::value == false, "Unexpected value presence" );
-	static_assert( tl_f::Contains<5>::value == true, "Unexpected value presence" );
-	static_assert( tl_f::FindIndex<3>::value == -1, "Unexpected value presence" );
-	static_assert( tl_f::FindIndex<5>::value == 0, "Unexpected value presence" );
-	static_assert( tl_f::Reverse<>::type::ByIndex<0>::value == 5, "Unexpected type" );
-	static_assert( tl_f::Split<0>::former::kNumValues == 0, "Unexpected size" );
-	static_assert( tl_f::Split<0>::latter::ByIndex<0>::value == 5, "Unexpected type" );
-	static_assert( tl_f::Split<1>::former::ByIndex<0>::value == 5, "Unexpected type" );
-	static_assert( tl_f::Split<1>::latter::kNumValues == 0, "Unexpected size" );
+	using vl_5 = ValueList<int, 5>;
+	static_assert( vl_5::kNumValues == 1, "Unexpected size" );
+	static_assert( vl_5::ByIndex<0>::value == 5, "Unexpected value" );
+	static_assert( vl_5::Contains<3>::value == false, "Unexpected value presence" );
+	static_assert( vl_5::Contains<5>::value == true, "Unexpected value presence" );
+	static_assert( vl_5::FindIndex<3>::value == -1, "Unexpected value presence" );
+	static_assert( vl_5::FindIndex<5>::value == 0, "Unexpected value presence" );
+	static_assert( vl_5::Reverse<>::type::ByIndex<0>::value == 5, "Unexpected type" );
+	static_assert( vl_5::Split<0>::former::kNumValues == 0, "Unexpected size" );
+	static_assert( vl_5::Split<0>::latter::ByIndex<0>::value == 5, "Unexpected type" );
+	static_assert( vl_5::Split<1>::former::ByIndex<0>::value == 5, "Unexpected type" );
+	static_assert( vl_5::Split<1>::latter::kNumValues == 0, "Unexpected size" );
 }
 
 
 
 TEST( ValueList, Multiple )
 {
-	using tl_ifc = ValueList<int, 5, 7, 8>;
-	static_assert( tl_ifc::kNumValues == 3, "Unexpected size" );
-	static_assert( tl_ifc::ByIndex<0>::value == 5, "Unexpected value" );
-	static_assert( tl_ifc::ByIndex<1>::value == 7, "Unexpected value" );
-	static_assert( tl_ifc::ByIndex<2>::value == 8, "Unexpected value" );
-	static_assert( tl_ifc::Contains<3>::value == false, "Unexpected value presence" );
-	static_assert( tl_ifc::Contains<5>::value == true, "Unexpected value presence" );
-	static_assert( tl_ifc::Contains<7>::value == true, "Unexpected value presence" );
-	static_assert( tl_ifc::Contains<8>::value == true, "Unexpected value presence" );
-	static_assert( tl_ifc::FindIndex<3>::value == -1, "Unexpected value presence" );
-	static_assert( tl_ifc::FindIndex<5>::value == 0, "Unexpected value presence" );
-	static_assert( tl_ifc::FindIndex<7>::value == 1, "Unexpected value presence" );
-	static_assert( tl_ifc::FindIndex<8>::value == 2, "Unexpected value presence" );
-	static_assert( tl_ifc::Reverse<>::type::ByIndex<0>::value == 8, "Unexpected type" );
-	static_assert( tl_ifc::Reverse<>::type::ByIndex<1>::value == 7, "Unexpected type" );
-	static_assert( tl_ifc::Reverse<>::type::ByIndex<2>::value == 5, "Unexpected type" );
-	static_assert( tl_ifc::Split<0>::former::kNumValues == 0, "Unexpected size" );
-	static_assert( tl_ifc::Split<0>::latter::ByIndex<0>::value == 5, "Unexpected type" );
-	static_assert( tl_ifc::Split<0>::latter::ByIndex<1>::value == 7, "Unexpected type" );
-	static_assert( tl_ifc::Split<0>::latter::ByIndex<2>::value == 8, "Unexpected type" );
-	static_assert( tl_ifc::Split<1>::former::ByIndex<0>::value == 5, "Unexpected type" );
-	static_assert( tl_ifc::Split<1>::latter::ByIndex<0>::value == 7, "Unexpected type" );
-	static_assert( tl_ifc::Split<1>::latter::ByIndex<1>::value == 8, "Unexpected type" );
-	static_assert( tl_ifc::Split<2>::former::ByIndex<0>::value == 5, "Unexpected type" );
-	static_assert( tl_ifc::Split<2>::former::ByIndex<1>::value == 7, "Unexpected type" );
-	static_assert( tl_ifc::Split<2>::latter::ByIndex<0>::value == 8, "Unexpected type" );
-	static_assert( tl_ifc::Split<3>::former::ByIndex<0>::value == 5, "Unexpected type" );
-	static_assert( tl_ifc::Split<3>::former::ByIndex<1>::value == 7, "Unexpected type" );
-	static_assert( tl_ifc::Split<3>::former::ByIndex<2>::value == 8, "Unexpected type" );
-	static_assert( tl_ifc::Split<3>::latter::kNumValues == 0, "Unexpected size" );
+	using vl_578 = ValueList<int, 5, 7, 8>;
+	static_assert( vl_578::kNumValues == 3, "Unexpected size" );
+	static_assert( vl_578::ByIndex<0>::value == 5, "Unexpected value" );
+	static_assert( vl_578::ByIndex<1>::value == 7, "Unexpected value" );
+	static_assert( vl_578::ByIndex<2>::value == 8, "Unexpected value" );
+	static_assert( vl_578::Contains<3>::value == false, "Unexpected value presence" );
+	static_assert( vl_578::Contains<5>::value == true, "Unexpected value presence" );
+	static_assert( vl_578::Contains<7>::value == true, "Unexpected value presence" );
+	static_assert( vl_578::Contains<8>::value == true, "Unexpected value presence" );
+	static_assert( vl_578::FindIndex<3>::value == -1, "Unexpected value presence" );
+	static_assert( vl_578::FindIndex<5>::value == 0, "Unexpected value presence" );
+	static_assert( vl_578::FindIndex<7>::value == 1, "Unexpected value presence" );
+	static_assert( vl_578::FindIndex<8>::value == 2, "Unexpected value presence" );
+	static_assert( vl_578::Reverse<>::type::ByIndex<0>::value == 8, "Unexpected type" );
+	static_assert( vl_578::Reverse<>::type::ByIndex<1>::value == 7, "Unexpected type" );
+	static_assert( vl_578::Reverse<>::type::ByIndex<2>::value == 5, "Unexpected type" );
+	static_assert( vl_578::Split<0>::former::kNumValues == 0, "Unexpected size" );
+	static_assert( vl_578::Split<0>::latter::ByIndex<0>::value == 5, "Unexpected type" );
+	static_assert( vl_578::Split<0>::latter::ByIndex<1>::value == 7, "Unexpected type" );
+	static_assert( vl_578::Split<0>::latter::ByIndex<2>::value == 8, "Unexpected type" );
+	static_assert( vl_578::Split<1>::former::ByIndex<0>::value == 5, "Unexpected type" );
+	static_assert( vl_578::Split<1>::latter::ByIndex<0>::value == 7, "Unexpected type" );
+	static_assert( vl_578::Split<1>::latter::ByIndex<1>::value == 8, "Unexpected type" );
+	static_assert( vl_578::Split<2>::former::ByIndex<0>::value == 5, "Unexpected type" );
+	static_assert( vl_578::Split<2>::former::ByIndex<1>::value == 7, "Unexpected type" );
+	static_assert( vl_578::Split<2>::latter::ByIndex<0>::value == 8, "Unexpected type" );
+	static_assert( vl_578::Split<3>::former::ByIndex<0>::value == 5, "Unexpected type" );
+	static_assert( vl_578::Split<3>::former::ByIndex<1>::value == 7, "Unexpected type" );
+	static_assert( vl_578::Split<3>::former::ByIndex<2>::value == 8, "Unexpected type" );
+	static_assert( vl_578::Split<3>::latter::kNumValues == 0, "Unexpected size" );
 }
 
 
 
 TEST( ValueList, Add )
 {
-	using tl_ifc = ValueList<int, 5, 7, 8>;
-	using tl_empty = ValueList<int>;
+	using vl_578 = ValueList<int, 5, 7, 8>;
+	using vl_empty = ValueList<int>;
 
 	// Append/prepend on empty should be identical
-	static_assert( std::is_same<tl_empty::Append<7>::type, tl_empty::Prepend<7>::type>::value, "Unexpected type" );
+	static_assert( std::is_same<vl_empty::Append<7>::type, vl_empty::Prepend<7>::type>::value, "Unexpected type" );
 
-	using tl_added_f = tl_empty::Append<7>::type;
-	using tl_added_if = tl_added_f::Prepend<5>::type;
-	using tl_added_ifc = tl_added_if::Append<8>::type;
-	static_assert( std::is_same<tl_ifc, tl_added_ifc>::value, "Unexpected type" );
+	using vl_added_7 = vl_empty::Append<7>::type;
+	using vl_added_57 = vl_added_7::Prepend<5>::type;
+	using vl_added_578 = vl_added_57::Append<8>::type;
+	static_assert( std::is_same<vl_578, vl_added_578>::value, "Unexpected type" );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
