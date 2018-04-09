fa8e4c45981a24ef738a768d5061babab6af1eda
Max Delta <Max Delta@localhost>
2018-04-08 18:07:31 -0700

    More uuid nonsense

--- libs/core_platform/uuid.cpp
+++ libs/core_platform/uuid.cpp
@@ -82,6 +82,48 @@ bool Uuid::IsValid() const
 
 
 
+bool Uuid::ExposesComputerInformation() const
+{
+	uint8_t const version = GetVersion();
+	// 3 and 5 MAY expose computer information, but it's not knowable. Version
+	//  5 is much less likely to expose information than version 3 though.
+	constexpr bool kParanoid = true;
+	return
+		version == 1 ||
+		version == 2 ||
+		( kParanoid && version == 3 );
+}
+
+
+
+uint8_t Uuid::GetVersion() const
+{
+	// 0 = Invalid?
+	// 1 = Time and MAC
+	// 2 = DCE Security
+	// 3 = MD5 hash
+	// 4 = RNG and pray
+	// 5 = SHA-1 hash
+	Octet const& versionOctet = mOctetSequence[6];
+	uint8_t const version = ( versionOctet & 0xf0u ) >> 4;
+	return version;
+}
+
+
+
+uint8_t Uuid::GetVariant() const
+{
+	// 0 - 3 = NCS compat
+	// 4 - 5 = Standard
+	// 6 = MS compat
+	// 7 = Reserved
+	Octet const& variantOctet = mOctetSequence[6];
+	uint8_t const variant = ( variantOctet & 0xe0u ) >> 5;
+	return variant;
+}
+
+
+
 Uuid::OctetSequence const & Uuid::GetSequence() const
 {
 	return mOctetSequence;
@@ -92,7 +134,7 @@ Uuid::OctetSequence const & Uuid::GetSequence() const
 rftl::string Uuid::GetDebugString() const
 {
 	char octetBuf[3] = { '0','0','\0' };
-	constexpr char kExample[] = "{11223344-1122-1122-1122-112233445566}(v1,1)";
+	constexpr char kExample[] = "{11223344-1122-1122-1122-112233445566}(v1!,1)";
 	rftl::string retVal;
 	retVal.reserve( sizeof( kExample ) );
 
@@ -117,26 +159,16 @@ rftl::string Uuid::GetDebugString() const
 	retVal.push_back( '(' );
 	retVal.push_back( 'v' );
 	{
-		// 0 = Invalid?
-		// 1 = Time and MAC
-		// 2 = DCE Security
-		// 3 = MD5 hash
-		// 4 = RNG and pray
-		// 5 = SHA-1 hash
-		Octet const& versionOctet = mOctetSequence[6];
-		uint8_t const version = ( versionOctet & 0xf0u ) >> 4;
-		char const versionChar = '0' + version;
+		char const versionChar = '0' + GetVersion();
 		retVal.push_back( versionChar );
+		if( ExposesComputerInformation() )
+		{
+			retVal.push_back( '!' );
+		}
 	}
 	retVal.push_back( ',' );
 	{
-		// 0 - 3 = NCS compat
-		// 4 - 5 = Standard
-		// 6 = MS compat
-		// 7 = Reserved
-		Octet const& variantOctet = mOctetSequence[6];
-		uint8_t const variant = ( variantOctet & 0xe0u ) >> 5;
-		char const variantChar = '0' + variant;
+		char const variantChar = '0' + GetVariant();
 		retVal.push_back( variantChar );
 	}
 	retVal.push_back( ')' );
