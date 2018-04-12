4f72452e4ff2e1ae9dd72320a218fd89a5c65e2b
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-04-11 21:32:00 -0700

    Minor logging

--- libs/PlatformUtils_win32/project.h
+++ libs/PlatformUtils_win32/project.h
@@ -1,7 +1,11 @@
 #pragma once
+#include "Logging/Constants.h"
 
 #ifdef PLATFORMUTILS_EXPORTS
 	#define PLATFORMUTILS_API __declspec(dllexport)
 #else
 	#define PLATFORMUTILS_API __declspec(dllimport)
 #endif
+
+constexpr char const* RFCAT_PLATFORMUTILS = "PlatformUtils";
+constexpr uint64_t RFCAT_PLATFORMUTILS_SEV_WHITELIST = RF::logging::kDefaultSeverityMask;
