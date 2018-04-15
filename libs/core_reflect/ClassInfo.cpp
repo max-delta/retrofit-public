2b3d6fe21ef0501109191a200be8492117f6bac2
Max Delta <Max Delta@localhost>
2018-04-15 14:05:58 -0700

    Added identifiers to class info

---
+++ libs/core_reflect/ClassInfo.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "ClassInfo.h"
+
+namespace RF { namespace reflect {
+///////////////////////////////////////////////////////////////////////////////
+
+char const * ClassInfo::StoreString( char const * string )
+{
+	rftl::string const& storedString = mIdentifierStorage.emplace_back( string );
+	return storedString.data();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+}}
