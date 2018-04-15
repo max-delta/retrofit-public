60588ddca2dc6cff1e22a4750d71b75b4f4f3ae2
Max Delta <Max Delta@localhost>
2018-04-15 15:32:56 -0700

    Added single object serializer

--- libs/RFType/TypeTraverser.h
+++ libs/RFType/TypeTraverser.h
@@ -34,6 +34,7 @@ public:
 public:
 	using OnMemberVariableFunc = void( *)( MemberVariableInstance const& );
 	using OnNestedTypeFoundFunc = void( *)( MemberVariableInstance const&, bool& shouldRecurse );
+	using OnReturnFromNestedTypeFunc = void( *)( );
 
 
 	//
@@ -42,44 +43,50 @@ public:
 	static void TraverseVariables(
 		reflect::VirtualClass const& traversalRoot,
 		OnMemberVariableFunc const& onMemberVariableFunc,
-		OnNestedTypeFoundFunc const& onNestedTypeFoundFunc );
+		OnNestedTypeFoundFunc const& onNestedTypeFoundFunc,
+		OnReturnFromNestedTypeFunc const& onReturnFromNestedTypeFunc );
 
-	template<typename OnMemberVariableFuncT, typename OnNestedTypeFoundFuncT>
+	template<typename OnMemberVariableFuncT, typename OnNestedTypeFoundFuncT, typename OnReturnFromNestedTypeFuncT>
 	static void TraverseVariablesT(
 		reflect::VirtualClass const& traversalRoot,
 		OnMemberVariableFuncT const& onMemberVariableFunc,
-		OnNestedTypeFoundFuncT const& onNestedTypeFoundFunc );
+		OnNestedTypeFoundFuncT const& onNestedTypeFoundFunc,
+		OnReturnFromNestedTypeFuncT const& onReturnFromNestedTypeFunc );
 
 	static void TraverseVariables(
 		reflect::ClassInfo const& classInfo,
 		void const* classLocation,
 		OnMemberVariableFunc const& onMemberVariableFunc,
-		OnNestedTypeFoundFunc const& onNestedTypeFoundFunc );
+		OnNestedTypeFoundFunc const& onNestedTypeFoundFunc,
+		OnReturnFromNestedTypeFunc const& onReturnFromNestedTypeFunc );
 
-	template<typename OnMemberVariableFuncT, typename OnNestedTypeFoundFuncT>
+	template<typename OnMemberVariableFuncT, typename OnNestedTypeFoundFuncT, typename OnReturnFromNestedTypeFuncT>
 	static void TraverseVariablesT(
 		reflect::ClassInfo const& classInfo,
 		void const* classLocation,
 		OnMemberVariableFuncT const& onMemberVariableFunc,
-		OnNestedTypeFoundFuncT const& onNestedTypeFoundFunc );
+		OnNestedTypeFoundFuncT const& onNestedTypeFoundFunc,
+		OnReturnFromNestedTypeFuncT const& onReturnFromNestedTypeFunc );
 
 
 	//
 	// Private methods
 private:
-	template< typename OnMemberVariableFuncT, typename OnNestedTypeFoundFuncT>
+	template< typename OnMemberVariableFuncT, typename OnNestedTypeFoundFuncT, typename OnReturnFromNestedTypeFuncT>
 	static void TraverseVariablesWithInheritanceT(
 		reflect::ClassInfo const& classInfo,
 		void const* classLocation,
 		OnMemberVariableFuncT const& onMemberVariableFunc,
-		OnNestedTypeFoundFuncT const& onNestedTypeFoundFunc );
+		OnNestedTypeFoundFuncT const& onNestedTypeFoundFunc,
+		OnReturnFromNestedTypeFuncT const& onReturnFromNestedTypeFunc );
 
-	template< typename OnMemberVariableFuncT, typename OnNestedTypeFoundFuncT>
+	template< typename OnMemberVariableFuncT, typename OnNestedTypeFoundFuncT, typename OnReturnFromNestedTypeFuncT>
 	static void TraverseVariablesWithoutInheritanceT(
 		reflect::ClassInfo const& classInfo,
 		void const* classLocation,
 		OnMemberVariableFuncT const& onMemberVariableFunc,
-		OnNestedTypeFoundFuncT const& onNestedTypeFoundFunc );
+		OnNestedTypeFoundFuncT const& onNestedTypeFoundFunc,
+		OnReturnFromNestedTypeFuncT const& onReturnFromNestedTypeFunc );
 };
 
 ///////////////////////////////////////////////////////////////////////////////
