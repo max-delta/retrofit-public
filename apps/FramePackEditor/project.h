c264e52bc366015ac1583797bf6aeba799888e14
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-04-21 15:43:35 -0700

    Split FramePackEditor from RetroFit test app

--- apps/FramePackEditor/project.h
+++ apps/FramePackEditor/project.h
@@ -4,5 +4,5 @@
 constexpr char const* RFCAT_STARTUP = "Startup";
 constexpr uint64_t RFCAT_STARTUP_SEV_WHITELIST = RF::logging::kDefaultSeverityMask;
 
-constexpr char const* RFCAT_STARTUPTEST = "StartupTest";
-constexpr uint64_t RFCAT_STARTUPTEST_SEV_WHITELIST = RF::logging::kDefaultSeverityMask;
+constexpr char const* RFCAT_FRAMEPACKEDITOR = "FramePackEditor";
+constexpr uint64_t RFCAT_FRAMEPACKEDITOR_SEV_WHITELIST = RF::logging::kDefaultSeverityMask;
