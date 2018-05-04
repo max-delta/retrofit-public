c903abd753c3b8495890aeac14fe184321e2c615
Max Delta <Max Delta@localhost>
2018-05-03 21:24:13 -0700

    Added threadable task worker

--- libs/Scheduling/project.h
+++ libs/Scheduling/project.h
@@ -1,7 +1,11 @@
 #pragma once
+#include "Logging/Constants.h"
 
 #ifdef SCHEDULING_EXPORTS
 #define SCHEDULING_API __declspec(dllexport)
 #else
 #define SCHEDULING_API __declspec(dllimport)
 #endif
+
+constexpr char const* RFCAT_SCHEDULING = "Scheduling";
+constexpr uint64_t RFCAT_SCHEDULING_SEV_WHITELIST = RF::logging::kDefaultSeverityMask;
