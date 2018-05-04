8d96e3b2841a486b40e1daf8b99845fb9821ed22
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-05-03 21:24:13 -0700

    Added threadable task worker

---
+++ libs/Scheduling/taskworkers/ThreadableTaskWorker.cpp
@@ -0,0 +1,65 @@
+#include "stdafx.h"
+#include "ThreadableTaskWorker.h"
+
+#include "Scheduling/tasks/Task.h"
+#include "Logging/Logging.h"
+
+
+namespace RF { namespace scheduling {
+///////////////////////////////////////////////////////////////////////////////
+
+ThreadableTaskWorker::~ThreadableTaskWorker()
+{
+	bool const lockSuccess = mExecutionMutex.try_lock();
+	if( lockSuccess )
+	{
+		mExecutionMutex.unlock();
+	}
+	else
+	{
+		RFLOG_FATAL( nullptr, RFCAT_SCHEDULING,
+			"Threadable worker being destroyed while holding an execution lock."
+			" Not only is this behavior undefined by the standard, it's also an"
+			" indication that another thread is running code that this"
+			" destruction is about to invalidate" );
+	}
+}
+
+
+
+void ThreadableTaskWorker::AddTask( Task * task, TaskScheduler * scheduler )
+{
+	rftl::unique_lock<rftl::mutex> workLock( mPendingWorkMutex );
+	mPendingWork.emplace_back( WorkItem{ task, scheduler } );
+}
+
+
+
+void ThreadableTaskWorker::ExecuteUntilStarved()
+{
+	rftl::unique_lock<rftl::mutex> execLock( mExecutionMutex );
+
+	while( true )
+	{
+		// Pull work
+		WorkItem workItem;
+		{
+			rftl::unique_lock<rftl::mutex> workLock( mPendingWorkMutex );
+			if( mPendingWork.empty() )
+			{
+				return;
+			}
+			workItem = mPendingWork.back();
+			mPendingWork.pop_back();
+		}
+
+		// Execute
+		{
+			TaskState const newTaskState = workItem.mTask->Step();
+			OnWorkComplete( workItem.mTask, newTaskState, workItem.mScheduler );
+		}
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+}}
---
+++ libs/Scheduling/taskworkers/ThreadableTaskWorker.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "project.h"
+
+#include "Scheduling/taskworkers/TaskWorker.h"
+
+#include "rftl/vector"
+#include "rftl/mutex"
+
+
+namespace RF { namespace scheduling {
+///////////////////////////////////////////////////////////////////////////////
+
+class SCHEDULING_API ThreadableTaskWorker : public TaskWorker
+{
+	RF_NO_COPY( ThreadableTaskWorker );
+
+	//
+	// Structs
+private:
+	struct WorkItem
+	{
+		Task* mTask;
+		TaskScheduler* mScheduler;
+	};
+
+
+	//
+	// Public methods
+public:
+	ThreadableTaskWorker() = default;
+	virtual ~ThreadableTaskWorker();
+
+	virtual void AddTask( Task* task, TaskScheduler* scheduler ) override;
+
+	void ExecuteUntilStarved();
+
+
+	//
+	// Private data
+private:
+	rftl::mutex mPendingWorkMutex;
+	rftl::vector<WorkItem> mPendingWork;
+
+	rftl::mutex mExecutionMutex;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+}}
