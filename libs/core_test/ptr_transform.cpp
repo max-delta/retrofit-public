a6a2d9ad938e4564d8539fb73d213c1118cd90f2
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-05-03 23:00:42 -0700

    Test notes and switches for unsafe behavior

--- libs/core_test/ptr_transform.cpp
+++ libs/core_test/ptr_transform.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 
 #include "core/ptr/ptr_transform.h"
+#include "core/ptr/weak_shared_ptr.h"
 #include "core/ptr/entwined_creator.h"
 
 #include "rftl/cstdlib"
@@ -38,6 +39,13 @@ TEST(UniquePtr, TransformToShared)
 
 TEST( SharedPtr, TransformToUnique )
 {
+	// This is a dangerous transformation, which can be made to fail by using
+	//  these flags to violate the ownership contracts
+	constexpr bool kCauseFailureOnCreate = false;
+	constexpr bool kCauseFailureOnDestroy = false;
+	WeakSharedPtr<int> unsafe_wsptr;
+	SharedPtr<int> unsafe_sptr;
+
 	WeakPtr<int> wptr;
 	ASSERT_TRUE( wptr == nullptr );
 	{
@@ -49,10 +57,28 @@ TEST( SharedPtr, TransformToUnique )
 			wptr = sptr;
 			ASSERT_TRUE( wptr != nullptr );
 			{
+				if( kCauseFailureOnCreate )
+				{
+					// This creates a shared scenario on create
+					// NOTE: This is detectable
+					unsafe_sptr = sptr;
+				}
+				if( kCauseFailureOnDestroy )
+				{
+					// This creates a lurking danger that is hidden on create
+					// NOTE: This is not detectable at time of writing
+					unsafe_wsptr = sptr;
+				}
 				PtrTransformer<int>::PerformUncheckedTransformation( rftl::move( sptr ), uptr );
 				ASSERT_TRUE( sptr == nullptr );
 				ASSERT_TRUE( uptr != nullptr );
 				ASSERT_TRUE( wptr != nullptr );
+				if( kCauseFailureOnDestroy )
+				{
+					// This creates a shared scenario on destroy
+					// NOTE: This is detectable
+					unsafe_sptr = unsafe_wsptr.Lock();
+				}
 			}
 		}
 		ASSERT_TRUE( uptr != nullptr );
