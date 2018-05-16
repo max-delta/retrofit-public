bd00c126137ab1858e9a95019230541174143f9e
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-05-15 23:31:16 -0700

    Shame the static analysis is so broken right now, made some more notes about it :/

--- config/StaticAnalysis.h
+++ config/StaticAnalysis.h
@@ -1,2 +1,8 @@
 #include <CppCoreCheck/warnings.h>
+
+// In general, these warnings are full of trash, and even selective
+//  whitelisting isn't viable due to bugs relating to static analysis and
+//  the (at time of writing) experimental third-party warning disabling
+//  feature in MSVC
+// TODO: Review once these features are less experimental
 #pragma warning(disable:ALL_CPPCORECHECK_WARNINGS)
