1c0d0026b01dc829c7d892539caa51d529e1d0d2
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-05-27 13:45:32 -0700

    Moved old squirrelt test to actual test case

--- libs/foundation_test/scripting_squirrel.cpp
+++ libs/foundation_test/scripting_squirrel.cpp
@@ -11,5 +11,41 @@ TEST( Squirrel, EmptyVM )
 	SquirrelVM vm;
 }
 
+
+
+TEST( Squirrel, LegacyTest )
+{
+	SquirrelVM vm;
+	constexpr char source[] =
+		"x <- 5;"
+		"y <- 7.0;"
+		"z <- true;"
+		"s <- \"STRING\";"
+		"n <- null;"
+		"a <- [\"first\", \"second\"];"
+		"\n";
+	bool const sourceAdd = vm.AddSourceFromBuffer( source );
+	ASSERT_TRUE( sourceAdd );
+
+	SquirrelVM::Element xElem = vm.GetGlobalVariable( "x" );
+	ASSERT_TRUE( rftl::get_if<SquirrelVM::Integer>( &xElem ) != nullptr );
+	SquirrelVM::Element yElem = vm.GetGlobalVariable( "y" );
+	ASSERT_TRUE( rftl::get_if<SquirrelVM::FloatingPoint>( &yElem ) != nullptr );
+	SquirrelVM::Element zElem = vm.GetGlobalVariable( "z" );
+	ASSERT_TRUE( rftl::get_if<SquirrelVM::Boolean>( &zElem ) != nullptr );
+	SquirrelVM::Element sElem = vm.GetGlobalVariable( "s" );
+	ASSERT_TRUE( rftl::get_if<SquirrelVM::String>( &sElem ) != nullptr );
+	SquirrelVM::Element nElem = vm.GetGlobalVariable( "n" );
+	ASSERT_TRUE( rftl::get_if<SquirrelVM::Null>( &nElem ) != nullptr );
+	SquirrelVM::Element aElem = vm.GetGlobalVariable( "a" );
+	ASSERT_TRUE( rftl::get_if<SquirrelVM::ArrayTag>( &aElem ) != nullptr );
+	SquirrelVM::ElementArray aElemArr = vm.GetGlobalVariableAsArray( "a" );
+	{
+		ASSERT_TRUE( aElemArr.size() == 2 );
+		ASSERT_TRUE( rftl::get_if<SquirrelVM::String>( &aElemArr[0] ) != nullptr );
+		ASSERT_TRUE( rftl::get_if<SquirrelVM::String>( &aElemArr[1] ) != nullptr );
+	}
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 }}
