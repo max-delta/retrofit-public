77a54abb55485c340f1fa9223af96daf3b88ec55
Max Delta <Max Delta@localhost>
2018-05-27 15:32:28 -0700

    Fixed oversight which could lead to bugs, added more explicit checks to legacy test

--- libs/foundation_test/scripting_squirrel.cpp
+++ libs/foundation_test/scripting_squirrel.cpp
@@ -30,24 +30,31 @@ TEST( Squirrel, LegacyTest )
 	SquirrelVM::Element xElem = vm.GetGlobalVariable( "x" );
 	ASSERT_TRUE( rftl::get_if<reflect::Value>( &xElem ) != nullptr );
 	ASSERT_TRUE( rftl::get_if<reflect::Value>( &xElem )->GetAs<SquirrelVM::Integer>() != nullptr );
+	ASSERT_TRUE( *rftl::get_if<reflect::Value>( &xElem )->GetAs<SquirrelVM::Integer>() == 5 );
 	SquirrelVM::Element yElem = vm.GetGlobalVariable( "y" );
 	ASSERT_TRUE( rftl::get_if<reflect::Value>( &yElem ) != nullptr );
 	ASSERT_TRUE( rftl::get_if<reflect::Value>( &yElem )->GetAs<SquirrelVM::FloatingPoint>() != nullptr );
+	ASSERT_TRUE( *rftl::get_if<reflect::Value>( &yElem )->GetAs<SquirrelVM::FloatingPoint>() == 7.f );
 	SquirrelVM::Element zElem = vm.GetGlobalVariable( "z" );
 	ASSERT_TRUE( rftl::get_if<reflect::Value>( &zElem ) != nullptr );
 	ASSERT_TRUE( rftl::get_if<reflect::Value>( &zElem )->GetAs<SquirrelVM::Boolean>() != nullptr );
+	ASSERT_TRUE( *rftl::get_if<reflect::Value>( &zElem )->GetAs<SquirrelVM::Boolean>() == true );
 	SquirrelVM::Element sElem = vm.GetGlobalVariable( "s" );
 	ASSERT_TRUE( rftl::get_if<SquirrelVM::String>( &sElem ) != nullptr );
+	ASSERT_TRUE( *rftl::get_if<SquirrelVM::String>( &sElem ) == "STRING" );
 	SquirrelVM::Element nElem = vm.GetGlobalVariable( "n" );
 	ASSERT_TRUE( rftl::get_if<reflect::Value>( &nElem ) != nullptr );
 	ASSERT_TRUE( rftl::get_if<reflect::Value>( &nElem )->GetAs<SquirrelVM::Pointer>() != nullptr );
+	ASSERT_TRUE( *rftl::get_if<reflect::Value>( &nElem )->GetAs<SquirrelVM::Pointer>() == nullptr );
 	SquirrelVM::Element aElem = vm.GetGlobalVariable( "a" );
 	ASSERT_TRUE( rftl::get_if<SquirrelVM::ArrayTag>( &aElem ) != nullptr );
 	SquirrelVM::ElementArray aElemArr = vm.GetGlobalVariableAsArray( "a" );
 	{
 		ASSERT_TRUE( aElemArr.size() == 2 );
 		ASSERT_TRUE( rftl::get_if<SquirrelVM::String>( &aElemArr[0] ) != nullptr );
+		ASSERT_TRUE( *rftl::get_if<SquirrelVM::String>( &aElemArr[0] ) == "first" );
 		ASSERT_TRUE( rftl::get_if<SquirrelVM::String>( &aElemArr[1] ) != nullptr );
+		ASSERT_TRUE( *rftl::get_if<SquirrelVM::String>( &aElemArr[1] ) == "second" );
 	}
 }
 
