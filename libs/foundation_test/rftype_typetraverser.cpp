8383e4811880cfa5c0ca04a5aab5f39e68c1e695
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-05-28 19:30:28 -0700

    Added tests to confirm type traverser can work on types only, without instances

--- libs/foundation_test/rftype_typetraverser.cpp
+++ libs/foundation_test/rftype_typetraverser.cpp
@@ -64,6 +64,45 @@ TEST( RFType, TraverseSingleInheritance )
 
 
 
+TEST( RFType, TraverseNullSingleInheritance )
+{
+	rftl::vector<TypeTraverser::MemberVariableInstance> members;
+	auto onMemberVariableFunc = [&members](
+		TypeTraverser::MemberVariableInstance const& varInst ) ->
+		void
+	{
+		members.emplace_back( varInst );
+	};
+
+	rftl::vector<TypeTraverser::MemberVariableInstance> nested;
+	auto onNestedTypeFoundFunc = [&nested](
+		TypeTraverser::MemberVariableInstance const& varInst,
+		bool& shouldRecurse ) ->
+		void
+	{
+		nested.emplace_back( varInst );
+		shouldRecurse = true;
+	};
+
+	auto onReturnFromNestedTypeFunc = [](
+		void ) ->
+		void
+	{
+		//
+	};
+
+	TypeTraverser::TraverseVariablesT(
+		GetClassInfo<rftype_example::ExampleDerivedClass>(),
+		nullptr,
+		onMemberVariableFunc,
+		onNestedTypeFoundFunc,
+		onReturnFromNestedTypeFunc );
+	ASSERT_EQ( members.size(), 2 );
+	ASSERT_EQ( nested.size(), 0 );
+}
+
+
+
 TEST( RFType, TraverseMultipleInheritance )
 {
 	rftl::vector<TypeTraverser::MemberVariableInstance> members;
@@ -116,6 +155,45 @@ TEST( RFType, TraverseMultipleInheritance )
 
 
 
+TEST( RFType, TraverseNullMultipleInheritance )
+{
+	rftl::vector<TypeTraverser::MemberVariableInstance> members;
+	auto onMemberVariableFunc = [&members](
+		TypeTraverser::MemberVariableInstance const& varInst ) ->
+		void
+	{
+		members.emplace_back( varInst );
+	};
+
+	rftl::vector<TypeTraverser::MemberVariableInstance> nested;
+	auto onNestedTypeFoundFunc = [&nested](
+		TypeTraverser::MemberVariableInstance const& varInst,
+		bool& shouldRecurse ) ->
+		void
+	{
+		nested.emplace_back( varInst );
+		shouldRecurse = true;
+	};
+
+	auto onReturnFromNestedTypeFunc = [](
+		void ) ->
+		void
+	{
+		//
+	};
+
+	TypeTraverser::TraverseVariablesT(
+		GetClassInfo<rftype_example::ExamplePoorLifeDecision>(),
+		nullptr,
+		onMemberVariableFunc,
+		onNestedTypeFoundFunc,
+		onReturnFromNestedTypeFunc );
+	ASSERT_EQ( members.size(), 2 );
+	ASSERT_EQ( nested.size(), 0 );
+}
+
+
+
 TEST( RFType, TraverseNesting )
 {
 	rftl::vector<TypeTraverser::MemberVariableInstance> members;
@@ -163,6 +241,45 @@ TEST( RFType, TraverseNesting )
 		expectedLocations.erase( varLoc );
 	}
 	ASSERT_EQ( expectedLocations.size(), 0 );
+}
+
+
+
+TEST( RFType, TraverseNullNesting )
+{
+	rftl::vector<TypeTraverser::MemberVariableInstance> members;
+	auto onMemberVariableFunc = [&members](
+		TypeTraverser::MemberVariableInstance const& varInst ) ->
+		void
+	{
+		members.emplace_back( varInst );
+	};
+
+	rftl::vector<TypeTraverser::MemberVariableInstance> nested;
+	auto onNestedTypeFoundFunc = [&nested](
+		TypeTraverser::MemberVariableInstance const& varInst,
+		bool& shouldRecurse ) ->
+		void
+	{
+		nested.emplace_back( varInst );
+		shouldRecurse = true;
+	};
+
+	auto onReturnFromNestedTypeFunc = [](
+		void ) ->
+		void
+	{
+		//
+	};
+
+	TypeTraverser::TraverseVariablesT(
+		GetClassInfo<rftype_example::ExampleWithClassAsMember>(),
+		nullptr,
+		onMemberVariableFunc,
+		onNestedTypeFoundFunc,
+		onReturnFromNestedTypeFunc );
+	ASSERT_EQ( members.size(), 1 );
+	ASSERT_EQ( nested.size(), 1 );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
