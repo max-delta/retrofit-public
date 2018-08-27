d243c611d52cf377174cbafc38d3b2ac95f694c8
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-08-26 17:23:19 -0700

    Clang cleanup

--- libs/core_platform/win_shim.h
+++ libs/core_platform/win_shim.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "core/compiler.h"
+
+#include "rftl/cstdint"
+
 namespace shim
 {
 //WinDef
@@ -17,16 +21,32 @@ typedef unsigned int        UINT;
 typedef unsigned int        *PUINT;
 typedef wchar_t WCHAR;
 typedef const WCHAR *LPCWSTR;
-#if defined(_WIN64)
-typedef __int64 INT_PTR, *PINT_PTR;
-typedef unsigned __int64 UINT_PTR, *PUINT_PTR;
-typedef __int64 LONG_PTR, *PLONG_PTR;
-typedef unsigned __int64 ULONG_PTR, *PULONG_PTR;
+#ifdef RF_PLATFORM_MSVC
+	#if defined(_WIN64)
+		typedef __int64 INT_PTR, *PINT_PTR;
+		typedef unsigned __int64 UINT_PTR, *PUINT_PTR;
+		typedef __int64 LONG_PTR, *PLONG_PTR;
+		typedef unsigned __int64 ULONG_PTR, *PULONG_PTR;
+	#else
+		typedef int INT_PTR, *PINT_PTR;
+		typedef unsigned int UINT_PTR, *PUINT_PTR;
+		typedef long LONG_PTR, *PLONG_PTR;
+		typedef unsigned long ULONG_PTR, *PULONG_PTR;
+	#endif
 #else
-typedef int INT_PTR, *PINT_PTR;
-typedef unsigned int UINT_PTR, *PUINT_PTR;
-typedef long LONG_PTR, *PLONG_PTR;
-typedef unsigned long ULONG_PTR, *PULONG_PTR;
+	#if RF_PLATFORM_POINTER_BYTES == 8
+		typedef int64_t INT_PTR, *PINT_PTR;
+		typedef uint64_t UINT_PTR, *PUINT_PTR;
+		typedef int64_t LONG_PTR, *PLONG_PTR;
+		typedef uint64_t ULONG_PTR, *PULONG_PTR;
+	#elif RF_PLATFORM_POINTER_BYTES == 4
+		typedef int32_t INT_PTR, *PINT_PTR;
+		typedef uint32_t UINT_PTR, *PUINT_PTR;
+		typedef int32_t LONG_PTR, *PLONG_PTR;
+		typedef uint32_t ULONG_PTR, *PULONG_PTR;
+	#else
+		#error Unsupported platform
+	#endif
 #endif
 typedef UINT_PTR            WPARAM;
 typedef LONG_PTR            LPARAM;
