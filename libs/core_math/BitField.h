08531d1b02d6c4c33a31b8b45114f37be2d54b85
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-08-29 20:38:50 -0700

    Minor bitfield cleanup

--- libs/core_math/BitField.h
+++ libs/core_math/BitField.h
@@ -1,20 +1,23 @@
 #pragma once
-#include "core/compiler.h"
 #include "core/meta/ValueList.h"
 #include "core_math/ListOperations.h"
 #include "core_math/math_clamps.h"
-#include "core_math/math_bits.h"
-#include "core_math/math_bytes.h"
 
 
 namespace RF { namespace math {
 ///////////////////////////////////////////////////////////////////////////////
 
+// Compressors compress/decompress different types in a bit-oriented manner
+// NOTE: See the default compressor handling integral types for an example of
+//  how to implement a compressor for a more complex custom type
 template<typename AccessTypeT, size_t storageSizeBits, size_t storageOffsetBits>
 struct Compressor;
 
 
 
+// Bitfields store data in bits in a platform-independent way with consistent
+//  endianness and padding behaviors, in contrast to the platform-dependent
+//  bitfield language feature specified in the C++ standard
 template<typename AccessTypeT, size_t... fieldSizes>
 class BitField
 {
@@ -55,257 +58,6 @@ private:
 };
 
 ///////////////////////////////////////////////////////////////////////////////
-
-template<typename AccessTypeT, size_t ...fieldSizes>
-inline void const * BitField<AccessTypeT, fieldSizes...>::Data() const
-{
-	return &mStorage[0];
-}
-
-
-
-template<typename AccessTypeT, size_t ...fieldSizes>
-template<size_t index>
-inline typename BitField<AccessTypeT, fieldSizes...>::AccessType BitField<AccessTypeT, fieldSizes...>::ReadAt() const
-{
-	constexpr size_t kBytesBeforeIndex = GetBytesBeforeIndex<index>();
-	constexpr size_t kBitsOffsetIntoIndex = GetBitsOffsetIntoIndex<index>();
-	constexpr size_t kBitsAtIndex = GetBitsAtIndex<index>();
-	static_assert( sizeof( AccessType ) * 8 >= kBitsAtIndex, "Field has more bits than access type" );
-
-	void const* const source = reinterpret_cast<uint8_t const*>( &mStorage ) + kBytesBeforeIndex;
-	return Compressor<AccessType, kBitsAtIndex, kBitsOffsetIntoIndex>::Decompress( source );
-}
-
-
-
-template<typename AccessTypeT, size_t ...fieldSizes>
-template<size_t index>
-inline void BitField<AccessTypeT, fieldSizes...>::WriteAt( AccessType const & value )
-{
-	constexpr size_t kBytesBeforeIndex = GetBytesBeforeIndex<index>();
-	constexpr size_t kBitsOffsetIntoIndex = GetBitsOffsetIntoIndex<index>();
-	constexpr size_t kBitsAtIndex = GetBitsAtIndex<index>();
-	static_assert( sizeof( AccessType ) * 8 >= kBitsAtIndex, "Field has more bits than access type" );
-
-	void* const destination = reinterpret_cast<uint8_t*>( &mStorage ) + kBytesBeforeIndex;
-	Compressor<AccessType, kBitsAtIndex, kBitsOffsetIntoIndex>::Compress( value, destination );
-}
-
-
-
-template<typename AccessTypeT, size_t ...fieldSizes>
-template<size_t index>
-inline constexpr size_t BitField<AccessTypeT, fieldSizes...>::GetBytesBeforeIndex()
-{
-	constexpr size_t kBitsBeforeIndex = ListSum<size_t, FieldsBeforeIndex<index>>::value;
-	constexpr size_t kBytesBeforeIndex = kBitsBeforeIndex / 8;
-	return kBytesBeforeIndex;
-}
-
-
-
-template<typename AccessTypeT, size_t ...fieldSizes>
-template<size_t index>
-inline constexpr size_t BitField<AccessTypeT, fieldSizes...>::GetBitsOffsetIntoIndex()
-{
-	constexpr size_t kBitsBeforeIndex = ListSum<size_t, FieldsBeforeIndex<index>>::value;
-	constexpr size_t kBytesBeforeIndex = kBitsBeforeIndex / 8;
-	constexpr size_t kBitsOffsetIntoIndex = kBitsBeforeIndex - ( kBytesBeforeIndex * 8 );
-	return kBitsOffsetIntoIndex;
-}
-
-
-
-template<typename AccessTypeT, size_t ...fieldSizes>
-template<size_t index>
-inline constexpr size_t BitField<AccessTypeT, fieldSizes...>::GetBitsAtIndex()
-{
-	static_assert( FieldsAtOrAfterIndex<index>::kNumValues > 0, "Index past number of fields" );
-	using FirstFieldAfterIndex = typename FieldsAtOrAfterIndex<index>::template ByIndex<0>;
-	constexpr size_t kBitsAtIndex = FirstFieldAfterIndex::value;
-	return kBitsAtIndex;
-}
-
-
-
-template<typename AccessTypeT, size_t storageOffsetBits>
-struct Compressor<AccessTypeT, 0, storageOffsetBits>
-{
-	using AccessType = AccessTypeT;
-
-	static void Compress( AccessType const&, void* )
-	{
-		// No bits, no operation
-	}
-	static AccessType Decompress( void const* )
-	{
-		// No bits, no operation
-		return AccessType{};
-	}
-};
-
-
-
-template<typename AccessTypeT, size_t storageSizeBits, size_t storageOffsetBits>
-struct Compressor
-{
-	using AccessType = AccessTypeT;
-	static_assert( sizeof( AccessType ) * 8 >= storageSizeBits, "Storage has more bits than access type" );
-
-	static void Compress( AccessType const& source, void* destination )
-	{
-		CompressImpl<AccessType>( source, destination );
-	}
-	static AccessType Decompress( void const* source )
-	{
-		return DecompressImpl<AccessType>( source );
-	}
-
-	// Store bits as big-endian, writing from right-to-left
-	template<typename AccessType,
-		typename rftl::enable_if<rftl::is_integral<AccessType>::value, int>::type = 0>
-	static void CompressImpl( AccessType const& source, void* destination )
-	{
-		#if defined(RF_PLATFORM_LITTLE_ENDIAN)
-		AccessType const& sourceValue = source;
-		#elif defined(RF_PLATFORM_BIG_ENDIAN)
-		AccessType const sourceValue = ReverseByteOrder( source );
-		#endif
-
-		constexpr size_t kBytesNeededingModification = SnapHighest<size_t>( storageSizeBits, 8 ) / 8;
-		constexpr int64_t kLeftMostByte = 0;
-		constexpr int64_t kRightmostByte = kBytesNeededingModification - 1;
-
-		constexpr size_t kExcessBytesToTrim = ( sizeof( AccessType ) * 8 ) - storageSizeBits;
-		constexpr AccessType kIncomingSizeMask = GetAllBitsSet<AccessType>() >> kExcessBytesToTrim;
-		AccessType incomingBitsFromRight = sourceValue & kIncomingSizeMask;
-		size_t bitsLeft = storageSizeBits;
-		for( int64_t byteOffset = kRightmostByte; byteOffset >= kLeftMostByte; byteOffset-- )
-		{
-			uint8_t& storageByte = *( reinterpret_cast<uint8_t*>( destination ) + byteOffset );
-			if( byteOffset == kLeftMostByte )
-			{
-				RF_ASSERT( bitsLeft <= 8 );
-				size_t const bitsInByte = bitsLeft;
-				size_t const leftShiftToMatchSize = 8 - bitsInByte;
-				constexpr size_t rightShiftToMatchOffset = storageOffsetBits;
-				uint8_t const incomingBits = static_cast<uint8_t>( static_cast<uint8_t>( static_cast<uint8_t>( incomingBitsFromRight ) << leftShiftToMatchSize ) >> rightShiftToMatchOffset );
-				uint8_t const incomingMask = static_cast<uint8_t>( static_cast<uint8_t>( GetAllBitsSet<uint8_t>() << leftShiftToMatchSize ) >> rightShiftToMatchOffset );
-				uint8_t sharedBits = storageByte; // Load
-				sharedBits &= ~incomingMask; // Clear
-				sharedBits |= incomingBits; // Set
-				storageByte = sharedBits; // Store
-				incomingBitsFromRight >>= bitsInByte;
-				bitsLeft -= bitsInByte;
-				RF_ASSERT( bitsLeft == 0 );
-			}
-			else if( byteOffset == kRightmostByte )
-			{
-				RF_ASSERT( bitsLeft > 0 );
-				RF_ASSERT( bitsLeft <= storageSizeBits );
-				constexpr size_t bitsInByteMod = ( storageSizeBits + storageOffsetBits ) % 8;
-				constexpr size_t bitsInByte = bitsInByteMod == 0 ? 8 : bitsInByteMod;
-				constexpr size_t leftShiftToMatchSize = 8 - bitsInByte;
-				constexpr size_t rightShiftToMatchOffset = 0;
-				uint8_t const incomingBits = static_cast<uint8_t>( static_cast<uint8_t>( static_cast<uint8_t>( incomingBitsFromRight ) << leftShiftToMatchSize ) >> rightShiftToMatchOffset );
-				constexpr uint8_t incomingMask = static_cast<uint8_t>( static_cast<uint8_t>( GetAllBitsSet<uint8_t>() << leftShiftToMatchSize ) >> rightShiftToMatchOffset );
-				uint8_t sharedBits = storageByte; // Load
-				sharedBits &= ~incomingMask; // Clear
-				sharedBits |= incomingBits; // Set
-				storageByte = sharedBits; // Store
-				incomingBitsFromRight >>= bitsInByte;
-				bitsLeft -= bitsInByte;
-				RF_ASSERT( bitsLeft > 0 );
-				RF_ASSERT( bitsLeft <= storageSizeBits );
-			}
-			else
-			{
-				RF_ASSERT( bitsLeft > 0 );
-				RF_ASSERT( bitsLeft <= storageSizeBits );
-				constexpr size_t bitsInByte = 8;
-				size_t const leftShiftToMatchSize = 8 - bitsInByte;
-				constexpr size_t rightShiftToMatchOffset = 0;
-				uint8_t const incomingBits = static_cast<uint8_t>( static_cast<uint8_t>( static_cast<uint8_t>( incomingBitsFromRight ) << leftShiftToMatchSize ) >> rightShiftToMatchOffset );
-				uint8_t const incomingMask = ( static_cast<uint8_t>( 0xffu ) << leftShiftToMatchSize ) >> rightShiftToMatchOffset;
-				uint8_t sharedBits = storageByte; // Load
-				sharedBits &= ~incomingMask; // Clear
-				sharedBits |= incomingBits; // Set
-				storageByte = sharedBits; // Store
-				incomingBitsFromRight >>= bitsInByte;
-				bitsLeft -= bitsInByte;
-				RF_ASSERT( bitsLeft > 0 );
-				RF_ASSERT( bitsLeft <= storageSizeBits );
-			}
-		}
-	}
-
-	// Load bits as big-endian, reading from right-to-left
-	template<typename AccessType,
-		typename rftl::enable_if<rftl::is_integral<AccessType>::value, int>::type = 0>
-	static AccessType DecompressImpl( void const* source )
-	{
-		constexpr size_t kBytesNeededingReads = SnapHighest<size_t>( storageSizeBits, 8 ) / 8;
-		constexpr int64_t kLeftMostByte = 0;
-		constexpr int64_t kRightmostByte = kBytesNeededingReads - 1;
-
-		AccessType destination = 0;
-
-		size_t bitsLeft = storageSizeBits;
-		for( int64_t byteOffset = kLeftMostByte; byteOffset <= kRightmostByte; byteOffset++ )
-		{
-			uint8_t const& storageByte = *( reinterpret_cast<uint8_t const*>( source ) + byteOffset );
-			if( byteOffset == kLeftMostByte )
-			{
-				RF_ASSERT( bitsLeft > 0 );
-				RF_ASSERT( bitsLeft <= storageSizeBits );
-				constexpr size_t bitsInByte = math::Min<size_t>( storageSizeBits, 8 - storageOffsetBits );
-				constexpr size_t rightShiftToMatchOffset = (8 - bitsInByte) - storageOffsetBits;
-				constexpr size_t leftShiftToMatchSize = bitsInByte;
-				constexpr AccessType outgoingMask = ~static_cast<AccessType>( GetAllBitsSet<AccessType>() << leftShiftToMatchSize );
-				uint8_t const outgoingBits = static_cast<uint8_t>( static_cast<uint8_t>( static_cast<uint8_t>( storageByte ) >> rightShiftToMatchOffset ) & outgoingMask );
-				destination <<= leftShiftToMatchSize; // Shift
-				destination &= ~outgoingMask; // Clear
-				destination |= outgoingBits; // Set
-				bitsLeft -= bitsInByte;
-				RF_ASSERT( bitsLeft >= 0 );
-				RF_ASSERT( bitsLeft <= storageSizeBits );
-			}
-			else if( byteOffset == kRightmostByte )
-			{
-				RF_ASSERT( bitsLeft <= 8 );
-				size_t const bitsInByte = bitsLeft;
-				size_t const rightShiftToMatchOffset = 8 - bitsInByte;
-				size_t const leftShiftToMatchSize = bitsInByte;
-				AccessType const outgoingMask = ~static_cast<AccessType>( GetAllBitsSet<AccessType>() << leftShiftToMatchSize );
-				uint8_t const outgoingBits = static_cast<uint8_t>( static_cast<uint8_t>( static_cast<uint8_t>( storageByte ) >> rightShiftToMatchOffset ) & outgoingMask );
-				destination <<= leftShiftToMatchSize; // Shift
-				destination &= ~outgoingMask; // Clear
-				destination |= outgoingBits; // Set
-				bitsLeft -= bitsInByte;
-				RF_ASSERT( bitsLeft == 0 );
-			}
-			else
-			{
-				RF_ASSERT( bitsLeft > 0 );
-				RF_ASSERT( bitsLeft <= storageSizeBits );
-				constexpr size_t bitsInByte = 8;
-				constexpr size_t rightShiftToMatchOffset = 0;
-				constexpr size_t leftShiftToMatchSize = bitsInByte;
-				constexpr AccessType outgoingMask = ~static_cast<AccessType>( GetAllBitsSet<AccessType>() << leftShiftToMatchSize );
-				uint8_t const outgoingBits = static_cast<uint8_t>( static_cast<uint8_t>( static_cast<uint8_t>( storageByte ) >> rightShiftToMatchOffset ) & outgoingMask );
-				destination <<= leftShiftToMatchSize; // Shift
-				destination &= ~outgoingMask; // Clear
-				destination |= outgoingBits; // Set
-				bitsLeft -= bitsInByte;
-				RF_ASSERT( bitsLeft > 0 );
-				RF_ASSERT( bitsLeft <= storageSizeBits );
-			}
-		}
-
-		return destination;
-	}
-};
-
-///////////////////////////////////////////////////////////////////////////////
 }}
+
+#include "BitField.inl"
