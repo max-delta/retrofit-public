4840e42c8b1a6f26ce75e8a4ee386828ea4fdbbd
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-11-11 15:51:20 -0800

    Logging stack-based conversion and Unicode api fixes

--- libs/core_unicode/InsertConvert.h
+++ libs/core_unicode/InsertConvert.h
@@ -4,24 +4,43 @@
 namespace RF { namespace unicode {
 ///////////////////////////////////////////////////////////////////////////////
 
-template<typename InserterT> void ConvertSingleUtf32ToUtf8( char32_t codePoint, InserterT insertIterator );
-template<typename InserterT> void ConvertSingleUtf32ToUtf16( char32_t codePoint, InserterT insertIterator );
+template<typename InserterT> void ConvertSingleUtf32ToUtf8( char32_t codePoint, InserterT& insertIterator );
+template<typename InserterT> void ConvertSingleUtf32ToUtf16( char32_t codePoint, InserterT& insertIterator );
 
-template<typename InserterT> void ConvertToASCII( char const* source, size_t numBytes, InserterT insertIterator );
-template<typename InserterT> void ConvertToASCII( char16_t const* source, size_t numPairs, InserterT insertIterator );
-template<typename InserterT> void ConvertToASCII( char32_t const* source, size_t numCodePoints, InserterT insertIterator );
+template<typename InserterT> void ConvertToASCII( char const* source, size_t numBytes, InserterT& insertIterator );
+template<typename InserterT> void ConvertToASCII( char16_t const* source, size_t numPairs, InserterT& insertIterator );
+template<typename InserterT> void ConvertToASCII( char32_t const* source, size_t numCodePoints, InserterT& insertIterator );
 
-template<typename InserterT> void ConvertToUtf8( char const* source, size_t numBytes, InserterT insertIterator );
-template<typename InserterT> void ConvertToUtf8( char16_t const* source, size_t numPairs, InserterT insertIterator );
-template<typename InserterT> void ConvertToUtf8( char32_t const* source, size_t numCodePoints, InserterT insertIterator );
+template<typename InserterT> void ConvertToUtf8( char const* source, size_t numBytes, InserterT& insertIterator );
+template<typename InserterT> void ConvertToUtf8( char16_t const* source, size_t numPairs, InserterT& insertIterator );
+template<typename InserterT> void ConvertToUtf8( char32_t const* source, size_t numCodePoints, InserterT& insertIterator );
 
-template<typename InserterT> void ConvertToUtf16( char const* source, size_t numBytes, InserterT insertIterator );
-template<typename InserterT> void ConvertToUtf16( char16_t const* source, size_t numPairs, InserterT insertIterator );
-template<typename InserterT> void ConvertToUtf16( char32_t const* source, size_t numCodePoints, InserterT insertIterator );
+template<typename InserterT> void ConvertToUtf16( char const* source, size_t numBytes, InserterT& insertIterator );
+template<typename InserterT> void ConvertToUtf16( char16_t const* source, size_t numPairs, InserterT& insertIterator );
+template<typename InserterT> void ConvertToUtf16( char32_t const* source, size_t numCodePoints, InserterT& insertIterator );
 
-template<typename InserterT> void ConvertToUtf32( char const* source, size_t numBytes, InserterT insertIterator );
-template<typename InserterT> void ConvertToUtf32( char16_t const* source, size_t numPairs, InserterT insertIterator );
-template<typename InserterT> void ConvertToUtf32( char32_t const* source, size_t numCodePoints, InserterT insertIterator );
+template<typename InserterT> void ConvertToUtf32( char const* source, size_t numBytes, InserterT& insertIterator );
+template<typename InserterT> void ConvertToUtf32( char16_t const* source, size_t numPairs, InserterT& insertIterator );
+template<typename InserterT> void ConvertToUtf32( char32_t const* source, size_t numCodePoints, InserterT& insertIterator );
+
+template<typename InserterT> void ConvertSingleUtf32ToUtf8( char32_t codePoint, InserterT&& insertIterator );
+template<typename InserterT> void ConvertSingleUtf32ToUtf16( char32_t codePoint, InserterT&& insertIterator );
+
+template<typename InserterT> void ConvertToASCII( char const* source, size_t numBytes, InserterT&& insertIterator );
+template<typename InserterT> void ConvertToASCII( char16_t const* source, size_t numPairs, InserterT&& insertIterator );
+template<typename InserterT> void ConvertToASCII( char32_t const* source, size_t numCodePoints, InserterT&& insertIterator );
+
+template<typename InserterT> void ConvertToUtf8( char const* source, size_t numBytes, InserterT&& insertIterator );
+template<typename InserterT> void ConvertToUtf8( char16_t const* source, size_t numPairs, InserterT&& insertIterator );
+template<typename InserterT> void ConvertToUtf8( char32_t const* source, size_t numCodePoints, InserterT&& insertIterator );
+
+template<typename InserterT> void ConvertToUtf16( char const* source, size_t numBytes, InserterT&& insertIterator );
+template<typename InserterT> void ConvertToUtf16( char16_t const* source, size_t numPairs, InserterT&& insertIterator );
+template<typename InserterT> void ConvertToUtf16( char32_t const* source, size_t numCodePoints, InserterT&& insertIterator );
+
+template<typename InserterT> void ConvertToUtf32( char const* source, size_t numBytes, InserterT&& insertIterator );
+template<typename InserterT> void ConvertToUtf32( char16_t const* source, size_t numPairs, InserterT&& insertIterator );
+template<typename InserterT> void ConvertToUtf32( char32_t const* source, size_t numCodePoints, InserterT&& insertIterator );
 
 ///////////////////////////////////////////////////////////////////////////////
 }}
