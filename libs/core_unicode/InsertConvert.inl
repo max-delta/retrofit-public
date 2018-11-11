94668bcb80e3e9227af520e8566b276ee34c1751
Max Delta <54338561+max-delta@users.noreply.github.com>
2018-11-11 14:15:45 -0800

    Added insert conversion for Unicode

---
+++ libs/core_unicode/InsertConvert.inl
@@ -0,0 +1,305 @@
+#include "stdafx.h"
+#include "InsertConvert.h"
+#include "CharConvert.h"
+
+#include "core/macros.h"
+
+#include "rftl/iterator"
+
+
+namespace RF { namespace unicode {
+///////////////////////////////////////////////////////////////////////////////
+namespace details {
+
+template<typename CharT, typename InserterT>
+void CollapseToAscii( CharT const* source, size_t numElements, InserterT insertIterator )
+{
+	rftl::string retVal;
+	retVal.reserve( numElements );
+	for( size_t i = 0; i < numElements; i++ )
+	{
+		CharT const& in = source[i];
+
+		if( in >= 0 && in <= 127 )
+		{
+			insertIterator = static_cast<char>( in );
+		}
+		else
+		{
+			insertIterator = '?';
+		}
+	}
+}
+
+}
+///////////////////////////////////////////////////////////////////////////////
+
+template<typename InserterT> void ConvertSingleUtf32ToUtf8( char32_t codePoint, InserterT insertIterator )
+{
+	char temp[4] = {};
+	size_t const numBytes = ConvertSingleUtf32ToUtf8( codePoint, temp );
+	switch( numBytes )
+	{
+		case 1:
+			insertIterator = temp[0];
+			break;
+		case 2:
+			insertIterator = temp[0];
+			insertIterator = temp[1];
+			break;
+		case 3:
+			insertIterator = temp[0];
+			insertIterator = temp[1];
+			insertIterator = temp[2];
+			break;
+		case 4:
+			insertIterator = temp[0];
+			insertIterator = temp[1];
+			insertIterator = temp[2];
+			insertIterator = temp[3];
+			break;
+		default:
+			insertIterator = '?';
+			break;
+	}
+}
+
+
+
+template<typename InserterT> void ConvertSingleUtf32ToUtf16( char32_t codePoint, InserterT insertIterator )
+{
+	char16_t temp[2] = {};
+	size_t const numPairs = ConvertSingleUtf32ToUtf16( codePoint, temp );
+	switch( numPairs )
+	{
+		case 1:
+			insertIterator = temp[0];
+			break;
+		case 2:
+			insertIterator = temp[0];
+			insertIterator = temp[1];
+			break;
+		default:
+			insertIterator = u'?';
+			break;
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+template<typename InserterT> void ConvertToASCII( char const* source, size_t numBytes, InserterT insertIterator )
+{
+	details::CollapseToAscii( source, numBytes, insertIterator );
+}
+
+
+
+template<typename InserterT> void ConvertToASCII( char16_t const* source, size_t numPairs, InserterT insertIterator )
+{
+	details::CollapseToAscii( source, numPairs, insertIterator );
+}
+
+
+
+template<typename InserterT> void ConvertToASCII( char32_t const* source, size_t numCodePoints, InserterT insertIterator )
+{
+	details::CollapseToAscii( source, numCodePoints, insertIterator );
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+template<typename InserterT> void ConvertToUtf8( char const* source, size_t numBytes, InserterT insertIterator )
+{
+	// Same
+	for( size_t i = 0; i < numBytes; i++ )
+	{
+		insertIterator = source[i];
+	}
+}
+
+
+
+template<typename InserterT> void ConvertToUtf8( char16_t const* source, size_t numPairs, InserterT insertIterator )
+{
+	// Transition through UTF-32
+	size_t i = 0;
+	while( i < numPairs )
+	{
+		char16_t const* const buffer = &( source[i] );
+		size_t const remainingPairs = numPairs - i;
+		size_t const neededPairs = NumPairsExpectedInUtf8( *buffer );
+
+		if( remainingPairs < neededPairs )
+		{
+			// Invalid, not enough pairs
+			insertIterator = U'?';
+			break;
+		}
+
+		if( neededPairs == 0 )
+		{
+			// Invalid, unable to decode
+			insertIterator = U'?';
+			break;
+		}
+
+		// Expand
+		char32_t const codePoint = ConvertSingleUtf16ToUtf32( buffer, neededPairs );
+
+		// Shrink
+		ConvertSingleUtf32ToUtf8( codePoint, insertIterator );
+
+		i += neededPairs;
+	}
+}
+
+
+
+template<typename InserterT> void ConvertToUtf8( char32_t const* source, size_t numCodePoints, InserterT insertIterator )
+{
+	// Shrink
+	for( size_t i = 0; i < numCodePoints; i++ )
+	{
+		char32_t const& codePoint = source[i];
+		ConvertSingleUtf32ToUtf8( codePoint, insertIterator );
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+template<typename InserterT> void ConvertToUtf16( char const* source, size_t numBytes, InserterT insertIterator )
+{
+	// Transition through UTF-32
+	size_t i = 0;
+	while( i < numBytes )
+	{
+		char const* const buffer = &( source[i] );
+		size_t const remainingBytes = numBytes - i;
+		size_t const neededBytes = NumBytesExpectedInUtf8( *buffer );
+
+		if( remainingBytes < neededBytes )
+		{
+			// Invalid, not enough bytes
+			insertIterator = U'?';
+			break;
+		}
+
+		if( neededBytes == 0 )
+		{
+			// Invalid, unable to decode
+			insertIterator = U'?';
+			break;
+		}
+
+		// Expand
+		char32_t const codePoint = ConvertSingleUtf8ToUtf32( buffer, neededBytes );
+
+		// Shrink
+		ConvertSingleUtf32ToUtf16( codePoint, insertIterator );
+
+		i += neededBytes;
+	}
+}
+
+
+
+template<typename InserterT> void ConvertToUtf16( char16_t const* source, size_t numPairs, InserterT insertIterator )
+{
+	// Same
+	for( size_t i = 0; i < numPairs; i++ )
+	{
+		insertIterator = source[i];
+	}
+}
+
+
+
+template<typename InserterT> void ConvertToUtf16( char32_t const* source, size_t numCodePoints, InserterT insertIterator )
+{
+	// Shrink
+	for( size_t i = 0; i < numCodePoints; i++ )
+	{
+		char32_t const& codePoint = source[i];
+		ConvertSingleUtf32ToUtf16( codePoint, insertIterator );
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+template<typename InserterT> void ConvertToUtf32( char const* source, size_t numBytes, InserterT insertIterator )
+{
+	// Expand
+	size_t i = 0;
+	while( i < numBytes )
+	{
+		char const* const buffer = &( source[i] );
+		size_t const remainingBytes = numBytes - i;
+		size_t const neededBytes = NumBytesExpectedInUtf8( *buffer );
+
+		if( remainingBytes < neededBytes )
+		{
+			// Invalid, not enough bytes
+			insertIterator = U'?';
+			break;
+		}
+
+		if( neededBytes == 0 )
+		{
+			// Invalid, unable to decode
+			insertIterator = U'?';
+			break;
+		}
+
+		char32_t const codePoint = ConvertSingleUtf8ToUtf32( buffer, neededBytes );
+		insertIterator = codePoint;
+
+		i += neededBytes;
+	}
+}
+
+
+
+template<typename InserterT> void ConvertToUtf32( char16_t const* source, size_t numPairs, InserterT insertIterator )
+{
+	// Expand
+	size_t i = 0;
+	while( i < numPairs )
+	{
+		char16_t const* const buffer = &( source[i] );
+		size_t const remainingPairs = numPairs - i;
+		size_t const neededPairs = NumPairsExpectedInUtf8( *buffer );
+
+		if( remainingPairs < neededPairs )
+		{
+			// Invalid, not enough pairs
+			insertIterator = U'?';
+			break;
+		}
+
+		if( neededPairs == 0 )
+		{
+			// Invalid, unable to decode
+			insertIterator = U'?';
+			break;
+		}
+
+		char32_t const codePoint = ConvertSingleUtf16ToUtf32( buffer, neededPairs );
+		insertIterator = codePoint;
+
+		i += neededPairs;
+	}
+}
+
+
+
+template<typename InserterT> void ConvertToUtf32( char32_t const* source, size_t numCodePoints, InserterT insertIterator )
+{
+	// Same
+	for( size_t i = 0; i < numCodePoints; i++ )
+	{
+		insertIterator = source[i];
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+}}
--- libs/core_unicode/StringConvert.cpp
+++ libs/core_unicode/StringConvert.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "StringConvert.h"
 #include "CharConvert.h"
+#include "InsertConvert.h"
 
 #include "core/macros.h"
 
@@ -9,114 +10,62 @@
 
 namespace RF { namespace unicode {
 ///////////////////////////////////////////////////////////////////////////////
-namespace details {
-
-template<typename CharT>
-rftl::string CollapseToAscii( CharT const* source, size_t numElements )
-{
-	rftl::string retVal;
-	retVal.reserve( numElements );
-	for( size_t i = 0; i < numElements; i++ )
-	{
-		CharT const& in = source[i];
-
-		if( in >= 0 && in <= 127 )
-		{
-			retVal.push_back( static_cast<char>( in ) );
-		}
-		else
-		{
-			retVal.push_back( '?' );
-		}
-	}
-	return retVal;
-}
-
-
-
-template<typename CharT>
-rftl::string CollapseToAscii( rftl::basic_string<CharT> const& source )
-{
-	return CollapseToAscii( source.data(), source.size() );
-}
-
-}
-///////////////////////////////////////////////////////////////////////////////
 
 rftl::string unicode::ConvertToASCII( char const * source, size_t numBytes )
 {
-	return details::CollapseToAscii( source, numBytes );
+	rftl::string retVal;
+	retVal.reserve( numBytes );
+	ConvertToASCII( source, numBytes, rftl::back_inserter( retVal ) );
+	return retVal;
 }
 
 
 
 rftl::string ConvertToASCII( char16_t const * source, size_t numPairs )
 {
-	return details::CollapseToAscii( source, numPairs );
+	rftl::string retVal;
+	retVal.reserve( numPairs );
+	ConvertToASCII( source, numPairs, rftl::back_inserter( retVal ) );
+	return retVal;
 }
 
 
 
 rftl::string ConvertToASCII( char32_t const * source, size_t numCodePoints )
 {
-	return details::CollapseToAscii( source, numCodePoints );
+	rftl::string retVal;
+	retVal.reserve( numCodePoints );
+	ConvertToASCII( source, numCodePoints, rftl::back_inserter( retVal ) );
+	return retVal;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
 rftl::string ConvertToUtf8( char const * source, size_t numBytes )
 {
-	// Same
-	return rftl::string( source, numBytes );
+	rftl::string retVal;
+	retVal.reserve( numBytes );
+	ConvertToUtf8( source, numBytes, rftl::back_inserter( retVal ) );
+	return retVal;
 }
 
 
 
 rftl::string ConvertToUtf8( char16_t const * source, size_t numPairs )
 {
-	// Shrink
-	// HACK: Transition through UTF-32
-	return ConvertToUtf8( ConvertToUtf32( source, numPairs ) );
+	rftl::string retVal;
+	retVal.reserve( numPairs );
+	ConvertToUtf8( source, numPairs, rftl::back_inserter( retVal ) );
+	return retVal;
 }
 
 
 
 rftl::string ConvertToUtf8( char32_t const * source, size_t numCodePoints )
 {
-	// Shrink
 	rftl::string retVal;
 	retVal.reserve( numCodePoints * 4 );
-	for( size_t i = 0; i < numCodePoints; i++ )
-	{
-		char32_t const& codePoint = source[i];
-
-		char temp[4] = {};
-		size_t const numBytes = ConvertSingleUtf32ToUtf8( codePoint, temp );
-		switch( numBytes )
-		{
-			case 1:
-				retVal.push_back( temp[0] );
-				break;
-			case 2:
-				retVal.push_back( temp[0] );
-				retVal.push_back( temp[1] );
-				break;
-			case 3:
-				retVal.push_back( temp[0] );
-				retVal.push_back( temp[1] );
-				retVal.push_back( temp[2] );
-				break;
-			case 4:
-				retVal.push_back( temp[0] );
-				retVal.push_back( temp[1] );
-				retVal.push_back( temp[2] );
-				retVal.push_back( temp[3] );
-				break;
-			default:
-				retVal.push_back( '?' );
-				break;
-		}
-	}
+	ConvertToUtf8( source, numCodePoints, rftl::back_inserter( retVal ) );
 	return retVal;
 }
 
@@ -124,46 +73,29 @@ rftl::string ConvertToUtf8( char32_t const * source, size_t numCodePoints )
 
 rftl::u16string ConvertToUtf16( char const * source, size_t numBytes )
 {
-	// Expand
-	// HACK: Transition through UTF-32
-	return ConvertToUtf16( ConvertToUtf32( source, numBytes ) );
+	rftl::u16string retVal;
+	retVal.reserve( numBytes );
+	ConvertToUtf16( source, numBytes, rftl::back_inserter( retVal ) );
+	return retVal;
 }
 
 
 
 rftl::u16string ConvertToUtf16( char16_t const * source, size_t numPairs )
 {
-	// Same
-	return rftl::u16string( source, numPairs );
+	rftl::u16string retVal;
+	retVal.reserve( numPairs );
+	ConvertToUtf16( source, numPairs, rftl::back_inserter( retVal ) );
+	return retVal;
 }
 
 
 
 rftl::u16string ConvertToUtf16( char32_t const * source, size_t numCodePoints )
 {
-	// Shrink
 	rftl::u16string retVal;
 	retVal.reserve( numCodePoints * 2 );
-	for( size_t i = 0; i < numCodePoints; i++ )
-	{
-		char32_t const& codePoint = source[i];
-
-		char16_t temp[2] = {};
-		size_t const numPairs = ConvertSingleUtf32ToUtf16( codePoint, temp );
-		switch( numPairs )
-		{
-			case 1:
-				retVal.push_back( temp[0] );
-				break;
-			case 2:
-				retVal.push_back( temp[0] );
-				retVal.push_back( temp[1] );
-				break;
-			default:
-				retVal.push_back( u'?' );
-				break;
-		}
-	}
+	ConvertToUtf16( source, numCodePoints, rftl::back_inserter( retVal ) );
 	return retVal;
 }
 
@@ -171,36 +103,9 @@ rftl::u16string ConvertToUtf16( char32_t const * source, size_t numCodePoints )
 
 rftl::u32string ConvertToUtf32( char const * source, size_t numBytes )
 {
-	// Expand
 	rftl::u32string retVal;
 	retVal.reserve( numBytes );
-	size_t i = 0;
-	while( i < numBytes )
-	{
-		char const* const buffer = &( source[i] );
-		size_t const remainingBytes = numBytes - i;
-		size_t const neededBytes = NumBytesExpectedInUtf8( *buffer );
-
-		if( remainingBytes < neededBytes )
-		{
-			// Invalid, not enough bytes
-			retVal.push_back( U'?' );
-			break;
-		}
-
-		if( neededBytes == 0 )
-		{
-			// Invalid, unable to decode
-			retVal.push_back( U'?' );
-			break;
-		}
-
-		char32_t const codePoint = ConvertSingleUtf8ToUtf32( buffer, neededBytes );
-		retVal.push_back( codePoint );
-
-		i += neededBytes;
-	}
-
+	ConvertToUtf32( source, numBytes, rftl::back_inserter( retVal ) );
 	return retVal;
 }
 
@@ -208,36 +113,9 @@ rftl::u32string ConvertToUtf32( char const * source, size_t numBytes )
 
 rftl::u32string ConvertToUtf32( char16_t const * source, size_t numPairs )
 {
-	// Expand
 	rftl::u32string retVal;
 	retVal.reserve( numPairs );
-	size_t i = 0;
-	while( i < numPairs )
-	{
-		char16_t const* const buffer = &( source[i] );
-		size_t const remainingPairs = numPairs - i;
-		size_t const neededPairs = NumPairsExpectedInUtf8( *buffer );
-
-		if( remainingPairs < neededPairs )
-		{
-			// Invalid, not enough pairs
-			retVal.push_back( U'?' );
-			break;
-		}
-
-		if( neededPairs == 0 )
-		{
-			// Invalid, unable to decode
-			retVal.push_back( U'?' );
-			break;
-		}
-
-		char32_t const codePoint = ConvertSingleUtf16ToUtf32( buffer, neededPairs );
-		retVal.push_back( codePoint );
-
-		i += neededPairs;
-	}
-
+	ConvertToUtf32( source, numPairs, rftl::back_inserter( retVal ) );
 	return retVal;
 }
 
@@ -245,8 +123,10 @@ rftl::u32string ConvertToUtf32( char16_t const * source, size_t numPairs )
 
 rftl::u32string ConvertToUtf32( char32_t const * source, size_t numCodePoints )
 {
-	// Same
-	return rftl::u32string( source, numCodePoints );
+	rftl::u32string retVal;
+	retVal.reserve( numCodePoints );
+	ConvertToUtf32( source, numCodePoints, rftl::back_inserter( retVal ) );
+	return retVal;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
