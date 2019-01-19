63ad94de013d99e0b26061516fd7a2d7f754d8cd
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-01-19 15:34:10 -0800

    Support for vectors of values from script->reflect

---
+++ libs/core/meta/ScopedCleanup.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "core/macros.h"
+
+
+namespace RF {
+///////////////////////////////////////////////////////////////////////////////
+
+// Sometimes you want to do something when a scope ends, regardless of which
+//  branches it may have taken, but you only want to write the code once
+// EXAMPLE:
+//  auto onScopeEnd = RF::OnScopeEnd( [handle]()
+//  {
+//    handle->Cleanup();
+//  } );
+template<typename CALLABLE>
+struct OnScopeEnd
+{
+	RF_NO_COPY( OnScopeEnd );
+	RF_NO_MOVE( OnScopeEnd );
+
+	OnScopeEnd( CALLABLE const& callable )
+		: mCallable( callable )
+	{
+		//
+	}
+
+	OnScopeEnd( CALLABLE && callable )
+		: mCallable( std::move( callable ) )
+	{
+		//
+	}
+
+	~OnScopeEnd()
+	{
+		mCallable();
+	}
+
+	CALLABLE const mCallable;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+}
