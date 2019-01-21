ff5013f7d58eac008d9be70b5342256c6ac43ace
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-01-20 22:30:46 -0800

    Added test assets for tilemap layer support

---
+++ art/tilesets/common/pallete16_4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="pallete16_4" tilewidth="1" tileheight="1" tilecount="16" columns="4">
+ <image source="../../../data/textures/common/pallete16_4.png" width="4" height="4"/>
+</tileset>
