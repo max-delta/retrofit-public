f68a7a7ba0cfd1bfc6a6696c7d76a615ed516896
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-01-27 12:17:14 -0800

    Added pdbs to binaries list

--- package_binaries.bat
+++ package_binaries.bat
@@ -7,14 +7,30 @@ copy _artifacts\x64\Retail\Out\core_test.exe _binaries\core_test_x64.exe
 copy _artifacts\Win32\Retail\Out\core_test.exe _binaries\core_test_x86.exe
 copy _artifacts\LLVM_x64\Retail\Out\core_test.exe _binaries\core_test_LLVM.exe
 
+copy _artifacts\x64\Retail\Out\core_test.pdb _binaries\core_test_x64.pdb
+copy _artifacts\Win32\Retail\Out\core_test.pdb _binaries\core_test_x86.pdb
+copy _artifacts\LLVM_x64\Retail\Out\core_test.pdb _binaries\core_test_LLVM.pdb
+
 copy _artifacts\x64\Retail\Out\foundation_test.exe _binaries\foundation_test_x64.exe
 copy _artifacts\Win32\Retail\Out\foundation_test.exe _binaries\foundation_test_x86.exe
 copy _artifacts\LLVM_x64\Retail\Out\foundation_test.exe _binaries\foundation_test_LLVM.exe
+
+copy _artifacts\x64\Retail\Out\foundation_test.pdb _binaries\foundation_test_x64.pdb
+copy _artifacts\Win32\Retail\Out\foundation_test.pdb _binaries\foundation_test_x86.pdb
+copy _artifacts\LLVM_x64\Retail\Out\foundation_test.pdb _binaries\foundation_test_LLVM.pdb
 
 copy _artifacts\x64\Retail\Out\retrofit.exe _binaries\retrofit_x64.exe
 copy _artifacts\Win32\Retail\Out\retrofit.exe _binaries\retrofit_x86.exe
 copy _artifacts\LLVM_x64\Retail\Out\retrofit.exe _binaries\retrofit_LLVM.exe
 
+copy _artifacts\x64\Retail\Out\retrofit.pdb _binaries\retrofit_x64.pdb
+copy _artifacts\Win32\Retail\Out\retrofit.pdb _binaries\retrofit_x86.pdb
+copy _artifacts\LLVM_x64\Retail\Out\retrofit.pdb _binaries\retrofit_LLVM.pdb
+
 copy _artifacts\x64\Retail\Out\FramePackEditor.exe _binaries\FramePackEditor_x64.exe
 copy _artifacts\Win32\Retail\Out\FramePackEditor.exe _binaries\FramePackEditor_x86.exe
 copy _artifacts\LLVM_x64\Retail\Out\FramePackEditor.exe _binaries\FramePackEditor_LLVM.exe
+
+copy _artifacts\x64\Retail\Out\FramePackEditor.pdb _binaries\FramePackEditor_x64.pdb
+copy _artifacts\Win32\Retail\Out\FramePackEditor.pdb _binaries\FramePackEditor_x86.pdb
+copy _artifacts\LLVM_x64\Retail\Out\FramePackEditor.pdb _binaries\FramePackEditor_LLVM.pdb
