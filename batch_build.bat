ada13924f016caeba42c037a09bad02711ab74cd
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-01-27 00:10:30 -0800

    Added batch build script

---
+++ batch_build.bat
@@ -0,0 +1,10 @@
+REM VS2017 Batch Build routinely corrupts all your build artifacts. Known issue...
+REM Work-around is to roll your own batch file :/
+
+cls
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x64 /p:Configuration=Debug
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x64 /p:Configuration=Release
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86 /p:Configuration=Debug
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86 /p:Configuration=Release
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=LLVM_x64 /p:Configuration=Debug
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=LLVM_x64 /p:Configuration=Release
