7ee0a767b03c357246c8cf0511be191ad5b936b8
Max Delta <Max Delta@localhost>
2019-01-27 00:43:09 -0800

    Build fixes

--- batch_build.bat
+++ batch_build.bat
@@ -1,7 +1,8 @@
-REM VS2017 Batch Build routinely corrupts all your build artifacts. Known issue...
-REM Work-around is to roll your own batch file :/
+@REM VS2017 Batch Build routinely corrupts all your build artifacts. Known issue...
+@REM Work-around is to roll your own batch file :/
 
-cls
+@cls
+@echo Low verbosity! May sit for a while without messages!
 msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x64 /p:Configuration=Debug
 msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x64 /p:Configuration=Release
 msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86 /p:Configuration=Debug
