2b03b23375cfbdd4695f44b86640ae1de0b2f0a9
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-01-27 00:47:44 -0800

    Added batch test

---
+++ batch_test.bat
@@ -0,0 +1,14 @@
+@cls
+_artifacts\x64\Debug\Out\core_test.exe
+_artifacts\x64\Release\Out\core_test.exe
+_artifacts\x86\Debug\Out\core_test.exe
+_artifacts\x86\Release\Out\core_test.exe
+_artifacts\LLVM_x64\Debug\Out\core_test.exe
+_artifacts\LLVM_x64\Release\Out\core_test.exe
+
+_artifacts\x64\Debug\Out\foundation_test.exe
+_artifacts\x64\Release\Out\foundation_test.exe
+_artifacts\x86\Debug\Out\foundation_test.exe
+_artifacts\x86\Release\Out\foundation_test.exe
+_artifacts\LLVM_x64\Debug\Out\foundation_test.exe
+_artifacts\LLVM_x64\Release\Out\foundation_test.exe
