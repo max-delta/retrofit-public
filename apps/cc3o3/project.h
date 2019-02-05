7f6219aa7fcdaa7c40788ff711647c8b196b36af
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-02-04 17:50:04 -0800

    Added cc3o3 app, fixed sln bugs, minor cleanup

---
+++ apps/cc3o3/project.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "AppCommon_GraphicalClient/project.h"
+
+constexpr char const* RFCAT_CC3O3 = "CC3O3";
+constexpr uint64_t RFCAT_CC3O3_SEV_WHITELIST = RF::logging::kDefaultSeverityMask;
