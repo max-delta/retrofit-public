6e0f5ca4119545a003362e83b93d87327aabb8f7
Max Delta <Max Delta@localhost>
2019-02-09 13:42:14 -0800

    More color features

---
+++ libs/core_math/Rand.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "rftl/cstdint"
+
+namespace RF { namespace math {
+///////////////////////////////////////////////////////////////////////////////
+
+// This random function will not change based on platform
+// NOTE: Not a 'good' generator, but good enough for gameplay code
+constexpr uint32_t kStableRandLCGMaxEntropy = 2'147'483'647;
+uint32_t StableRandLCG( uint32_t previousValue );
+uint8_t StableRandLCGPercent( uint32_t& lastGeneratorValue );
+bool StableRandLCGPercentAbove( uint8_t percentThreshold, uint32_t& lastGeneratorValue );
+bool StableRandLCGPercentBelow( uint8_t percentThreshold, uint32_t& lastGeneratorValue );
+
+uint32_t GetSeedFromHash( uint64_t hash );
+
+///////////////////////////////////////////////////////////////////////////////
+}}
