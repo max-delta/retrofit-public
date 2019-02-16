b0c55efe49e444d64154d8f2b87fa4a924252905
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-02-16 13:08:16 -0800

    Cleaned up DllMains

---
+++ libs/core_platform/dllmain.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "core/compiler.h"
+
+#ifdef RF_PLATFORM_WINDOWS
+
+#include "core_platform/win_shim.h"
+#define RF_EMPTY_DLLMAIN() \
+	shim::BOOL __stdcall DllMain( shim::HMODULE hModule, \
+		shim::DWORD ul_reason_for_call, \
+		shim::LPVOID lpReserved ) \
+	{ \
+		return 1 /*win32::TRUE*/; \
+	}
+#define RF_DLLMAIN() \
+	shim::BOOL __stdcall DllMain( shim::HMODULE hModule, \
+		shim::DWORD ul_reason_for_call, \
+		shim::LPVOID lpReserved ) \
+	{ \
+		switch( ul_reason_for_call ) \
+		{ \
+			case 1 /*DLL_PROCESS_ATTACH*/: \
+				return OnDllMainProcessAttach(); \
+			case 2 /*DLL_THREAD_ATTACH*/: \
+				return OnDllMainThreadAttach(); \
+			case 3 /*DLL_THREAD_DETACH*/: \
+				return OnDllMainThreadDetach(); \
+			case 0 /*DLL_PROCESS_DETACH*/: \
+				return OnDllMainProcessDetach(); \
+			default: \
+				return 1 /*win32::TRUE*/; \
+		} \
+	}
+
+#else
+
+#define RF_EMPTY_DLLMAIN()
+
+#endif
