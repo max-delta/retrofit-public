d86f5992288a6caef377c97b1a90b365b422907e
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-02-16 13:08:16 -0800

    Cleaned up DllMains

--- libs/Logging/dllmain.cpp
+++ libs/Logging/dllmain.cpp
@@ -1,27 +1,30 @@
 #include "stdafx.h"
-
-#include "core_platform/windows_inc.h"
-
+#include "core_platform/dllmain.h"
 
 // Direct forwards to avoid Windows.h contamination into headers
 namespace RF { namespace logging {
-	void InsertFallbackLogger();
+void InsertFallbackLogger();
 }}
 
-win32::BOOL APIENTRY DllMain( win32::HMODULE hModule,
-	win32::DWORD ul_reason_for_call,
-	win32::LPVOID lpReserved )
+shim::BOOL OnDllMainProcessAttach()
 {
-	switch( ul_reason_for_call )
-	{
-		case DLL_PROCESS_ATTACH:
-			RF::logging::InsertFallbackLogger();
-			break;
-		case DLL_THREAD_ATTACH:
-		case DLL_THREAD_DETACH:
-		case DLL_PROCESS_DETACH:
-		default:
-			break;
-	}
-	return win32::TRUE;
+	RF::logging::InsertFallbackLogger();
+	return 1 /*win32::TRUE*/;
 }
+
+shim::BOOL OnDllMainProcessDetach()
+{
+	return 1 /*win32::TRUE*/;
+}
+
+shim::BOOL OnDllMainThreadAttach()
+{
+	return 1 /*win32::TRUE*/;
+}
+
+shim::BOOL OnDllMainThreadDetach()
+{
+	return 1 /*win32::TRUE*/;
+}
+
+RF_DLLMAIN();
