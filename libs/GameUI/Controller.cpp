e72a08d7af02b8cbf9ec097540794d91fb54b682
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-02-16 17:45:52 -0800

    UI permission cleanup

--- libs/GameUI/Controller.cpp
+++ libs/GameUI/Controller.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "Controller.h"
 
+#include "GameUI/ContainerManager.h"
+
 
 namespace RF { namespace ui {
 ///////////////////////////////////////////////////////////////////////////////
@@ -22,6 +24,52 @@ void Controller::OnAABBRecalc( ContainerManager& manager, Container& container )
 void Controller::OnImminentDestruction( ContainerManager& manager, Container const& container )
 {
 	//
+}
+
+
+
+Container& Controller::GetMutableContainer( ContainerManager& manager, ContainerID containerID )
+{
+	return manager.GetMutableContainer( containerID );
+}
+
+
+
+ContainerID Controller::CreateChildContainer(
+	ContainerManager& manager,
+	Container& parentContainer,
+	AnchorID leftConstraint,
+	AnchorID rightConstraint,
+	AnchorID topConstraint,
+	AnchorID bottomConstraint )
+{
+	return manager.CreateChildContainer(
+		parentContainer,
+		leftConstraint,
+		rightConstraint,
+		topConstraint,
+		bottomConstraint );
+}
+
+
+
+void Controller::DestroyContainer( ContainerManager& manager, ContainerID containerID )
+{
+	manager.DestroyContainer( containerID );
+}
+
+
+
+AnchorID Controller::CreateAnchor( ContainerManager& manager, Container& container )
+{
+	return manager.CreateAnchor( container );
+}
+
+
+
+void Controller::MoveAnchor( ContainerManager& manager, AnchorID anchorID, gfx::PPUCoord pos )
+{
+	manager.MoveAnchor( anchorID, pos );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
