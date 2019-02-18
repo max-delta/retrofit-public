8f34886aad03e65ae8a0a5580af120d30a2bd658
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-02-18 15:06:47 -0800

    Minor org and cleanup

--- libs/GameAppState/AppState.h
+++ libs/GameAppState/AppState.h
@@ -3,27 +3,8 @@
 
 #include "GameAppState/AppStateFwd.h"
 
-#include "core/ptr/unique_ptr.h"
-
 
 namespace RF { namespace appstate {
-///////////////////////////////////////////////////////////////////////////////
-
-class GAMEAPPSTATE_API AppStateRef
-{
-	RF_NO_COPY( AppStateRef );
-
-public:
-	AppStateRef() = default;
-
-	void OnEnter( AppStateChangeContext& context );
-	void OnExit( AppStateChangeContext& context );
-	void OnTick( AppStateTickContext& context );
-
-public:
-	UniquePtr<AppState> mAppState;
-};
-
 ///////////////////////////////////////////////////////////////////////////////
 
 class GAMEAPPSTATE_API AppState
