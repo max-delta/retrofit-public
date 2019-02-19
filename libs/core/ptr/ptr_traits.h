ca269b972de63b9f47465bf4dc7ff5da3ae12d95
Max Delta <Max Delta@localhost>
2019-02-18 21:56:48 -0800

    Fixed a correctness issue with the deletion check, and moved it to a location that compiles more reliably

--- libs/core/ptr/ptr_traits.h
+++ libs/core/ptr/ptr_traits.h
@@ -1,4 +1,19 @@
 #pragma once
+#include "rftl/type_traits"
+
+
+#define RF_PTR_ASSERT_DELETABLE( TYPE ) \
+	static_assert( \
+		rftl::is_polymorphic<TYPE>::value == false || \
+			rftl::has_virtual_destructor<TYPE>::value || \
+			rftl::is_base_of<::RF::PtrTrait::NoVirtualDestructor, TYPE>::value, \
+		"Potentially unsafe deletions due to non-virtual destructor on base class. Please use 'virtual ~Base() = default;' at a minimum." );
+
+#define RF_PTR_ASSERT_CASTABLE( BASETYPE, DERIVEDTYPE ) \
+	static_assert( \
+		rftl::is_base_of<BASETYPE, DERIVEDTYPE>::value, \
+		"Failed to cast PTR<" #DERIVEDTYPE "> to PTR<" #BASETYPE ">" ); \
+	RF_PTR_ASSERT_DELETABLE( BASETYPE );
 
 namespace RF { namespace PtrTrait {
 ///////////////////////////////////////////////////////////////////////////////
