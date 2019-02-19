3ba64839fab820dd942fe6df815f08d9a4bee6ae
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-02-19 00:01:17 -0800

    Rudimentart loading of some fonts

---
+++ apps/cc3o3/ui/UIFwd.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "project.h"
+
+#include "GameUI/UIFwd.h"
+
+namespace RF { namespace cc { namespace ui {
+///////////////////////////////////////////////////////////////////////////////
+
+using namespace RF::ui;
+
+namespace font {
+enum : FontPurposeID
+{
+	Invalid = kInvalidFontPurposeID,
+	MinSize,
+	HalfTileSize
+}; 
+}
+
+///////////////////////////////////////////////////////////////////////////////
+}}}
