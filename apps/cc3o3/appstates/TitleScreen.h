3133a746dd968e23db2358f633c4967f901a051d
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-02-18 19:22:46 -0800

    Added stub state logic

---
+++ apps/cc3o3/appstates/TitleScreen.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "project.h"
+
+#include "cc3o3/appstates/AppStatesFwd.h"
+
+#include "GameAppState/AppState.h"
+
+#include "core/ptr/unique_ptr.h"
+
+
+namespace RF { namespace cc { namespace appstate {
+///////////////////////////////////////////////////////////////////////////////
+
+class TitleScreen final : public AppState
+{
+	RF_NO_COPY( TitleScreen );
+
+public:
+	TitleScreen() = default;
+
+	virtual void OnEnter( AppStateChangeContext& context ) override;
+	virtual void OnExit( AppStateChangeContext& context ) override;
+	virtual void OnTick( AppStateTickContext& context ) override;
+
+
+private:
+	struct InternalState;
+	UniquePtr<InternalState> mInternalState;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+}}}
