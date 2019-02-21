15f226d2b028f9bdcb83e8b38d924678f0e93df7
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-02-20 22:30:17 -0800

    Minor macro cleanup

--- libs/Serialization/CsvReader.h
+++ libs/Serialization/CsvReader.h
@@ -12,13 +12,9 @@ namespace RF { namespace serialization {
 
 class SERIALIZATION_API CsvReader
 {
-	RF_NO_COPY( CsvReader );
-	RF_NO_MOVE( CsvReader );
+	RF_NO_INSTANCE( CsvReader );
 
 public:
-	CsvReader() = delete;
-	~CsvReader() = delete;
-
 	static rftl::deque<rftl::deque<rftl::basic_string<char>>> TokenizeToDeques( char const* buffer, size_t bufferLen );
 	static rftl::deque<rftl::deque<rftl::basic_string<char16_t>>> TokenizeToDeques( char16_t const* buffer, size_t bufferLen );
 	static rftl::deque<rftl::deque<rftl::basic_string<char32_t>>> TokenizeToDeques( char32_t const* buffer, size_t bufferLen );
