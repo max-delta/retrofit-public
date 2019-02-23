2c665e474c621e739c621b292df9c1dbb7d56d7d
Max Delta <Max Delta@localhost>
2019-02-23 13:55:54 -0800

    Added RowSlicer, minor cleanup

---
+++ libs/GameUI/controllers/RowSlicer.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "project.h"
+
+#include "GameUI/Controller.h"
+
+
+namespace RF { namespace ui { namespace controller {
+///////////////////////////////////////////////////////////////////////////////
+
+class GAMEUI_API RowSlicer final : public Controller
+{
+	RFTYPE_ENABLE_VIRTUAL_LOOKUP();
+
+	//
+	// Types
+public:
+	using Ratios = rftl::vector<rftl::pair<float, bool>>;
+
+
+	//
+	// Public methods
+public:
+	RowSlicer() = delete;
+	RowSlicer( Ratios const& ratios );
+
+	ContainerID GetChildContainerID( size_t sliceIndex ) const;
+	void CreateChildContainer( ContainerManager& manager, size_t sliceIndex );
+	void DestroyChildContainer( ContainerManager& manager, size_t sliceIndex );
+
+	virtual void OnAssign( ContainerManager& manager, Container& container ) override;
+	virtual void OnAABBRecalc( ContainerManager& manager, Container& container ) override;
+
+
+	//
+	// Private methods
+private:
+	void CreateChildContainerInternal( ContainerManager& manager, Container& container, size_t sliceIndex );
+
+
+	//
+	// Private data
+private:
+	ContainerID mParentContainerID = kInvalidContainerID;
+	rftl::vector<AnchorID> mAnchors;
+	rftl::vector<ContainerID> mContainers;
+	Ratios mRatios;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+}}}
