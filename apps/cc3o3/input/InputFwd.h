f86111bbf54a4580aa01a3f9dd119b41419ecb7a
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-03-19 22:26:24 -0700

    Added basic input mapping as hard-coded setup

---
+++ apps/cc3o3/input/InputFwd.h
@@ -0,0 +1,61 @@
+#pragma once
+#include "project.h"
+
+#include "GameInput/InputFwd.h"
+
+namespace RF { namespace cc { namespace input {
+///////////////////////////////////////////////////////////////////////////////
+
+using namespace RF::input;
+
+namespace player {
+enum : PlayerID
+{
+	Invalid = kInvalidPlayerID,
+	P1
+};
+}
+
+namespace layer {
+enum : LayerID
+{
+	Invalid = kInvalidLayerID,
+	MainMenu,
+	GameMenu,
+	CharacterControl
+};
+}
+
+namespace command {
+namespace raw {
+enum : RawCommandType
+{
+	Invalid = kInvalidRawCommand,
+
+	Up,
+	Down,
+	Left,
+	Right,
+
+	Affirmative,
+	Negative,
+	Auxiliary
+};
+}
+namespace game {
+enum : GameCommandType
+{
+	Invalid = kInvalidGameCommand,
+
+	UINavigateUp,
+	UINavigateDown,
+	UINavigateLeft,
+	UINavigateRight,
+	UIActivateSelection,
+	UICancelSelection
+};
+}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+}}}
