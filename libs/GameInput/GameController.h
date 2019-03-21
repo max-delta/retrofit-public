7c55175867b33f9f0390b6172c34e0384f00239e
Max Delta <Max Delta@localhost>
2019-03-20 20:20:01 -0700

    Added time filtering to controllers

--- libs/GameInput/GameController.h
+++ libs/GameInput/GameController.h
@@ -34,10 +34,12 @@ public:
 	virtual ~GameController() = default;
 	void GetGameCommandStream( rftl::virtual_iterator<Command>& parser ) const;
 	virtual void GetGameCommandStream( rftl::virtual_iterator<Command>& parser, size_t maxCommands ) const;
+	void GetGameCommandStream( rftl::virtual_iterator<Command>& parser, time::FrameClock::time_point earliestTime ) const;
 	void GetKnownSignals( rftl::virtual_iterator<GameSignalType>& iter ) const;
 	virtual void GetKnownSignals( rftl::virtual_iterator<GameSignalType>& iter, size_t maxTypes ) const;
 	void GetGameSignalStream( rftl::virtual_iterator<Signal>& sampler, GameSignalType type ) const;
 	virtual void GetGameSignalStream( rftl::virtual_iterator<Signal>& sampler, size_t maxSamples, GameSignalType type ) const;
+	void GetGameSignalStream( rftl::virtual_iterator<Signal>& sampler, time::FrameClock::time_point earliestTime, GameSignalType type ) const;
 	void GetTextStream( rftl::u16string& text ) const;
 	virtual void GetTextStream( rftl::u16string& text, size_t maxLen ) const;
 };
--- libs/GameInput/RawController.cpp
+++ libs/GameInput/RawController.cpp
@@ -14,6 +14,21 @@ void RawController::GetRawCommandStream( rftl::virtual_iterator<Command>& parser
 
 
 
+void RawController::GetRawCommandStream( rftl::virtual_iterator<Command>& parser, time::FrameClock::time_point earliestTime ) const
+{
+	auto const onElement = [&parser, &earliestTime]( RawController::Command const& element ) -> void
+	{
+		if( element.mTime >= earliestTime )
+		{
+			parser( element );
+		}
+	};
+	rftl::virtual_callable_iterator<RawController::Command, decltype( onElement )> filter( onElement );
+	GetRawCommandStream( filter );
+}
+
+
+
 void RawController::GetKnownSignals( rftl::virtual_iterator<RawSignalType>& iter ) const
 {
 	return GetKnownSignals( iter, rftl::numeric_limits<size_t>::max() );
@@ -24,6 +39,21 @@ void RawController::GetKnownSignals( rftl::virtual_iterator<RawSignalType>& iter
 void RawController::GetRawSignalStream( rftl::virtual_iterator<Signal>& sampler, RawSignalType type ) const
 {
 	return GetRawSignalStream( sampler, rftl::numeric_limits<size_t>::max(), type );
+}
+
+
+
+void RawController::GetRawSignalStream( rftl::virtual_iterator<Signal>& sampler, time::FrameClock::time_point earliestTime, RawSignalType type ) const
+{
+	auto const onElement = [&sampler, &earliestTime]( RawController::Signal const& element ) -> void
+	{
+		if( element.mTime >= earliestTime )
+		{
+			sampler( element );
+		}
+	};
+	rftl::virtual_callable_iterator<RawController::Signal, decltype( onElement )> filter( onElement );
+	GetRawSignalStream( filter, type );
 }
 
 
