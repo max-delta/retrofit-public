e2438e05e22454dd5cb003f5065e00f41481c974
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-03-20 20:20:01 -0700

    Added time filtering to controllers

--- libs/GameInput/RawController.h
+++ libs/GameInput/RawController.h
@@ -40,10 +40,12 @@ public:
 
 	void GetRawCommandStream( rftl::virtual_iterator<Command>& parser ) const;
 	virtual void GetRawCommandStream( rftl::virtual_iterator<Command>& parser, size_t maxCommands ) const = 0;
+	void GetRawCommandStream( rftl::virtual_iterator<Command>& parser, time::FrameClock::time_point earliestTime ) const;
 	void GetKnownSignals( rftl::virtual_iterator<RawSignalType>& iter ) const;
 	virtual void GetKnownSignals( rftl::virtual_iterator<RawSignalType>& iter, size_t maxTypes ) const = 0;
 	void GetRawSignalStream( rftl::virtual_iterator<Signal>& sampler, RawSignalType type ) const;
 	virtual void GetRawSignalStream( rftl::virtual_iterator<Signal>& sampler, size_t maxSamples, RawSignalType type ) const = 0;
+	void GetRawSignalStream( rftl::virtual_iterator<Signal>& sampler, time::FrameClock::time_point earliestTime, RawSignalType type ) const;
 	void GetTextStream( rftl::u16string& text ) const;
 	virtual void GetTextStream( rftl::u16string& text, size_t maxLen ) const = 0;
 };
