d4f788f31f0d111e562d6c519dd4e5f513355535
Max Delta <Max Delta@localhost>
2019-03-21 23:02:16 -0700

    Added buildstamp and display

--- config/Config.Debug.h
+++ config/Config.Debug.h
@@ -1,5 +1,14 @@
 #include "Config.h"
 
+#if __has_include( "__BuildStamp.h" )
+	#include "__BuildStamp.h"
+#else
+	#define RF_BUILDSTAMP_TYPE "debug"
+	#define RF_BUILDSTAMP_VERSION "dev"
+	#define RF_BUILDSTAMP_TIME "" __DATE__ " " __TIME__ ""
+	#define RF_BUILDSTAMP_SOURCE "devpc"
+#endif
+
 #define RF_CONFIG_ASSERTS RF_ALLOW
 #define RF_CONFIG_FILENAME_MACRO RF_ALLOW
 
