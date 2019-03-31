20153ca94280d23176efb07203e6c56de8e3c36c
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-03-31 13:17:20 -0700

    Whoops, missed some files in a previous commit

---
+++ apps/cc3o3/appstates/TitleScreen_CharCreate.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "project.h"
+
+#include "cc3o3/appstates/AppStatesFwd.h"
+
+#include "GameAppState/AppState.h"
+
+#include "core/ptr/unique_ptr.h"
+
+
+namespace RF { namespace cc { namespace appstate {
+///////////////////////////////////////////////////////////////////////////////
+
+class TitleScreen_CharCreate final : public AppState
+{
+	RF_NO_COPY( TitleScreen_CharCreate );
+
+public:
+	TitleScreen_CharCreate() = default;
+
+	virtual void OnEnter( AppStateChangeContext& context ) override;
+	virtual void OnExit( AppStateChangeContext& context ) override;
+	virtual void OnTick( AppStateTickContext& context ) override;
+
+
+private:
+	struct InternalState;
+	UniquePtr<InternalState> mInternalState;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+}}}
