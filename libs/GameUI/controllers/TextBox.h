d5e3bb21d2755eb185e3ddc3640867d42c4c352c
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-03-31 13:16:38 -0700

    Added TextBox and WIP MessageBox

---
+++ libs/GameUI/controllers/TextBox.h
@@ -0,0 +1,65 @@
+#pragma once
+#include "project.h"
+
+#include "GameUI/controllers/InstancedController.h"
+
+#include "core_math/Color3f.h"
+
+// Forwards
+namespace RF { namespace ui { namespace controller {
+	class TextRows;
+}}}
+
+namespace RF { namespace ui { namespace controller {
+///////////////////////////////////////////////////////////////////////////////
+
+class GAMEUI_API TextBox final : public InstancedController
+{
+	RFTYPE_ENABLE_VIRTUAL_LOOKUP();
+	RF_NO_COPY( TextBox );
+
+	//
+	// Public methods
+public:
+	TextBox() = delete;
+	TextBox(
+		size_t numRows,
+		FontPurposeID purpose,
+		rftl::unordered_set<char> const& breakableChars );
+	TextBox(
+		size_t numRows,
+		FontPurposeID purpose,
+		Justification justification,
+		math::Color3f color,
+		rftl::unordered_set<char> const& breakableChars );
+
+	ContainerID GetChildContainerID() const;
+
+	// NOTE: Right-to-left text is parsed right-to-left, so the leftmost text
+	//  is the text that will get truncated first from the string argument
+	void SetText( rftl::string const& text, bool rightToLeft );
+
+	size_t GetNumCharactersUnwrittenLastRender() const;
+
+	virtual void OnInstanceAssign( UIContext& context, Container& container ) override;
+	virtual void OnRender( UIConstContext const& context, Container const& container, bool& blockChildRendering ) override;
+
+
+	//
+	// Private data
+private:
+	size_t const mNumRows;
+	FontPurposeID const mFontPurpose;
+	Justification const mJustification;
+	math::Color3f const mColor;
+	ContainerID mChildContainerID = kInvalidContainerID;
+	bool mRightToLeft = false;
+	rftl::string mText;
+	rftl::unordered_set<char> mBreakableChars;
+	WeakPtr<TextRows> mSlotController;
+
+	size_t mNumCharactersUnwrittenLastRender = 0;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+}}}
