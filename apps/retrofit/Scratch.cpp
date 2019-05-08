fadeede5fd86947c57fc43730ac3356ffd88c88a
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-05-08 13:21:51 -0700

    More character compositor scratch work

--- apps/retrofit/Scratch.cpp
+++ apps/retrofit/Scratch.cpp
@@ -23,70 +23,79 @@ namespace RF { namespace scratch {
 ///////////////////////////////////////////////////////////////////////////////
 namespace details {
 
-struct CompositeFrameParams
+struct CompositeSequenceParams
 {
 	size_t mTileWidth = 0;
 	size_t mTileHeight = 0;
 
-	size_t mColumn = 0;
-
+	file::VFSPath mBasePieces = {};
 	sprite::Bitmap const* mBaseTex = nullptr;
 	size_t mBaseRow = 0;
 
+	file::VFSPath mClothingPieces = {};
 	sprite::Bitmap const* mClothingTex = nullptr;
 	size_t mClothingRow = 0;
 
+	file::VFSPath mHairPieces = {};
 	sprite::Bitmap const* mHairTex = nullptr;
 	size_t mHairRow = 0;
 
+	file::VFSPath mSpeciesPieces = {};
 	sprite::Bitmap const* mSpeciesTex = nullptr;
 	size_t mSpeciesNearRow = 0;
 	size_t mSpeciesFarRow = 0;
 	size_t mSpeciesTailRow = 0;
 };
 
+struct CompositeFrameParams
+{
+	CompositeSequenceParams mSequence = {};
+
+	size_t mColumn = 0;
+};
+
 
 
 sprite::Bitmap CreateCompositeFrame( CompositeFrameParams const& params )
 {
-	sprite::Bitmap baseFrame = params.mBaseTex->ExtractRegion(
-		params.mTileWidth * params.mColumn,
-		params.mTileHeight * params.mBaseRow,
-		params.mTileWidth,
-		params.mTileHeight );
+	sprite::Bitmap baseFrame = params.mSequence.mBaseTex->ExtractRegion(
+		params.mSequence.mTileWidth * params.mColumn,
+		params.mSequence.mTileHeight * params.mSequence.mBaseRow,
+		params.mSequence.mTileWidth,
+		params.mSequence.mTileHeight );
 
-	sprite::Bitmap clothingFrame = params.mClothingTex->ExtractRegion(
-		params.mTileWidth * params.mColumn,
-		params.mTileHeight * params.mClothingRow,
-		params.mTileWidth,
-		params.mTileHeight );
+	sprite::Bitmap clothingFrame = params.mSequence.mClothingTex->ExtractRegion(
+		params.mSequence.mTileWidth * params.mColumn,
+		params.mSequence.mTileHeight * params.mSequence.mClothingRow,
+		params.mSequence.mTileWidth,
+		params.mSequence.mTileHeight );
 
-	sprite::Bitmap hairFrame = params.mHairTex->ExtractRegion(
-		params.mTileWidth * params.mColumn,
-		params.mTileHeight * params.mHairRow,
-		params.mTileWidth,
-		params.mTileHeight );
+	sprite::Bitmap hairFrame = params.mSequence.mHairTex->ExtractRegion(
+		params.mSequence.mTileWidth * params.mColumn,
+		params.mSequence.mTileHeight * params.mSequence.mHairRow,
+		params.mSequence.mTileWidth,
+		params.mSequence.mTileHeight );
 
-	sprite::Bitmap speciesNearFrame = params.mSpeciesTex->ExtractRegion(
-		params.mTileWidth * params.mColumn,
-		params.mTileHeight * params.mSpeciesNearRow,
-		params.mTileWidth,
-		params.mTileHeight );
+	sprite::Bitmap speciesNearFrame = params.mSequence.mSpeciesTex->ExtractRegion(
+		params.mSequence.mTileWidth * params.mColumn,
+		params.mSequence.mTileHeight * params.mSequence.mSpeciesNearRow,
+		params.mSequence.mTileWidth,
+		params.mSequence.mTileHeight );
 
-	sprite::Bitmap speciesFarFrame = params.mSpeciesTex->ExtractRegion(
-		params.mTileWidth * params.mColumn,
-		params.mTileHeight * params.mSpeciesFarRow,
-		params.mTileWidth,
-		params.mTileHeight );
+	sprite::Bitmap speciesFarFrame = params.mSequence.mSpeciesTex->ExtractRegion(
+		params.mSequence.mTileWidth * params.mColumn,
+		params.mSequence.mTileHeight * params.mSequence.mSpeciesFarRow,
+		params.mSequence.mTileWidth,
+		params.mSequence.mTileHeight );
 
-	sprite::Bitmap speciesTailFrame = params.mSpeciesTex->ExtractRegion(
-		params.mTileWidth * params.mColumn,
-		params.mTileHeight * params.mSpeciesTailRow,
-		params.mTileWidth,
-		params.mTileHeight );
+	sprite::Bitmap speciesTailFrame = params.mSequence.mSpeciesTex->ExtractRegion(
+		params.mSequence.mTileWidth * params.mColumn,
+		params.mSequence.mTileHeight * params.mSequence.mSpeciesTailRow,
+		params.mSequence.mTileWidth,
+		params.mSequence.mTileHeight );
 
 	// Composite
-	sprite::Bitmap composite( params.mTileWidth, params.mTileHeight );
+	sprite::Bitmap composite( params.mSequence.mTileWidth, params.mSequence.mTileHeight );
 	{
 		static constexpr sprite::Bitmap::ElementType::ElementType kMinAlpha = 1;
 		composite.ApplyStencilOverwrite( speciesFarFrame, kMinAlpha );
@@ -127,14 +136,63 @@ void WriteFrameToDisk( sprite::Bitmap const& frame, file::VFSPath const& path )
 	}
 }
 
+
+
+class BitmapCache
+{
+	RF_NO_COPY( BitmapCache );
+
+private:
+	using CachedBitmaps = rftl::unordered_map<file::VFSPath, UniquePtr<sprite::Bitmap>>;
+
+public:
+	explicit BitmapCache( WeakPtr<file::VFS const> vfs );
+
+	WeakPtr<sprite::Bitmap const> Fetch( file::VFSPath const& path );
+
+private:
+	WeakPtr<file::VFS const> const mVfs;
+	CachedBitmaps mCachedBitmaps;
+};
+
+
+
+BitmapCache::BitmapCache( WeakPtr<file::VFS const> vfs )
+	: mVfs( vfs )
+{
+	//
+}
+
+
+WeakPtr<sprite::Bitmap const> BitmapCache::Fetch( file::VFSPath const& path )
+{
+	CachedBitmaps::const_iterator const iter = mCachedBitmaps.find( path );
+	if( iter != mCachedBitmaps.end() )
+	{
+		// Return cached
+		RF_ASSERT( iter->second != nullptr );
+		return iter->second;
+	}
+
+	// Load to cache
+	UniquePtr<sprite::Bitmap> bitmap = DefaultCreator<sprite::Bitmap>::Create( ExplicitDefaultConstruct{} );
+	WeakPtr<sprite::Bitmap const> retVal = bitmap;
+	{
+		file::FileHandlePtr const handle = mVfs->GetFileForRead( path );
+		RF_ASSERT( handle != nullptr );
+		file::FileBuffer const buffer( *handle, false );
+		*bitmap = sprite::BitmapReader::ReadRGBABitmap( buffer.GetData(), buffer.GetSize() );
+	}
+	mCachedBitmaps[path] = rftl::move( bitmap );
+	return retVal;
+}
+
 }
 ///////////////////////////////////////////////////////////////////////////////
 
 void Start()
 {
 	using namespace details;
-
-	file::VFS* vfs = app::gVfs;
 
 	file::VFSPath const charPieces = file::VFS::kRoot.GetChild( "assets", "textures", "char" );
 
@@ -147,47 +205,27 @@ void Start()
 
 	file::VFSPath const outDir = file::VFS::kRoot.GetChild( "scratch", "char", id );
 
-	// Load base texture
-	sprite::Bitmap baseTex( ExplicitDefaultConstruct{} );
-	{
-		file::FileBuffer const buffer( *vfs->GetFileForRead( basePieces ), false );
-		baseTex = sprite::BitmapReader::ReadRGBABitmap( buffer.GetData(), buffer.GetSize() );
-	}
+	details::BitmapCache bitmapCache( app::gVfs );
 
-	// Load clothing texture
-	sprite::Bitmap clothingTex( ExplicitDefaultConstruct{} );
-	{
-		file::FileBuffer const buffer( *vfs->GetFileForRead( clothingPieces ), false );
-		clothingTex = sprite::BitmapReader::ReadRGBABitmap( buffer.GetData(), buffer.GetSize() );
-	}
-
-	// Load hair texture
-	sprite::Bitmap hairTex( ExplicitDefaultConstruct{} );
-	{
-		file::FileBuffer const buffer( *vfs->GetFileForRead( hairPieces ), false );
-		hairTex = sprite::BitmapReader::ReadRGBABitmap( buffer.GetData(), buffer.GetSize() );
-	}
-
-	// Load species texture
-	sprite::Bitmap speciesTex( ExplicitDefaultConstruct{} );
-	{
-		file::FileBuffer const buffer( *vfs->GetFileForRead( speciesPieces ), false );
-		speciesTex = sprite::BitmapReader::ReadRGBABitmap( buffer.GetData(), buffer.GetSize() );
-	}
+	// Load textures
+	WeakPtr<sprite::Bitmap const> baseTex = bitmapCache.Fetch( basePieces );
+	WeakPtr<sprite::Bitmap const> clothingTex = bitmapCache.Fetch( clothingPieces );
+	WeakPtr<sprite::Bitmap const> hairTex = bitmapCache.Fetch( hairPieces );
+	WeakPtr<sprite::Bitmap const> speciesTex = bitmapCache.Fetch( speciesPieces );
 
 	details::CompositeFrameParams params = {};
-	params.mTileWidth = 16;
-	params.mTileHeight = 18;
-	params.mBaseTex = &baseTex;
-	params.mBaseRow = 1;
-	params.mClothingTex = &clothingTex;
-	params.mClothingRow = 13;
-	params.mHairTex = &hairTex;
-	params.mHairRow = 3;
-	params.mSpeciesTex = &speciesTex;
-	params.mSpeciesNearRow = 3;
-	params.mSpeciesFarRow = 4;
-	params.mSpeciesTailRow = 5;
+	params.mSequence.mTileWidth = 16;
+	params.mSequence.mTileHeight = 18;
+	params.mSequence.mBaseTex = baseTex;
+	params.mSequence.mBaseRow = 1;
+	params.mSequence.mClothingTex = clothingTex;
+	params.mSequence.mClothingRow = 13;
+	params.mSequence.mHairTex = hairTex;
+	params.mSequence.mHairRow = 3;
+	params.mSequence.mSpeciesTex = speciesTex;
+	params.mSequence.mSpeciesNearRow = 3;
+	params.mSequence.mSpeciesFarRow = 4;
+	params.mSequence.mSpeciesTailRow = 5;
 	size_t const startColumn = 12;
 
 	static constexpr size_t kBitmapFramesPerDirection = 3;
@@ -196,12 +234,14 @@ void Start()
 		"n0.bmp", "n1.bmp", "n2.bmp",
 		"e0.bmp", "e1.bmp", "e2.bmp",
 		"s0.bmp", "s1.bmp", "s2.bmp",
-		"w0.bmp", "w1.bmp", "w2.bmp" };
+		"w0.bmp", "w1.bmp", "w2.bmp"
+	};
 	static constexpr size_t kColumnOffsets[kTotalFrames] = {
 		0, 1, 2,
 		4, 5, 6,
 		8, 9, 10,
-		12, 13, 14 };
+		12, 13, 14
+	};
 
 	// Write frames to disk
 	for( size_t i = 0; i < kTotalFrames; i++ )
@@ -213,14 +253,17 @@ void Start()
 	static constexpr size_t kAnimFramesPerDirection = 4;
 	static constexpr size_t kTotalFramepacks = 4;
 	static constexpr char const* kFramepackNames[kTotalFramepacks] = {
-		"n.fpack", "e.fpack", "s.fpack", "w.fpack" };
+		"n.fpack", "e.fpack", "s.fpack", "w.fpack"
+	};
 	static constexpr size_t kFramepackSourceFrames[kTotalFramepacks][kAnimFramesPerDirection] = {
 		{ 0, 1, 2, 1 },
 		{ 3, 4, 5, 4 },
 		{ 6, 7, 8, 7 },
-		{ 9, 10, 11, 10 } };
+		{ 9, 10, 11, 10 }
+	};
 
 	// Create framepacks
+	file::VFS* const vfs = app::gVfs;
 	gfx::PPUController* const ppu = app::gGraphics;
 	gfx::TextureManager& texMan = *ppu->DebugGetTextureManager();
 	for( size_t i_framepack = 0; i_framepack < kTotalFramepacks; i_framepack++ )
