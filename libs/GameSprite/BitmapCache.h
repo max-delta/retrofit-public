f9e783fc1a689390953b12a870ad2a81047f1c7e
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-05-14 16:00:12 -0700

    Minor commenting

--- libs/GameSprite/BitmapCache.h
+++ libs/GameSprite/BitmapCache.h
@@ -15,14 +15,22 @@ class GAMESPRITE_API BitmapCache
 {
 	RF_NO_COPY( BitmapCache );
 
+	//
+	// Types and constants
 private:
 	using CachedBitmaps = rftl::unordered_map<file::VFSPath, UniquePtr<sprite::Bitmap>>;
 
+
+	//
+	// Public methods
 public:
 	explicit BitmapCache( WeakPtr<file::VFS const> vfs );
 
 	WeakPtr<sprite::Bitmap const> Fetch( file::VFSPath const& path );
 
+
+	//
+	// Private data
 private:
 	WeakPtr<file::VFS const> const mVfs;
 	CachedBitmaps mCachedBitmaps;
