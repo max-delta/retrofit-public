4adef4a7ec7fe5a376a591743093038cc47c1064
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-05-20 20:34:52 -0700

    More minclude tests

---
+++ apps/core_test/minclude_weak.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+
+#include "core/ptr/weak_ptr.h"
+namespace {
+
+RF_CLANG_IGNORE( "-Wunused-function" );
+
+class ForwardedClass;
+
+// Do not need type to be defined to destroy it
+void test_destroy( RF::WeakPtr<ForwardedClass>&& wptr )
+{
+	wptr = nullptr;
+}
+
+// Do not need type to be defined to assign it
+void test_assign( RF::WeakPtr<ForwardedClass>&& wptr )
+{
+	RF::WeakPtr<ForwardedClass> same{ wptr };
+	same = wptr;
+
+	RF::WeakPtr<ForwardedClass const> cv{ wptr };
+	cv = wptr;
+}
+
+}
