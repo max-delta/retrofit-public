97e5e56510d75b7cbb78522cfb01e3b60be89050
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-05-21 16:08:17 -0700

    Basic character sanitization

--- apps/cc3o3/char/CharacterValidator.cpp
+++ apps/cc3o3/char/CharacterValidator.cpp
@@ -1,12 +1,18 @@
 #include "stdafx.h"
 #include "CharacterValidator.h"
 
+#include "cc3o3/char/Character.h"
+
+#include "GameSprite/CharacterCreator.h"
+
 #include "PlatformFilesystem/VFS.h"
 #include "PlatformFilesystem/FileBuffer.h"
 
 #include "Serialization/CsvReader.h"
 
 #include "core_math/math_casts.h"
+
+#include "rftl/sstream"
 
 
 namespace RF { namespace cc { namespace character {
@@ -26,7 +32,7 @@ bool CharacterValidator::LoadGeneticsTable( file::VFSPath const& geneticsTablePa
 	rftl::deque<rftl::deque<rftl::string>> geneticsTable = LoadCSV( geneticsTablePath );
 	if( geneticsTable.empty() )
 	{
-		RFLOG_NOTIFY( geneticsTablePath, RFCAT_CC3O3, "Failed to load genetics table file" );
+		RFLOG_NOTIFY( geneticsTablePath, RFCAT_CHAR, "Failed to load genetics table file" );
 		return false;
 	}
 
@@ -42,7 +48,7 @@ bool CharacterValidator::LoadGeneticsTable( file::VFSPath const& geneticsTablePa
 		header.at( 3 ) != "playable" ||
 		header.at( 4 ) != "9-char" )
 	{
-		RFLOG_NOTIFY( geneticsTablePath, RFCAT_CC3O3, "Malformed genetics table header, expected 'id, species, gender, playable, 9-char'" );
+		RFLOG_NOTIFY( geneticsTablePath, RFCAT_CHAR, "Malformed genetics table header, expected 'id, species, gender, playable, 9-char'" );
 		return false;
 	}
 	line++;
@@ -52,7 +58,7 @@ bool CharacterValidator::LoadGeneticsTable( file::VFSPath const& geneticsTablePa
 		rftl::deque<rftl::string> const entry = geneticsTable.front();
 		if( entry.size() != 5 )
 		{
-			RFLOG_NOTIFY( geneticsTablePath, RFCAT_CC3O3, "Malformed genetics entry at line %i, expected 5 columns", math::integer_cast<int>( line ) );
+			RFLOG_NOTIFY( geneticsTablePath, RFCAT_CHAR, "Malformed genetics entry at line %i, expected 5 columns", math::integer_cast<int>( line ) );
 			return false;
 		}
 		rftl::string const& id = entry.at( 0 );
@@ -63,19 +69,19 @@ bool CharacterValidator::LoadGeneticsTable( file::VFSPath const& geneticsTablePa
 
 		if( id.empty() )
 		{
-			RFLOG_NOTIFY( geneticsTablePath, RFCAT_CC3O3, "Malformed genetics entry at line %i, not a valid id", math::integer_cast<int>( line ) );
+			RFLOG_NOTIFY( geneticsTablePath, RFCAT_CHAR, "Malformed genetics entry at line %i, not a valid id", math::integer_cast<int>( line ) );
 			return false;
 		}
 
 		if( species.empty() )
 		{
-			RFLOG_NOTIFY( geneticsTablePath, RFCAT_CC3O3, "Malformed genetics entry at line %i, not a valid species", math::integer_cast<int>( line ) );
+			RFLOG_NOTIFY( geneticsTablePath, RFCAT_CHAR, "Malformed genetics entry at line %i, not a valid species", math::integer_cast<int>( line ) );
 			return false;
 		}
 
 		if( gender.empty() )
 		{
-			RFLOG_NOTIFY( geneticsTablePath, RFCAT_CC3O3, "Malformed genetics entry at line %i, not a valid gender", math::integer_cast<int>( line ) );
+			RFLOG_NOTIFY( geneticsTablePath, RFCAT_CHAR, "Malformed genetics entry at line %i, not a valid gender", math::integer_cast<int>( line ) );
 			return false;
 		}
 
@@ -90,13 +96,13 @@ bool CharacterValidator::LoadGeneticsTable( file::VFSPath const& geneticsTablePa
 		}
 		else
 		{
-			RFLOG_NOTIFY( geneticsTablePath, RFCAT_CC3O3, "Malformed genetics entry at line %i, not a valid playability", math::integer_cast<int>( line ) );
+			RFLOG_NOTIFY( geneticsTablePath, RFCAT_CHAR, "Malformed genetics entry at line %i, not a valid playability", math::integer_cast<int>( line ) );
 			return false;
 		}
 
 		if( nineChar.empty() )
 		{
-			RFLOG_NOTIFY( geneticsTablePath, RFCAT_CC3O3, "Malformed genetics entry at line %i, not a valid 9-char", math::integer_cast<int>( line ) );
+			RFLOG_NOTIFY( geneticsTablePath, RFCAT_CHAR, "Malformed genetics entry at line %i, not a valid 9-char", math::integer_cast<int>( line ) );
 			return false;
 		}
 
@@ -104,6 +110,7 @@ bool CharacterValidator::LoadGeneticsTable( file::VFSPath const& geneticsTablePa
 		newEntry.mSpecies = species;
 		newEntry.mGender = gender;
 		newEntry.mPlayable = playable;
+		newEntry.m9Char = nineChar;
 		mGeneticsStorage[id] = rftl::move( newEntry );
 
 		geneticsTable.pop_front();
@@ -115,16 +122,387 @@ bool CharacterValidator::LoadGeneticsTable( file::VFSPath const& geneticsTablePa
 
 
 
+rftl::unordered_set<CharacterValidator::GeneticsID> CharacterValidator::GetGeneticsIDs() const
+{
+	rftl::unordered_set<CharacterValidator::GeneticsID> retVal;
+	for( GeneticsStorage::value_type const& pair : mGeneticsStorage )
+	{
+		retVal.emplace( pair.first );
+	}
+	return retVal;
+}
+
+
+
+GeneticsEntry const& CharacterValidator::GetGeneticEntry( GeneticsID const& id ) const
+{
+	return mGeneticsStorage.at( id );
+}
+
+
+
+bool CharacterValidator::LoadStatBonusesTable( file::VFSPath const& statBonusesTablePath )
+{
+	rftl::deque<rftl::deque<rftl::string>> statBonusesTable = LoadCSV( statBonusesTablePath );
+	if( statBonusesTable.empty() )
+	{
+		RFLOG_NOTIFY( statBonusesTablePath, RFCAT_CHAR, "Failed to load stat bonuses table file" );
+		return false;
+	}
+
+	size_t line = 0;
+
+	rftl::deque<rftl::string> const header = statBonusesTable.front();
+	statBonusesTable.pop_front();
+	if(
+		header.size() != 9 ||
+		header.at( 0 ) != "species" ||
+		header.at( 1 ) != "mhealth" ||
+		header.at( 2 ) != "physatk" ||
+		header.at( 3 ) != "physdef" ||
+		header.at( 4 ) != "elematk" ||
+		header.at( 5 ) != "elemdef" ||
+		header.at( 6 ) != "balance" ||
+		header.at( 7 ) != "techniq" ||
+		header.at( 8 ) != "elempwr" )
+	{
+		RFLOG_NOTIFY( statBonusesTablePath, RFCAT_CHAR, "Malformed stat bonuses table header, expected 'mhealth, physatk, physdef, elematk, elemdef, balance, techniq, elempwr'" );
+		return false;
+	}
+	line++;
+
+	while( statBonusesTable.empty() == false )
+	{
+		rftl::deque<rftl::string> const entry = statBonusesTable.front();
+		if( entry.size() != 9 )
+		{
+			RFLOG_NOTIFY( statBonusesTablePath, RFCAT_CHAR, "Malformed stat bonuses entry at line %i, expected 9 columns", math::integer_cast<int>( line ) );
+			return false;
+		}
+		rftl::string const& species = entry.at( 0 );
+		rftl::string const& mhealthString = entry.at( 1 );
+		rftl::string const& physatkString = entry.at( 2 );
+		rftl::string const& physdefString = entry.at( 3 );
+		rftl::string const& elematkString = entry.at( 4 );
+		rftl::string const& elemdefString = entry.at( 5 );
+		rftl::string const& balanceString = entry.at( 6 );
+		rftl::string const& techniqString = entry.at( 7 );
+		rftl::string const& elempwrString = entry.at( 8 );
+
+		if( species.empty() )
+		{
+			RFLOG_NOTIFY( statBonusesTablePath, RFCAT_CHAR, "Malformed stat bonuses entry at line %i, not a valid species", math::integer_cast<int>( line ) );
+			return false;
+		}
+
+		rftl::stringstream ss;
+
+		static constexpr auto load = []( rftl::stringstream& ss, rftl::string const& str, int8_t& val ) -> bool
+		{
+			ss.clear();
+			ss << str;
+			int16_t notAChar; // C++ I/O is stupid
+			ss >> notAChar;
+			val = math::integer_cast<int8_t>( notAChar );
+			if( val < Stats::kMinModifier || val > Stats::kMaxModifier )
+			{
+				return false;
+			}
+			return true;
+		};
+
+		Stats stats = {};
+		bool validStats =
+			load( ss, mhealthString, stats.mMHealth ) &&
+			load( ss, physatkString, stats.mPhysAtk ) &&
+			load( ss, physdefString, stats.mPhysDef ) &&
+			load( ss, elematkString, stats.mElemAtk ) &&
+			load( ss, elemdefString, stats.mElemDef ) &&
+			load( ss, balanceString, stats.mBalance ) &&
+			load( ss, techniqString, stats.mTechniq ) &&
+			load( ss, elempwrString, stats.mElemPwr );
+		if( validStats == false )
+		{
+			RFLOG_NOTIFY( statBonusesTablePath, RFCAT_CHAR, "Malformed stat bonuses entry at line %i, not a valid modifier", math::integer_cast<int>( line ) );
+			return false;
+		}
+
+		mStatBonuesStorage[species] = rftl::move( stats );
+
+		statBonusesTable.pop_front();
+		line++;
+	}
+
+	return true;
+}
+
+
+
+Stats const& CharacterValidator::GetStatBonuses( SpeciesID const& id ) const
+{
+	return mStatBonuesStorage.at( id );
+}
+
+
+
 void CharacterValidator::SanitizeForCharacterCreation( Character& character ) const
 {
-	RF_TODO_BREAK();
+	Description& description = character.mDescription;
+	Genetics& genetics = character.mGenetics;
+	Visuals& visuals = character.mVisuals;
+	Stats& stats = character.mStats;
+
+	// Name
+	if( description.mName.empty() )
+	{
+		RFLOG_WARNING( nullptr, RFCAT_CHAR, "Invalid name" );
+		description.mName = "CHARACTERNAME";
+	}
+
+	// TODO: Check for valid titles from a list
+	if( description.mTitle.empty() == false )
+	{
+		RFLOG_WARNING( nullptr, RFCAT_CHAR, "Invalid title" );
+		description.mTitle = "";
+	}
+
+	// Species
+	{
+		RF_ASSERT( mGeneticsStorage.empty() == false );
+		bool validSpecies = false;
+		rftl::string fallbackSpecies;
+		for( GeneticsStorage::value_type const& pair : mGeneticsStorage )
+		{
+			if( pair.second.mPlayable == false )
+			{
+				continue;
+			}
+
+			if( fallbackSpecies.empty() )
+			{
+				fallbackSpecies = pair.second.mSpecies;
+			}
+
+			if( genetics.mSpecies == pair.second.mSpecies )
+			{
+				validSpecies = true;
+				break;
+			}
+		}
+		if( validSpecies == false )
+		{
+			RFLOG_WARNING( nullptr, RFCAT_CHAR, "Invalid species" );
+			RF_ASSERT( fallbackSpecies.empty() == false );
+			genetics.mSpecies = fallbackSpecies;
+		}
+	}
+
+	// Gender
+	{
+		RF_ASSERT( mGeneticsStorage.empty() == false );
+		bool validGender = false;
+		rftl::string fallbackGender;
+		for( GeneticsStorage::value_type const& pair : mGeneticsStorage )
+		{
+			if( pair.second.mPlayable == false )
+			{
+				continue;
+			}
+
+			if( pair.second.mSpecies != genetics.mSpecies )
+			{
+				continue;
+			}
+
+			if( fallbackGender.empty() )
+			{
+				fallbackGender = pair.second.mGender;
+			}
+
+			if( genetics.mGender == pair.second.mGender )
+			{
+				validGender = true;
+				break;
+			}
+		}
+		if( validGender == false )
+		{
+			RFLOG_WARNING( nullptr, RFCAT_CHAR, "Invalid gender" );
+			RF_ASSERT( fallbackGender.empty() == false );
+			genetics.mGender = fallbackGender;
+		}
+	}
+
+	// Visuals
+	{
+		sprite::CharacterCreator const& charCreate = *mCharacterCreator;
+		sprite::CharacterCreator::TagBits const tagBits =
+			charCreate.GetTagsAsFlags(
+				{ genetics.mSpecies, genetics.mGender } );
+
+		static constexpr auto sanitizePiece = [](
+												  sprite::CharacterCreator const& charCreate,
+												  sprite::CharacterCreator::TagBits const& tagBits,
+												  rftl::string& targetPiece,
+												  sprite::CharacterPieceType const pieceType ) -> void {
+			bool validPiece = false;
+			sprite::CharacterCreator::PieceId fallbackPiece;
+			sprite::CharacterCreator::PieceId iter = charCreate.IterateNextValidPiece( {}, pieceType, tagBits );
+			RF_ASSERT( iter.empty() == false );
+			while( iter.empty() == false )
+			{
+				if( fallbackPiece.empty() )
+				{
+					fallbackPiece = iter;
+				}
+
+				if( targetPiece == iter )
+				{
+					validPiece = true;
+					break;
+				}
+
+				iter = charCreate.IterateNextValidPiece( iter, pieceType, tagBits );
+				continue;
+			}
+			if( validPiece == false )
+			{
+				RFLOG_WARNING( nullptr, RFCAT_CHAR, "Invalid piece" );
+				RF_ASSERT( fallbackPiece.empty() == false );
+				targetPiece = fallbackPiece;
+			}
+		};
+
+		sanitizePiece( charCreate, tagBits, visuals.mBase, sprite::CharacterPieceType::Base );
+		sanitizePiece( charCreate, tagBits, visuals.mClothing, sprite::CharacterPieceType::Clothing );
+		sanitizePiece( charCreate, tagBits, visuals.mHair, sprite::CharacterPieceType::Hair );
+		sanitizePiece( charCreate, tagBits, visuals.mSpecies, sprite::CharacterPieceType::Species );
+	}
+
+	// Stats
+	{
+		Stats sanitizedStats = GetStatBonuses( genetics.mSpecies );
+		int16_t availablePoints =
+			kMaxTotalStatPoints +
+			-sanitizedStats.mMHealth +
+			-sanitizedStats.mPhysAtk +
+			-sanitizedStats.mPhysDef +
+			-sanitizedStats.mElemAtk +
+			-sanitizedStats.mElemDef +
+			-sanitizedStats.mBalance +
+			-sanitizedStats.mTechniq +
+			-sanitizedStats.mElemPwr;
+		RF_ASSERT( availablePoints >= 0 );
+		while( availablePoints > 0 )
+		{
+			static constexpr auto siphon = []( int16_t& reserve, int8_t& cur, int8_t const& desire ) -> bool
+			{
+				if(
+					reserve > 0 &&
+					cur < desire &&
+					cur < kMaxPointsPerStat )
+				{
+					cur++;
+					reserve--;
+					return true;
+				}
+				return false;
+			};
+
+			Stats const& desiredStats = stats;
+			bool const siphoned =
+				siphon( availablePoints, sanitizedStats.mMHealth, desiredStats.mMHealth ) ||
+				siphon( availablePoints, sanitizedStats.mPhysAtk, desiredStats.mPhysAtk ) ||
+				siphon( availablePoints, sanitizedStats.mPhysDef, desiredStats.mPhysDef ) ||
+				siphon( availablePoints, sanitizedStats.mElemAtk, desiredStats.mElemAtk ) ||
+				siphon( availablePoints, sanitizedStats.mElemDef, desiredStats.mElemDef ) ||
+				siphon( availablePoints, sanitizedStats.mBalance, desiredStats.mBalance ) ||
+				siphon( availablePoints, sanitizedStats.mTechniq, desiredStats.mTechniq ) ||
+				siphon( availablePoints, sanitizedStats.mElemPwr, desiredStats.mElemPwr );
+			if( siphoned )
+			{
+				continue;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		stats = sanitizedStats;
+	}
 }
 
 
 
 void CharacterValidator::SanitizeForGameplay( Character& character ) const
 {
-	RF_TODO_BREAK();
+	Stats& stats = character.mStats;
+	Equipment& equipment = character.mEquipment;
+	ElementSlots& innateElements = character.mInnateElements;
+	ElementSlots& equippedElements = character.mEquippedElements;
+
+	SanitizeForCharacterCreation( character );
+
+	// Stats
+	{
+		int16_t availablePoints =
+			kMaxTotalStatPoints +
+			-stats.mMHealth +
+			-stats.mPhysAtk +
+			-stats.mPhysDef +
+			-stats.mElemAtk +
+			-stats.mElemDef +
+			-stats.mBalance +
+			-stats.mTechniq +
+			-stats.mElemPwr;
+		RF_ASSERT( availablePoints >= 0 );
+		while( availablePoints > 0 )
+		{
+			static constexpr auto grow = []( int16_t& reserve, int8_t& dest ) -> bool
+			{
+				if(
+					reserve > 0 &&
+					dest < kMaxPointsPerStat )
+				{
+					dest++;
+					reserve--;
+					return true;
+				}
+				return false;
+			};
+
+			// NOTE: Health intentionally ommitted
+			// NOTE: Ordered to favor 'simplest' stats for padding
+			bool const grown =
+				grow( availablePoints, stats.mPhysDef ) ||
+				grow( availablePoints, stats.mPhysAtk ) ||
+				grow( availablePoints, stats.mElemDef ) ||
+				grow( availablePoints, stats.mElemAtk ) ||
+				grow( availablePoints, stats.mElemPwr ) ||
+				grow( availablePoints, stats.mBalance ) ||
+				grow( availablePoints, stats.mTechniq );
+			if( grown )
+			{
+				RFLOG_WARNING( nullptr, RFCAT_CHAR, "Had to pad some stats since not all points were allocated" );
+				continue;
+			}
+			else
+			{
+				RF_ASSERT( availablePoints == 0 );
+				break;
+			}
+		}
+	}
+
+	// TODO: Check for valid equipment from a list
+	equipment = {};
+
+	// TODO: Check for valid innates from a list
+	innateElements = {};
+
+	// TODO: Check for valid elements from a list
+	equippedElements = {};
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -134,14 +512,14 @@ rftl::deque<rftl::deque<rftl::string>> CharacterValidator::LoadCSV( file::VFSPat
 	file::FileHandlePtr const handle = mVfs->GetFileForRead( path );
 	if( handle == nullptr )
 	{
-		RFLOG_NOTIFY( path, RFCAT_CC3O3, "Failed to get file for read" );
+		RFLOG_NOTIFY( path, RFCAT_CHAR, "Failed to get file for read" );
 		return {};
 	}
 
 	file::FileBuffer const buffer{ *handle.Get(), false };
 	if( buffer.GetData() == nullptr )
 	{
-		RFLOG_NOTIFY( path, RFCAT_CC3O3, "Failed to get data from file buffer" );
+		RFLOG_NOTIFY( path, RFCAT_CHAR, "Failed to get data from file buffer" );
 		return {};
 	}
 
@@ -150,7 +528,7 @@ rftl::deque<rftl::deque<rftl::string>> CharacterValidator::LoadCSV( file::VFSPat
 	rftl::deque<rftl::deque<rftl::string>> const csv = serialization::CsvReader::TokenizeToDeques( data, size );
 	if( csv.empty() )
 	{
-		RFLOG_NOTIFY( path, RFCAT_CC3O3, "Failed to read file as csv" );
+		RFLOG_NOTIFY( path, RFCAT_CHAR, "Failed to read file as csv" );
 		return {};
 	}
 
--- apps/cc3o3/char/CharacterValidator.h
+++ apps/cc3o3/char/CharacterValidator.h
@@ -2,6 +2,7 @@
 #include "project.h"
 
 #include "cc3o3/char/CharFwd.h"
+#include "cc3o3/char/Stats.h"
 
 #include "GameSprite/SpriteFwd.h"
 
@@ -9,9 +10,10 @@
 
 #include "core/ptr/weak_ptr.h"
 
-#include "rftl/extension/immutable_string.h"
 #include "rftl/unordered_map"
+#include "rftl/unordered_set"
 #include "rftl/deque"
+#include "rftl/string"
 
 
 namespace RF { namespace cc { namespace character {
@@ -32,11 +34,15 @@ class CharacterValidator
 	RF_NO_COPY( CharacterValidator );
 
 	//
-	// Types
+	// Types and constants
 public:
-	using GeneticsID = rftl::immutable_string;
+	using GeneticsID = rftl::string;
+	using SpeciesID = rftl::string;
 private:
 	using GeneticsStorage = rftl::unordered_map<GeneticsID, GeneticsEntry>;
+	using StatBonuesStorage = rftl::unordered_map<SpeciesID, Stats>;
+	static constexpr int8_t kMaxTotalStatPoints = 13;
+	static constexpr int8_t kMaxPointsPerStat = 5;
 
 
 	//
@@ -45,6 +51,11 @@ public:
 	CharacterValidator( WeakPtr<file::VFS const> vfs, WeakPtr<sprite::CharacterCreator const> characterCreator );
 
 	bool LoadGeneticsTable( file::VFSPath const& geneticsTablePath );
+	rftl::unordered_set<GeneticsID> GetGeneticsIDs() const;
+	GeneticsEntry const& GetGeneticEntry( GeneticsID const& id ) const;
+
+	bool LoadStatBonusesTable( file::VFSPath const& statBonusesTablePath );
+	Stats const& GetStatBonuses( SpeciesID const& id ) const;
 
 	void SanitizeForCharacterCreation( Character& character ) const;
 	void SanitizeForGameplay( Character& character ) const;
@@ -65,7 +76,13 @@ private:
 	//  then re-used by the various systems that need it
 	WeakPtr<sprite::CharacterCreator const> mCharacterCreator;
 
+	// TODO: This should probably be seperated and then re-used by the various
+	//  systems that need it
 	GeneticsStorage mGeneticsStorage;
+
+	// TODO: This should probably be seperated and then re-used by the various
+	//  systems that need it
+	StatBonuesStorage mStatBonuesStorage;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
