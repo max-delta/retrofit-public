220c89d372e7407a4eab660ad492be9502e9b14b
Max Delta <Max Delta@localhost>
2019-05-21 16:09:53 -0700

    Missed a file for logging

--- apps/cc3o3/project.h
+++ apps/cc3o3/project.h
@@ -3,3 +3,6 @@
 
 constexpr char const* RFCAT_CC3O3 = "CC3O3";
 constexpr uint64_t RFCAT_CC3O3_SEV_WHITELIST = RF::logging::kDefaultSeverityMask;
+
+constexpr char const* RFCAT_CHAR = "Character";
+constexpr uint64_t RFCAT_CHAR_SEV_WHITELIST = RF::logging::kDefaultSeverityMask;
