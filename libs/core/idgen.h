8fa6b464c28c18bb1c42719eb822dd70d1a95a65
Max Delta <Max Delta@localhost>
2019-08-18 13:40:19 -0700

    Fixes for idgen

--- libs/core/idgen.h
+++ libs/core/idgen.h
@@ -24,10 +24,14 @@ public:
 	static constexpr ID kInvalid = rftl::numeric_limits<ID>::min();
 	static constexpr ID kInitial = kInvalid + 1;
 	static_assert( kInvalid != kInitial, "Unexpected incrementation behavior" );
-	static constexpr ID kFinal = rftl::numeric_limits<ID>::max();
+	static constexpr ID kFinal = rftl::numeric_limits<ID>::max() - 1u;
+
 private:
 	using Storage = rftl::atomic<ID>;
-	static constexpr bool kRolloverUnrealistic = kInitial > 0 || kFinal < rftl::numeric_limits<int64_t>::max();
+	static constexpr bool kRolloverUnrealistic =
+		kInitial >= 0 &&
+		kInitial <= rftl::numeric_limits<int8_t>::max() &&
+		kFinal >= rftl::numeric_limits<int64_t>::max();
 
 
 	//
@@ -46,16 +50,24 @@ public:
 		{
 			// May be able to rollover, check and prevent that
 			ID const currentID = mStorage.load( rftl::memory_order::memory_order_acquire );
-			if( currentID == kFinal )
+			static_assert( kFinal + 1 > kFinal, "Unexpected rollover" );
+			if( currentID > kFinal )
 			{
 				RF_DBGFAIL_MSG( "Prevented ID generation roll-over" );
 				return kInvalid;
 			}
 		}
 
-		ID newID = mStorage.fetch_add( 1, rftl::memory_order::memory_order_acq_rel );
+		ID const newID = mStorage.fetch_add( 1, rftl::memory_order::memory_order_acq_rel );
 		RF_ASSERT_MSG( newID != kInvalid, "Invalid ID generation, probabaly from roll-over" );
 		return newID;
+	}
+
+	ID GetLastGeneratedID() const
+	{
+		ID const nextID = mStorage.load( rftl::memory_order::memory_order_acquire );
+		RF_ASSERT_MSG( nextID > kInvalid, "Invalid ID generation, probabaly from roll-over" );
+		return nextID - 1;
 	}
 
 
