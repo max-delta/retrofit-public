003ab7f3fdd5451b00e134392e4e346d6e3c1f21
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-08-20 17:18:35 -0700

    More ObjectManager implementation

--- apps/core_test/component_objectmanager.cpp
+++ apps/core_test/component_objectmanager.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 
 #include "core_component/ObjectManager.h"
+#include "core_component/ObjectRef.h"
 
 
 namespace RF { namespace component {
@@ -8,15 +9,46 @@ namespace RF { namespace component {
 
 TEST( ObjectManager, Empty )
 {
-	ObjectManager const manager( 1 );
+	static constexpr ManagerIdentifier kMan = 1;
+
+	ObjectManager const manager( kMan );
 
 	ObjectManager::InternalStateIteration const initialState = manager.GetInternalStateIteration();
 
-	ASSERT_EQ( manager.GetManagerIdentifier(), 1 );
+	ASSERT_EQ( manager.GetManagerIdentifier(), kMan );
 	ASSERT_FALSE( manager.IsValidObject( kInvalidObjectIdentifier ) );
 	ASSERT_FALSE( manager.IsValidComponent( kInvalidObjectIdentifier, kInvalidResolvedComponentType ) );
 
 	ASSERT_EQ( manager.GetInternalStateIteration(), initialState );
+}
+
+
+
+TEST( ObjectManager, BasicAddRemoveObject )
+{
+	static constexpr ManagerIdentifier kMan = 1;
+	static constexpr ScopeIdentifier kScope = 3;
+
+	ObjectManager manager( kMan, kScope );
+	ObjectManager::InternalStateIteration latestState = manager.GetInternalStateIteration();
+
+	ObjectRef const newObj = manager.AddObject();
+	ASSERT_NE( manager.GetInternalStateIteration(), latestState );
+	latestState = manager.GetInternalStateIteration();
+	ASSERT_EQ( ObjectManager::GetManagerIdentifierFromObjectIdentifier( newObj.GetIdentifier() ), kMan );
+	ASSERT_EQ( ObjectManager::GetGenerationScopeFromObjectIdentifier( newObj.GetIdentifier() ), kScope );
+
+	bool const validPostAdd = manager.IsValidObject( newObj.GetIdentifier() );
+	ASSERT_EQ( manager.GetInternalStateIteration(), latestState );
+	ASSERT_TRUE( validPostAdd );
+
+	bool const removeSuccess = manager.RemoveObject( newObj.GetIdentifier() );
+	ASSERT_NE( manager.GetInternalStateIteration(), latestState );
+	latestState = manager.GetInternalStateIteration();
+
+	bool const validPostRemove = manager.IsValidObject( newObj.GetIdentifier() );
+	ASSERT_EQ( manager.GetInternalStateIteration(), latestState );
+	ASSERT_FALSE( validPostRemove );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
--- libs/core_component/ObjectManager.h
+++ libs/core_component/ObjectManager.h
@@ -17,11 +17,18 @@ class ObjectManager
 	// Types
 public:
 	using InternalStateIteration = uint64_t;
+	static constexpr ScopeIdentifier kDefaultScopeIdentifier = kInvalidScopeIdentifier + 1;
+
+private:
+	using ScopedObjectIdentifier = uint32_t;
+	using ObjectSet = rftl::unordered_set<ObjectIdentifier>;
 
 
 	//
 	// Public methods
 public:
+	ObjectManager() = delete;
+	ObjectManager( ManagerIdentifier identifier, ScopeIdentifier generationScope );
 	ObjectManager( ManagerIdentifier identifier );
 
 	// Manager identifiers used to detect handles being improperly shared
@@ -29,16 +36,25 @@ public:
 	ManagerIdentifier GetManagerIdentifier() const;
 	static ManagerIdentifier GetManagerIdentifierFromObjectIdentifier( ObjectIdentifier identifier );
 
+	// Scope identifiers constrain what object identifiers managers are allowed
+	//  to generate, allowing managers to safely take responsibility for object
+	//  identifiers that they would not have generated on their own
+	// NOTE: Intended for use in multiplayer scenarios
+	ScopeIdentifier GetGenerationScope() const;
+	static ScopeIdentifier GetGenerationScopeFromObjectIdentifier( ObjectIdentifier identifier );
+
 	// Object management
 	bool IsValidObject( ObjectIdentifier identifier ) const;
 	ObjectRef GetObject( ObjectIdentifier identifier ) const;
-	ObjectRef AddObject();
+	MutableObjectRef GetMutableObject( ObjectIdentifier identifier );
+	MutableObjectRef AddObject();
 	bool RemoveObject( ObjectIdentifier identifier );
 
 	// Component management
 	bool IsValidComponent( ObjectIdentifier identifier, ResolvedComponentType componentType ) const;
 	ComponentRef GetComponent( ObjectIdentifier identifier, ResolvedComponentType componentType ) const;
-	ComponentRef AddUninitializedComponent( ObjectIdentifier identifier, ResolvedComponentType componentType );
+	MutableComponentRef GetMutableComponent( ObjectIdentifier identifier, ResolvedComponentType componentType ) const;
+	MutableComponentRef AddUninitializedComponent( ObjectIdentifier identifier, ResolvedComponentType componentType );
 	bool RemoveComponent( ObjectIdentifier identifier, ResolvedComponentType componentType );
 
 	// View management
@@ -53,10 +69,52 @@ public:
 
 
 	//
+	// Restricted methods
+public:
+	struct ConversionHelpers
+	{
+	private:
+		friend class ObjectRef;
+		friend class MutableObjectRef;
+		friend class ComponentRef;
+		friend class MutableComponentRef;
+
+		static ObjectRef CreateObjectRef( MutableObjectRef const& object );
+		static ObjectRef CreateObjectRef( ComponentRef const& component );
+		static MutableObjectRef CreateMutableObjectRef( MutableComponentRef const& component );
+
+		static ComponentRef CreateComponentRef( MutableComponentRef const& component );
+		static ComponentRef CreateComponentRef( ObjectRef const& object, ResolvedComponentType componentType );
+		static MutableComponentRef CreateMutableComponentRef( MutableObjectRef const& object, ResolvedComponentType componentType );
+	};
+
+
+	//
+	// Private methods
+private:
+	void RevState();
+
+	static ObjectIdentifier CompositeObjectIdentifier(
+		ManagerIdentifier managerIdentifier,
+		ScopeIdentifier scopeIdentifier,
+		ScopedObjectIdentifier scopedIdentifier );
+	ObjectIdentifier GenerateNewObjectIdentifier();
+
+	bool DoesObjectExistInternal( ObjectIdentifier identifier ) const;
+	bool CreateNewObjectInternal( ObjectIdentifier identifier );
+	bool RemoveExistingObjectInternal( ObjectIdentifier identifier );
+
+
+	//
 	// Private data
 private:
-	ManagerIdentifier const mIdentifier;
+	ManagerIdentifier const mManagerIdentifier;
 	NonloopingIDGenerator<InternalStateIteration> mInternalStateIteration;
+
+	ScopeIdentifier const mGenerationScope;
+	NonloopingIDGenerator<ScopedObjectIdentifier> mObjectIDGenerator;
+
+	ObjectSet mValidObjects;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
