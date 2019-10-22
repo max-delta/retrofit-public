9d1c1c90b9c0f56d7bba0f19557792c761f1a23d
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-10-22 06:15:30 +0000

    Added stub GameSync library

---
+++ libs/GameSync/project.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Logging/Constants.h"
+
+#ifndef RF_NO_SHARED_EXPORTS
+#ifdef GAMESYNC_EXPORTS
+#define GAMESYNC_API __declspec( dllexport )
+#else
+#define GAMESYNC_API __declspec( dllimport )
+#endif
+#else
+#define GAMESYNC_API
+#endif
+
+constexpr char const* RFCAT_GAMESYNC = "Sync";
+constexpr uint64_t RFCAT_GAMESYNC_SEV_WHITELIST = RF::logging::kDefaultSeverityMask;
