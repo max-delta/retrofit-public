8524844e9c83c4378502e158278f28ca91a98234
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-12-23 04:21:29 +0000

    Add option to disable pausing on test failure

--- apps/foundation_test/main.cpp
+++ apps/foundation_test/main.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "core/macros.h"
 
+#include "rftl/string"
+
 
 namespace RF {
 ///////////////////////////////////////////////////////////////////////////////
@@ -20,7 +22,14 @@ int main( int argc, char** argv )
 	int retVal = RUN_ALL_TESTS();
 	if( retVal != 0 )
 	{
-		system( "pause" );
+		if( argc >= 2 && rftl::string( argv[1] ) == "--nopause" )
+		{
+			fputs( "One or more tests failed", stderr );
+		}
+		else
+		{
+			system( "pause" );
+		}
 	}
 	return retVal;
 }
