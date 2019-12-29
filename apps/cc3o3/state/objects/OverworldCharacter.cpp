3be74c02143fd2e75bd111aa72493ce64e128131
Max Delta <54338561+max-delta@users.noreply.github.com>
2019-12-29 21:52:11 +0000

    Load framepacks for overworld characters

--- apps/cc3o3/state/objects/OverworldCharacter.cpp
+++ apps/cc3o3/state/objects/OverworldCharacter.cpp
@@ -1,13 +1,19 @@
 #include "stdafx.h"
 #include "OverworldCharacter.h"
 
+#include "cc3o3/Common.h"
+#include "cc3o3/char/CharacterDatabase.h"
 #include "cc3o3/state/ComponentResolver.h"
 #include "cc3o3/state/objects/EmptyObject.h"
 #include "cc3o3/state/components/OverworldVisual.h"
 #include "cc3o3/state/components/OverworldMovement.h"
 #include "cc3o3/state/StateLogging.h"
 
-#include "Logging/Logging.h"
+#include "AppCommon_GraphicalClient/Common.h"
+
+#include "PPU/PPUController.h"
+#include "PPU/FramePackManager.h"
+#include "PPU/FramePack.h"
 
 #include "core_component/TypedObjectRef.h"
 #include "core_component/TypedComponentRef.h"
@@ -28,17 +34,6 @@ MutableObjectRef CreateOverworldCharacterFromDB( state::VariableIdentifier const
 
 void MakeOverworldCharacterFromDB( MutableObjectRef const& ref, rftl::immutable_string const& charID )
 {
-	// Visual
-	{
-		MutableComponentInstanceRefT<comp::OverworldVisual> const visual =
-			ref.AddComponentInstanceT<comp::OverworldVisual>(
-				DefaultCreator<comp::OverworldVisual>::Create() );
-		RFLOG_TEST_AND_FATAL( visual != nullptr, ref, RFCAT_CC3O3, "Failed to add overworld visual component" );
-
-		// TODO
-		( (void)visual );
-	}
-
 	// Movement
 	{
 		MutableComponentInstanceRefT<comp::OverworldMovement> const move =
@@ -46,8 +41,50 @@ void MakeOverworldCharacterFromDB( MutableObjectRef const& ref, rftl::immutable_
 				DefaultCreator<comp::OverworldMovement>::Create() );
 		RFLOG_TEST_AND_FATAL( move != nullptr, ref, RFCAT_CC3O3, "Failed to add overworld movement component" );
 
-		// TODO
+		// TODO: Bind
 		( (void)move );
+	}
+
+	// Visual
+	{
+		MutableComponentInstanceRefT<comp::OverworldVisual> const visual =
+			ref.AddComponentInstanceT<comp::OverworldVisual>(
+				DefaultCreator<comp::OverworldVisual>::Create() );
+		RFLOG_TEST_AND_FATAL( visual != nullptr, ref, RFCAT_CC3O3, "Failed to add overworld visual component" );
+
+		character::CharacterDatabase const& charDB = *gCharacterDatabase;
+		sprite::CompositeCharacter const composite = charDB.FetchExistingComposite( charID );
+		RFLOG_TEST_AND_FATAL( composite.mCharacterSequenceType == sprite::CharacterSequenceType::N3_E3_S3_W3, ref, RFCAT_CC3O3, "Unsupported sequence" );
+		file::VFSPath const nWalk = composite.mFramepacksByAnim.at( sprite::CharacterAnimKey::NWalk );
+		file::VFSPath const eWalk = composite.mFramepacksByAnim.at( sprite::CharacterAnimKey::EWalk );
+		file::VFSPath const sWalk = composite.mFramepacksByAnim.at( sprite::CharacterAnimKey::SWalk );
+		file::VFSPath const wWalk = composite.mFramepacksByAnim.at( sprite::CharacterAnimKey::WWalk );
+
+		gfx::PPUController& ppu = *app::gGraphics;
+		gfx::FramePackManager const& fPackMan = *ppu.GetFramePackManager();
+		ppu.ForceImmediateLoadRequest( gfx::PPUController::AssetType::FramePack, nWalk );
+		ppu.ForceImmediateLoadRequest( gfx::PPUController::AssetType::FramePack, eWalk );
+		ppu.ForceImmediateLoadRequest( gfx::PPUController::AssetType::FramePack, sWalk );
+		ppu.ForceImmediateLoadRequest( gfx::PPUController::AssetType::FramePack, wWalk );
+		auto setAnim = [&fPackMan]( comp::OverworldVisual::Anim& anim, file::VFSPath const& source ) -> void //
+		{
+			anim.mFramePackID = fPackMan.GetManagedResourceIDFromResourceName( source );
+			anim.mMaxTimeIndex = fPackMan.GetResourceFromResourceName( source )->CalculateTimeIndexBoundary();
+			RF_ASSERT_MSG( anim.mMaxTimeIndex == 4, "Unexpected format" );
+		};
+		setAnim( visual->mWalkNorth, nWalk );
+		setAnim( visual->mWalkEast, eWalk );
+		setAnim( visual->mWalkSouth, sWalk );
+		setAnim( visual->mWalkWest, wWalk );
+
+		// TODO: Different format, with dedicated idle framepacks
+		setAnim( visual->mIdleNorth, nWalk );
+		setAnim( visual->mIdleEast, eWalk );
+		setAnim( visual->mIdleSouth, sWalk );
+		setAnim( visual->mIdleWest, wWalk );
+
+		// TODO: Bind
+		( (void)visual );
 	}
 
 	RFLOG_DEBUG( ref, RFCAT_CC3O3, "Prepared as overworld character" );
