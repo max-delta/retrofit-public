3bacafd9eb2362405ca52ac2afc6b7ddb3a33fd7
Max Delta <54338561+max-delta@users.noreply.github.com>
2020-05-01 21:27:27 +0000

    Added multi-passthrough UI controller

---
+++ libs/GameUI/controllers/MultiPassthrough.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+#include "MultiPassthrough.h"
+
+#include "GameUI/ContainerManager.h"
+#include "GameUI/Container.h"
+#include "GameUI/UIContext.h"
+
+#include "RFType/CreateClassInfoDefinition.h"
+
+
+RFTYPE_CREATE_META( RF::ui::controller::MultiPassthrough )
+{
+	RFTYPE_META().BaseClass<RF::ui::controller::InstancedController>();
+	RFTYPE_REGISTER_BY_QUALIFIED_NAME( RF::ui::controller::MultiPassthrough );
+}
+
+namespace RF { namespace ui { namespace controller {
+///////////////////////////////////////////////////////////////////////////////
+
+MultiPassthrough::MultiPassthrough( size_t numChildren )
+{
+	mChildContainerIDs.resize( numChildren, kInvalidContainerID );
+}
+
+
+
+ContainerID MultiPassthrough::GetChildContainerID( size_t index ) const
+{
+	RF_ASSERT( index < mChildContainerIDs.size() );
+	return mChildContainerIDs.at( index );
+}
+
+
+
+void MultiPassthrough::OnInstanceAssign( UIContext& context, Container& container )
+{
+	for( ContainerID& childContainerID : mChildContainerIDs )
+	{
+		childContainerID = CreateChildContainer(
+			context.GetMutableContainerManager(),
+			container,
+			container.mLeftConstraint,
+			container.mRightConstraint,
+			container.mTopConstraint,
+			container.mBottomConstraint );
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+}}}
---
+++ libs/GameUI/controllers/MultiPassthrough.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "project.h"
+
+#include "GameUI/controllers/InstancedController.h"
+
+
+namespace RF { namespace ui { namespace controller {
+///////////////////////////////////////////////////////////////////////////////
+
+class GAMEUI_API MultiPassthrough final : public InstancedController
+{
+	RFTYPE_ENABLE_VIRTUAL_LOOKUP();
+	RF_NO_COPY( MultiPassthrough );
+
+	//
+	// Public methods
+public:
+	MultiPassthrough( size_t numChildren );
+
+	ContainerID GetChildContainerID( size_t index ) const;
+
+	virtual void OnInstanceAssign( UIContext& context, Container& container ) override;
+
+
+	//
+	// Private data
+private:
+	rftl::vector<ContainerID> mChildContainerIDs;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+}}}
