ca7aca9608ac66bb53e8a28c4c7c45a22a53a71b
Max Delta <54338561+max-delta@users.noreply.github.com>
2020-06-21 22:23:03 +0000

    Minor debug tweak

--- apps/cc3o3/ui/ElementGridDisplayCache.cpp
+++ apps/cc3o3/ui/ElementGridDisplayCache.cpp
@@ -10,6 +10,7 @@ void ElementGridDisplayCache::UpdateFromCharacter( state::ObjectRef const& chara
 	// TODO
 	//RF_ASSERT( character.IsSet() );
 
+	size_t i = 0;
 	for( Column& column : mGrid )
 	{
 		for( Slot& slot : column )
@@ -20,7 +21,10 @@ void ElementGridDisplayCache::UpdateFromCharacter( state::ObjectRef const& chara
 			}
 			else
 			{
-				slot.mName = "UNSET";
+				slot.mName = "UNSET 00";
+				slot.mName.back() += i % 10;
+				*( slot.mName.rbegin() + 1 ) += math::integer_cast<uint8_t>( i / 10 );
+				i++;
 			}
 
 			slot.mTilesetIndex = ElementTilesetIndex::Invalid;
