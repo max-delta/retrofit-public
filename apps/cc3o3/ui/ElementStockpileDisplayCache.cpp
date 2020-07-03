fdf1760ce70c492f047f4be1e0a98dc30b96c7db
Max Delta <54338561+max-delta@users.noreply.github.com>
2020-07-03 22:43:27 +0000

    Basic stockpile element cache updating

--- apps/cc3o3/ui/ElementStockpileDisplayCache.cpp
+++ apps/cc3o3/ui/ElementStockpileDisplayCache.cpp
@@ -1,25 +1,35 @@
 #include "stdafx.h"
 #include "ElementStockpileDisplayCache.h"
 
+#include "cc3o3/Common.h"
+#include "cc3o3/company/CompanyManager.h"
+#include "cc3o3/elements/ElementDatabase.h"
+
 
 namespace RF::cc::ui {
 ///////////////////////////////////////////////////////////////////////////////
 
 void ElementStockpileDisplayCache::UpdateFromCompany( state::ObjectRef const& company )
 {
-	// TODO
-	//RF_ASSERT( company.IsSet() );
-
-	mStockpile.resize( 20 );
-	size_t i = 0;
-	for( Slot& slot : mStockpile )
+	using ElementCount = rftl::pair<element::ElementIdentifier, size_t>;
+	using ElementCounts = rftl::vector<ElementCount>;
+	ElementCounts sortedElements;
 	{
-		slot.mName = "UNSET 00";
-		slot.mName.back() += i % 10;
-		*( slot.mName.rbegin() + 1 ) += math::integer_cast<uint8_t>( i / 10 );
-		i++;
+		company::CompanyManager::ElementCounts const elementCounts = gCompanyManager->CalcTotalElements( company );
+		// TODO: Reduce counts by amounts equipped by active party
 
-		slot.mTilesetIndex = ElementTilesetIndex::Orange;
+		sortedElements.assign( elementCounts.begin(), elementCounts.end() );
+		// TODO: Sorting/filtering
+	}
+
+	size_t const numSlots = sortedElements.size();
+	mStockpile.resize( numSlots );
+	for( size_t i = 0; i < numSlots; i++ )
+	{
+		Slot& slot = mStockpile.at( i );
+		ElementCount const& countEntry = sortedElements.at( i );
+		element::ElementDesc const desc = gElementDatabase->GetElementDesc( countEntry.first );
+		slot.UpdateFromDesc( desc, true );
 	}
 }
 
