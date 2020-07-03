0d2be721a8ef85a434a4190565fa82f4ebee6e25
Max Delta <54338561+max-delta@users.noreply.github.com>
2020-07-03 22:37:37 +0000

    Reduce innate length

--- apps/cc3o3/elements/ElementFwd.h
+++ apps/cc3o3/elements/ElementFwd.h
@@ -6,9 +6,9 @@ namespace RF::cc::element {
 ///////////////////////////////////////////////////////////////////////////////
 
 // Element and innate identifier bytes are intended to be valid ASCII strings
-using ElementIdentifier = uint64_t;
+using ElementIdentifier = uint64_t; // 'B' 'u' 'f' 'S' '_' 'R' '\0' '\0'
 static constexpr ElementIdentifier kInvalidElementIdentifier = 0;
-using InnateIdentifier = uint64_t;
+using InnateIdentifier = uint32_t; // 'r' 'e' 'd' '\0'
 static constexpr InnateIdentifier kInvalidInnateIdentifier = 0;
 
 using ElementLevel = uint8_t;
