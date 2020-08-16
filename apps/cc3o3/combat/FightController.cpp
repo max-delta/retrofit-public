61dbf35a6e3f80ee8597e86498aa9bef734d2240
Max Delta <54338561+max-delta@users.noreply.github.com>
2020-08-16 20:28:07 +0000

    Basic attack buffering concepts, but without rollback support

--- apps/cc3o3/combat/FightController.cpp
+++ apps/cc3o3/combat/FightController.cpp
@@ -1,10 +1,14 @@
 #include "stdafx.h"
 #include "FightController.h"
 
+#include "cc3o3/combat/Attack.h"
+#include "cc3o3/combat/CombatEngine.h"
 #include "cc3o3/combat/CombatInstance.h"
 #include "cc3o3/company/CompanyManager.h"
 #include "cc3o3/elements/IdentifierUtils.h"
 #include "cc3o3/input/HardcodedSetup.h"
+
+#include "Logging/Logging.h"
 
 #include "core_component/TypedObjectRef.h"
 #include "core_math/math_casts.h"
@@ -56,6 +60,8 @@ uint8_t SanitizeFighterIndex( CombatInstance::FighterIDs const& ids, uint8_t ind
 FightController::FightController(
 	WeakPtr<CombatEngine const> const& combatEngine,
 	WeakPtr<company::CompanyManager const> const& companyManager )
+	: mCombatEngine( combatEngine )
+	, mCompanyManager( companyManager )
 {
 	mCombatInstance = DefaultCreator<CombatInstance>::Create( combatEngine );
 }
@@ -159,6 +165,43 @@ void FightController::HardcodedPlaceholderSetup()
 
 
 
+void FightController::TickPendingActions()
+{
+	// TODO: Actions have animations and other delays, which need to be handled
+	// NOTE: Probably want a seperate system to handle that aspect, since it
+	//  needs to work for other parties as well. Ownership of the main combat
+	//  instance will probably need to be moved into that new system.
+
+	CombatInstance& combatInstance = *mCombatInstance;
+
+	if( mAttackBuffer.empty() == false )
+	{
+		// Pop front
+		BufferedAttack const attack = mAttackBuffer.front();
+		mAttackBuffer.erase( mAttackBuffer.begin() );
+
+		if( combatInstance.CanPerformAttack( attack.mAttackerID, attack.mDefenderID, attack.mAttackStrength ) )
+		{
+			// Attack
+			AttackResult const result = combatInstance.ExecuteAttack( attack.mAttackerID, attack.mDefenderID, attack.mAttackStrength );
+
+			// TODO: Record to some 'recent attacks' buffer for displaying
+			// NOTE: Will likely be the responsibility of whatever system is
+			//  monitoring and executing the delays and animations, since we
+			//  need to display this for other parties as well
+			( (void)result );
+		}
+		else
+		{
+			RFLOG_WARNING( nullptr, RFCAT_CC3O3, "Buffered attack invalid, cancelling all attacks" );
+			RF_DBGFAIL_MSG( "Bad attack. How did this happen? Fix, or flag as benign and document behavior." );
+			mAttackBuffer.clear();
+		}
+	}
+}
+
+
+
 uint8_t FightController::SanitizeCharacterIndex( uint8_t attackerIndex, int8_t applyOffset ) const
 {
 	CombatInstance::FighterIDs const characters = mCombatInstance->GetFighterIDs( mLocalPartyID );
@@ -202,24 +245,95 @@ FighterID FightController::GetAttackTargetByIndex( uint8_t attackerIndex, uint8_
 
 bool FightController::CanCharacterPerformAttack( uint8_t attackerIndex ) const
 {
-	// TODO
-	return true;
+	FighterID const attackerID = GetCharacterByIndex( attackerIndex );
+	return mCombatInstance->CanPerformAttack( attackerID );
 }
 
 
 
 bool FightController::CanCharacterPerformAttack( uint8_t attackerIndex, uint8_t defenderIndex ) const
 {
-	// TODO
-	return true;
+	if( CanCharacterPerformAttack( attackerIndex ) == false )
+	{
+		return false;
+	}
+
+	FighterID const attackerID = GetCharacterByIndex( attackerIndex );
+	FighterID const defenderID = GetAttackTargetByIndex( attackerIndex, defenderIndex );
+	return mCombatInstance->CanPerformAttack( attackerID, defenderID );
 }
 
 
 
 bool FightController::CanCharacterPerformAttack( uint8_t attackerIndex, uint8_t defenderIndex, uint8_t attackStrength ) const
 {
-	// TODO
-	return false;
+	if( CanCharacterPerformAttack( attackerIndex, defenderIndex ) == false )
+	{
+		return false;
+	}
+
+	FighterID const attackerID = GetCharacterByIndex( attackerIndex );
+	FighterID const defenderID = GetAttackTargetByIndex( attackerIndex, defenderIndex );
+
+	if( mAttackBuffer.empty() )
+	{
+		return mCombatInstance->CanPerformAttack( attackerID, defenderID, attackStrength );
+	}
+	else
+	{
+		// Attacks are queued, simulate them along with this additional attack
+		CombatInstance simulation = *mCombatInstance;
+		bool const simSuccess = SimulateAttackBuffer( simulation, attackerID, defenderID, attackStrength );
+		return simSuccess;
+	}
+}
+
+
+
+void FightController::PredictAttack( AttackProfile& profile, AttackResult& result, uint8_t attackerIndex, uint8_t defenderIndex, uint8_t attackStrength ) const
+{
+	RF_ASSERT( CanCharacterPerformAttack( attackerIndex, defenderIndex, attackStrength ) );
+
+	FighterID const attackerID = GetCharacterByIndex( attackerIndex );
+	FighterID const defenderID = GetAttackTargetByIndex( attackerIndex, defenderIndex );
+
+	CombatInstance simulation = *mCombatInstance;
+	if( mAttackBuffer.empty() == false )
+	{
+		bool const simSuccess = SimulateAttackBuffer( simulation, attackerID, defenderID );
+
+		// The CanCharacterPerformAttack(...) check should already cover this
+		RF_ASSERT( simSuccess );
+	}
+
+	// The CanCharacterPerformAttack(...) check should already cover this
+	RF_ASSERT( simulation.CanPerformAttack( attackerID, defenderID, attackStrength ) );
+
+	profile = simulation.PrepareAttack( attackerID, defenderID, attackStrength );
+	result = simulation.ExecuteAttack( attackerID, defenderID, attackStrength );
+}
+
+
+
+void FightController::BufferAttack( uint8_t attackerIndex, uint8_t defenderIndex, uint8_t attackStrength )
+{
+	// TODO: This buffering needs to be re-designed to be stored into a
+	//  rollback domain, probably something like:
+	//  * buffered:Attacker
+	//  * buffered:Defender
+	//  * buffered:Attack:0
+	//  * buffered:Attack:1
+	//  * buffered:Attack:2 (Or whatever limit is reasonable)
+
+	RF_ASSERT( CanCharacterPerformAttack( attackerIndex, defenderIndex, attackStrength ) );
+	FighterID const attackerID = GetCharacterByIndex( attackerIndex );
+	FighterID const defenderID = GetAttackTargetByIndex( attackerIndex, defenderIndex );
+	if( mAttackBuffer.empty() == false )
+	{
+		RF_ASSERT( mAttackBuffer.front().mAttackerID == attackerID );
+		RF_ASSERT( mAttackBuffer.front().mDefenderID == defenderID );
+	}
+	mAttackBuffer.emplace_back( { attackerID, defenderID, attackStrength } );
 }
 
 
@@ -244,6 +358,47 @@ bool FightController::CanPartyEndTurn() const
 {
 	// TODO
 	return false;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+bool FightController::SimulateAttackBuffer( CombatInstance& simulation, FighterID attackerID, FighterID defenderID ) const
+{
+	RF_ASSERT( mAttackBuffer.empty() == false );
+
+	for( BufferedAttack const& attack : mAttackBuffer )
+	{
+		RF_ASSERT( attack.mAttackerID == attackerID );
+		RF_ASSERT( attack.mDefenderID == defenderID );
+		if( simulation.CanPerformAttack( attackerID, defenderID, attack.mAttackStrength ) == false )
+		{
+			return false;
+		}
+		simulation.ExecuteAttack( attackerID, defenderID, attack.mAttackStrength );
+	}
+	return true;
+}
+
+
+
+bool FightController::SimulateAttackBuffer( CombatInstance& simulation, FighterID attackerID, FighterID defenderID, uint8_t additionalAttackStrength ) const
+{
+	RF_ASSERT( mAttackBuffer.empty() == false );
+
+	// Buffered attacks
+	bool const bufferSuccessful = SimulateAttackBuffer( simulation, attackerID, defenderID );
+	if( bufferSuccessful == false )
+	{
+		return false;
+	}
+
+	// Additional attack
+	if( simulation.CanPerformAttack( attackerID, defenderID, additionalAttackStrength ) == false )
+	{
+		return false;
+	}
+	simulation.ExecuteAttack( attackerID, defenderID, additionalAttackStrength );
+	return true;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
--- apps/cc3o3/combat/FightController.h
+++ apps/cc3o3/combat/FightController.h
@@ -6,6 +6,8 @@
 
 #include "core/ptr/unique_ptr.h"
 
+#include "rftl/extension/static_vector.h"
+
 
 namespace RF::cc::combat {
 ///////////////////////////////////////////////////////////////////////////////
@@ -15,6 +17,19 @@ namespace RF::cc::combat {
 class FightController
 {
 	RF_NO_COPY( FightController );
+
+	//
+	// Types and constants
+private:
+	struct BufferedAttack
+	{
+		FighterID mAttackerID = {};
+		FighterID mDefenderID = {};
+		uint8_t mAttackStrength = 0;
+	};
+	static constexpr size_t kMaxBufferedAttacks = 7;
+	using AttackBuffer = rftl::static_vector<BufferedAttack, kMaxBufferedAttacks>;
+
 
 	//
 	// Public methods
@@ -27,6 +42,8 @@ public:
 
 	void HardcodedPlaceholderSetup();
 
+	void TickPendingActions();
+
 	uint8_t SanitizeCharacterIndex( uint8_t attackerIndex, int8_t applyOffset ) const;
 	uint8_t SanitizeAttackTargetIndex( uint8_t attackerIndex, uint8_t defenderIndex, int8_t applyOffset ) const;
 
@@ -37,6 +54,8 @@ public:
 	bool CanCharacterPerformAttack( uint8_t attackerIndex ) const;
 	bool CanCharacterPerformAttack( uint8_t attackerIndex, uint8_t defenderIndex ) const;
 	bool CanCharacterPerformAttack( uint8_t attackerIndex, uint8_t defenderIndex, uint8_t attackStrength ) const;
+	void PredictAttack( AttackProfile& profile, AttackResult& result, uint8_t attackerIndex, uint8_t defenderIndex, uint8_t attackStrength ) const;
+	void BufferAttack( uint8_t attackerIndex, uint8_t defenderIndex, uint8_t attackStrength );
 
 	bool CanCharacterCastElement( uint8_t attackerIndex ) const;
 
@@ -46,12 +65,21 @@ public:
 
 
 	//
+	// Private methods
+private:
+	bool SimulateAttackBuffer( CombatInstance& simulation, FighterID attackerID, FighterID defenderID ) const;
+	bool SimulateAttackBuffer( CombatInstance& simulation, FighterID attackerID, FighterID defenderID, uint8_t additionalAttackStrength ) const;
+
+
+	//
 	// Private data
 private:
+	WeakPtr<CombatEngine const> mCombatEngine;
 	WeakPtr<company::CompanyManager const> mCompanyManager;
 	UniquePtr<combat::CombatInstance> mCombatInstance;
 
 	PartyID mLocalPartyID = {};
+	AttackBuffer mAttackBuffer = {};
 };
 
 ///////////////////////////////////////////////////////////////////////////////
