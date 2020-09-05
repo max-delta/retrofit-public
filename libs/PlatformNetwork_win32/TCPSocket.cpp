3542ef57f4ed5e4896fc370b4f1f61be72edbe76
Max Delta <54338561+max-delta@users.noreply.github.com>
2020-09-05 23:57:18 +0000

    Fixes and diagnostics for TCP sockets

--- libs/PlatformNetwork_win32/TCPSocket.cpp
+++ libs/PlatformNetwork_win32/TCPSocket.cpp
@@ -6,6 +6,7 @@
 #include "core_platform/winsock2_inc.h"
 
 #include "core_math/math_casts.h"
+#include "core_math/math_bytes.h"
 
 #include "rftl/vector"
 
@@ -17,6 +18,17 @@ namespace details {
 // Addresses have pointer graphs that need free-ing
 struct AddressList
 {
+	RF_NO_COPY( AddressList );
+
+public:
+	AddressList() = default;
+	AddressList( AddressList&& rhs )
+		: RF_MOVE_CONSTRUCT( mList )
+		, RF_MOVE_CONSTRUCT( mHead )
+	{
+		rhs.mHead = nullptr;
+	}
+
 	~AddressList()
 	{
 		if( mHead != nullptr )
@@ -65,6 +77,7 @@ AddressList LookupSuggestedAddresses( win32::addrinfo const& hints, char const* 
 	while( iter != nullptr )
 	{
 		win32::addrinfo& back = retVal.mList.emplace_back( *iter );
+		RF_ASSERT( back.ai_family == AF_INET || back.ai_family == AF_INET6 );
 		back.ai_next = nullptr;
 		iter = iter->ai_next;
 	}
@@ -73,6 +86,49 @@ AddressList LookupSuggestedAddresses( win32::addrinfo const& hints, char const* 
 	SortSuggestions( retVal );
 
 	return retVal;
+}
+
+
+
+rftl::string GetAddress( win32::sockaddr const& sockAddr, size_t sockAddrLen )
+{
+	if( sockAddr.sa_family == AF_INET )
+	{
+		win32::sockaddr_in const& ipv4 = *reinterpret_cast<win32::sockaddr_in const*>( &sockAddr );
+		uint16_t const port = win32::ntohs( ipv4.sin_port );
+		auto const& bytes = ipv4.sin_addr.S_un.S_un_b;
+		static constexpr char kExample[] = "255.255.255.255:65535";
+		rftl::array<char, sizeof( kExample )> buf;
+		snprintf( buf.data(), buf.size(), "%u.%u.%u.%u:%u", bytes.s_b1, bytes.s_b2, bytes.s_b3, bytes.s_b4, port );
+		buf.back() = '\0';
+		return buf.data();
+	}
+	else if( sockAddr.sa_family == AF_INET6 )
+	{
+		win32::sockaddr_in6 const& ipv6 = *reinterpret_cast<win32::sockaddr_in6 const*>( &sockAddr );
+		uint16_t const port = win32::ntohs( ipv6.sin6_port );
+		uint8_t const( &bytes )[16] = ipv6.sin6_addr.u.Byte;
+		static constexpr char kExample[] = "[ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff]:65535";
+		rftl::array<uint16_t, 8> hextets = {};
+		for( size_t i = 0; i < 16; i += 2 )
+		{
+			hextets.at( i / 2 ) = math::integer_cast<uint16_t>( ( bytes[i] << 8 ) | bytes[i + 1] );
+			if constexpr( compiler::kEndianness == compiler::Endianness::Big )
+			{
+				hextets.at( i / 2 ) = math::ReverseByteOrder( hextets.at( i / 2 ) );
+			}
+		}
+		rftl::array<char, sizeof( kExample )> buf;
+		snprintf( buf.data(), buf.size(), "[%x:%x:%x:%x:%x:%x:%x:%x]:%u",
+			hextets[0], hextets[1], hextets[2], hextets[3],
+			hextets[4], hextets[5], hextets[6], hextets[7], port );
+		buf.back() = '\0';
+		return buf.data();
+	}
+	else
+	{
+		return "<UNKNOWN>";
+	}
 }
 
 }
@@ -113,6 +169,13 @@ bool TCPSocket::IsValid() const
 
 
 
+bool TCPSocket::IsServer() const
+{
+	return mServer;
+}
+
+
+
 shim::SOCKET TCPSocket::GetSocketHandle()
 {
 	return mSocket;
@@ -135,22 +198,32 @@ TCPSocket TCPSocket::ConnectClientSocket( rftl::string hostname, uint16_t port )
 	// Try suggestions in order
 	for( win32::addrinfo const& suggestion : suggestions.mList )
 	{
+		rftl::string const name = details::GetAddress( *suggestion.ai_addr, suggestion.ai_addrlen );
+		RFLOG_INFO( nullptr, RFCAT_PLATFORMNETWORK, "Attempting to create client TCP socket at \"%s\"", name.c_str() );
+
 		// Create
-		retVal.mSocket = win32::socket(
+		retVal.mSocket = win32::WSASocketW(
 			suggestion.ai_family,
 			suggestion.ai_socktype,
-			suggestion.ai_protocol );
+			suggestion.ai_protocol,
+			nullptr, // No contraints
+			0, // No grouping
+			WSA_FLAG_OVERLAPPED ); // Allow async usage
 		if( retVal.mSocket == shim::kINVALID_SOCKET )
 		{
 			RFLOG_WARNING( nullptr, RFCAT_PLATFORMNETWORK, "Failed to create unconnected TCP socket: WSA %i", win32::WSAGetLastError() );
 		}
 		else
 		{
-			// Bind
-			int const connectResult = win32::connect(
+			// Connect
+			int const connectResult = win32::WSAConnect(
 				retVal.mSocket,
 				suggestion.ai_addr,
-				math::integer_cast<int>( suggestion.ai_addrlen ) );
+				math::integer_cast<int>( suggestion.ai_addrlen ),
+				nullptr, // N/A on TCP
+				nullptr, // N/A on TCP
+				nullptr, // No flow-spec constraints
+				nullptr ); // No flow-spec constraints
 			if( connectResult != 0 )
 			{
 				RF_ASSERT( connectResult == SOCKET_ERROR );
@@ -160,7 +233,7 @@ TCPSocket TCPSocket::ConnectClientSocket( rftl::string hostname, uint16_t port )
 			{
 				// Success!
 				RF_ASSERT( retVal.IsValid() );
-				RFLOG_WARNING( nullptr, RFCAT_PLATFORMNETWORK, "Connected a TCP client socket for port %u", port );
+				RFLOG_MILESTONE( nullptr, RFCAT_PLATFORMNETWORK, "Connected a TCP client socket at \"%s\"", name.c_str() );
 				return retVal;
 			}
 		}
@@ -175,27 +248,33 @@ TCPSocket TCPSocket::ConnectClientSocket( rftl::string hostname, uint16_t port )
 
 
 
-TCPSocket TCPSocket::BindServerSocket( bool ipv6, uint16_t port )
+TCPSocket TCPSocket::BindServerSocket( bool preferIPv6, bool loopback, uint16_t port )
 {
 	TCPSocket retVal = {};
 
 	// There may be multiple interfaces, ask OS for suggestions
 	win32::addrinfo hints = {};
 	hints.ai_flags = AI_PASSIVE; // Expect bind(...)
-	hints.ai_family = ipv6 ? AF_INET6 : AF_INET; // IPv4 / IPv6
+	hints.ai_family = preferIPv6 ? AF_INET6 : AF_INET; // IPv4 / IPv6
 	hints.ai_socktype = SOCK_STREAM; // Stream
 	hints.ai_protocol = win32::IPPROTO_TCP; // TCP
-	static constexpr char const* kServerHostname = nullptr; // TODO: Support?
-	details::AddressList const suggestions = details::LookupSuggestedAddresses( hints, kServerHostname, port );
+	char const* const serverHostname = loopback ? "localhost" : nullptr;
+	details::AddressList const suggestions = details::LookupSuggestedAddresses( hints, serverHostname, port );
 
 	// Try suggestions in order
 	for( win32::addrinfo const& suggestion : suggestions.mList )
 	{
+		rftl::string const name = details::GetAddress( *suggestion.ai_addr, suggestion.ai_addrlen );
+		RFLOG_INFO( nullptr, RFCAT_PLATFORMNETWORK, "Attempting to create server TCP socket at \"%s\"", name.c_str() );
+
 		// Create
-		retVal.mSocket = win32::socket(
+		retVal.mSocket = win32::WSASocketW(
 			suggestion.ai_family,
 			suggestion.ai_socktype,
-			suggestion.ai_protocol );
+			suggestion.ai_protocol,
+			nullptr, // No contraints
+			0, // No grouping
+			WSA_FLAG_OVERLAPPED ); // Allow async usage
 		if( retVal.mSocket == shim::kINVALID_SOCKET )
 		{
 			RFLOG_WARNING( nullptr, RFCAT_PLATFORMNETWORK, "Failed to create unbound TCP socket: WSA %i", win32::WSAGetLastError() );
@@ -214,10 +293,23 @@ TCPSocket TCPSocket::BindServerSocket( bool ipv6, uint16_t port )
 			}
 			else
 			{
-				// Success!
-				RF_ASSERT( retVal.IsValid() );
-				RFLOG_WARNING( nullptr, RFCAT_PLATFORMNETWORK, "Bound a TCP server socket for port %u", port );
-				return retVal;
+				// Listen
+				int const listenResult = win32::listen(
+					retVal.mSocket,
+					SOMAXCONN );
+				if( listenResult != 0 )
+				{
+					RF_ASSERT( listenResult == SOCKET_ERROR );
+					RFLOG_WARNING( nullptr, RFCAT_PLATFORMNETWORK, "Failed to listen on TCP socket: WSA %i", win32::WSAGetLastError() );
+				}
+				else
+				{
+					// Success!
+					RF_ASSERT( retVal.IsValid() );
+					retVal.mServer = true;
+					RFLOG_MILESTONE( nullptr, RFCAT_PLATFORMNETWORK, "Bound a listening TCP server socket at \"%s\"", name.c_str() );
+					return retVal;
+				}
 			}
 		}
 
--- libs/PlatformNetwork_win32/TCPSocket.h
+++ libs/PlatformNetwork_win32/TCPSocket.h
@@ -22,10 +22,13 @@ public:
 	~TCPSocket();
 
 	bool IsValid() const;
+	bool IsServer() const;
 	shim::SOCKET GetSocketHandle();
 
+	// NOTE: An empty hostname will result in attempting connections to local
+	//  addresses, as opposed to "localhost" for loopback addresses
 	static TCPSocket ConnectClientSocket( rftl::string hostname, uint16_t port );
-	static TCPSocket BindServerSocket( bool ipv6, uint16_t port );
+	static TCPSocket BindServerSocket( bool preferIPv6, bool loopback, uint16_t port );
 
 
 	//
@@ -38,6 +41,7 @@ private:
 	//
 	// Private data
 private:
+	bool mServer = false;
 	shim::SOCKET mSocket = shim::kINVALID_SOCKET;
 };
 
