6b91168f7bdf459fa5f0b22553361647079a474e
Max Delta <54338561+max-delta@users.noreply.github.com>
2020-09-27 18:08:03 +0000

    Re-enable MSVC static analysis, but only on Azure post-build testing

--- config/StaticAnalysis.h
+++ config/StaticAnalysis.h
@@ -5,9 +5,17 @@
 //  the (at time of writing) experimental third-party warning disabling
 //  feature in MSVC
 // TODO: Review once these features are less experimental
-#pragma warning(disable:ALL_CPPCORECHECK_WARNINGS)
+#pragma warning( disable : ALL_CPPCORECHECK_WARNINGS )
 
 // This version of MSVC doesn't include this warning in <CppCoreCheck/warnings.h>
 #if _MSC_FULL_VER >= 191526726 && _MSC_FULL_VER <= 191526732
-	#pragma warning(disable:26455)
+	#pragma warning( disable : 26455 )
+#endif
+
+// False positives about locks being dropped in non-sensical locations
+// NOTE: Specifically observed with rftl::lock_guard<rftl::mutex> not
+//  understanding where scope ends, and thinking the destructor would happen
+//  in the same line as the constructor
+#if _MSC_FULL_VER == 192729111
+	#pragma warning( disable : 26110 )
 #endif
