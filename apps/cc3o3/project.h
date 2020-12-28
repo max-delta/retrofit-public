6454fa045c13cc5f2249caf58085d8b1fb6eebdd
Max Delta <54338561+max-delta@users.noreply.github.com>
2020-12-28 21:29:14 +0000

    Update CC3O3 to launcher model

--- apps/cc3o3/project.h
+++ apps/cc3o3/project.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "AppCommon_GraphicalClient/project.h"
 
+#define RF_MODULE_POINT __declspec( dllexport )
+
 constexpr char const* RFCAT_CC3O3 = "CC3O3";
 constexpr uint64_t RFCAT_CC3O3_SEV_WHITELIST = RF::logging::kDefaultSeverityMask;
 
