91701126e3da070483d5f553a38d2f773be67c55
Max Delta <54338561+max-delta@users.noreply.github.com>
2021-03-08 04:50:22 +0000

    Trying to unify more of the build platform identifiers

--- batch_build.bat
+++ batch_build.bat
@@ -5,28 +5,28 @@
 @cd /D "%~dp0"
 
 @echo Low verbosity! May sit for a while without messages!
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x64 /p:Configuration=LLVM_Debug
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x64 /p:Configuration=LLVM_Release
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x64 /p:Configuration=LLVM_Retail
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86 /p:Configuration=LLVM_Debug
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86 /p:Configuration=LLVM_Release
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86 /p:Configuration=LLVM_Retail
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM /p:Configuration=LLVM_Debug
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM /p:Configuration=LLVM_Release
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM /p:Configuration=LLVM_Retail
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM32 /p:Configuration=LLVM_Debug
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM32 /p:Configuration=LLVM_Release
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM32 /p:Configuration=LLVM_Retail
 msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM64 /p:Configuration=LLVM_Debug
 msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM64 /p:Configuration=LLVM_Release
 msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM64 /p:Configuration=LLVM_Retail
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86_32 /p:Configuration=LLVM_Debug
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86_32 /p:Configuration=LLVM_Release
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86_32 /p:Configuration=LLVM_Retail
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86_64 /p:Configuration=LLVM_Debug
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86_64 /p:Configuration=LLVM_Release
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86_64 /p:Configuration=LLVM_Retail
 
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x64 /p:Configuration=MSVC_Debug
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x64 /p:Configuration=MSVC_Release
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x64 /p:Configuration=MSVC_Retail
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86 /p:Configuration=MSVC_Debug
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86 /p:Configuration=MSVC_Release
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86 /p:Configuration=MSVC_Retail
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM /p:Configuration=MSVC_Debug
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM /p:Configuration=MSVC_Release
-msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM /p:Configuration=MSVC_Retail
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM32 /p:Configuration=MSVC_Debug
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM32 /p:Configuration=MSVC_Release
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM32 /p:Configuration=MSVC_Retail
 msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM64 /p:Configuration=MSVC_Debug
 msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM64 /p:Configuration=MSVC_Release
 msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=ARM64 /p:Configuration=MSVC_Retail
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86_32 /p:Configuration=MSVC_Debug
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86_32 /p:Configuration=MSVC_Release
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86_32 /p:Configuration=MSVC_Retail
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86_64 /p:Configuration=MSVC_Debug
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86_64 /p:Configuration=MSVC_Release
+msbuild.exe retrofit.sln /nologo /verbosity:q /maxcpucount /target:Build /p:Platform=x86_64 /p:Configuration=MSVC_Retail
--- batch_test.bat
+++ batch_test.bat
@@ -1,24 +1,30 @@
 @cls
 @cd /D "%~dp0"
 
-_artifacts\x64\MSVC_Debug\Out\core_test.exe
-_artifacts\x64\MSVC_Release\Out\core_test.exe
-_artifacts\x64\MSVC_Retail\Out\core_test.exe
-_artifacts\Win32\MSVC_Debug\Out\core_test.exe
-_artifacts\Win32\MSVC_Release\Out\core_test.exe
-_artifacts\Win32\MSVC_Retail\Out\core_test.exe
+_artifacts\x86_32\MSVC_Debug\Out\core_test.exe
+_artifacts\x86_32\MSVC_Release\Out\core_test.exe
+_artifacts\x86_32\MSVC_Retail\Out\core_test.exe
+_artifacts\x86_64\MSVC_Debug\Out\core_test.exe
+_artifacts\x86_64\MSVC_Release\Out\core_test.exe
+_artifacts\x86_64\MSVC_Retail\Out\core_test.exe
 
-_artifacts\x64\LLVM_Debug\Out\core_test.exe
-_artifacts\x64\LLVM_Release\Out\core_test.exe
-_artifacts\x64\LLVM_Retail\Out\core_test.exe
+_artifacts\x86_32\LLVM_Debug\Out\core_test.exe
+_artifacts\x86_32\LLVM_Release\Out\core_test.exe
+_artifacts\x86_32\LLVM_Retail\Out\core_test.exe
+_artifacts\x86_64\LLVM_Debug\Out\core_test.exe
+_artifacts\x86_64\LLVM_Release\Out\core_test.exe
+_artifacts\x86_64\LLVM_Retail\Out\core_test.exe
 
-_artifacts\x64\MSVC_Debug\Out\foundation_test.exe
-_artifacts\x64\MSVC_Release\Out\foundation_test.exe
-_artifacts\x64\MSVC_Retail\Out\foundation_test.exe
-_artifacts\Win32\MSVC_Debug\Out\foundation_test.exe
-_artifacts\Win32\MSVC_Release\Out\foundation_test.exe
-_artifacts\Win32\MSVC_Retail\Out\foundation_test.exe
+_artifacts\x86_32\MSVC_Debug\Out\foundation_test.exe
+_artifacts\x86_32\MSVC_Release\Out\foundation_test.exe
+_artifacts\x86_32\MSVC_Retail\Out\foundation_test.exe
+_artifacts\x86_64\MSVC_Debug\Out\foundation_test.exe
+_artifacts\x86_64\MSVC_Release\Out\foundation_test.exe
+_artifacts\x86_64\MSVC_Retail\Out\foundation_test.exe
 
-_artifacts\x64\LLVM_Debug\Out\foundation_test.exe
-_artifacts\x64\LLVM_Release\Out\foundation_test.exe
-_artifacts\x64\LLVM_Retail\Out\foundation_test.exe
+_artifacts\x86_32\LLVM_Debug\Out\foundation_test.exe
+_artifacts\x86_32\LLVM_Release\Out\foundation_test.exe
+_artifacts\x86_32\LLVM_Retail\Out\foundation_test.exe
+_artifacts\x86_64\LLVM_Debug\Out\foundation_test.exe
+_artifacts\x86_64\LLVM_Release\Out\foundation_test.exe
+_artifacts\x86_64\LLVM_Retail\Out\foundation_test.exe
