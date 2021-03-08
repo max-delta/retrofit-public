8cf17f9f92f1dc3b6a73093c1521dfce5d0f324a
Max Delta <54338561+max-delta@users.noreply.github.com>
2021-03-08 05:24:41 +0000

    Update binary packaging script

--- package_binaries.bat
+++ package_binaries.bat
@@ -2,43 +2,32 @@
 @cd /D "%~dp0"
 
 mkdir _binaries
+mkdir _binaries\test
 
-copy _artifacts\x64\Retail\Out\core_test.exe _binaries\core_test_x64.exe
-copy _artifacts\Win32\Retail\Out\core_test.exe _binaries\core_test_x86.exe
-copy _artifacts\LLVM_x64\Retail\Out\core_test.exe _binaries\core_test_LLVM.exe
+copy _artifacts\x86_64\MSVC_Retail\Out\core_test.exe _binaries\test\core_test_x64.exe
+copy _artifacts\x86_64\MSVC_Retail\Out\core_test.pdb _binaries\test\core_test_x64.pdb
+copy _artifacts\x86_64\MSVC_Retail\Out\foundation_test.exe _binaries\test\foundation_test_x64.exe
+copy _artifacts\x86_64\MSVC_Retail\Out\foundation_test.pdb _binaries\test\foundation_test_x64.pdb
+copy _artifacts\x86_32\MSVC_Retail\Out\core_test.exe _binaries\test\core_test_x86.exe
+copy _artifacts\x86_32\MSVC_Retail\Out\core_test.pdb _binaries\test\core_test_x86.pdb
+copy _artifacts\x86_32\MSVC_Retail\Out\foundation_test.exe _binaries\test\foundation_test_x86.exe
+copy _artifacts\x86_32\MSVC_Retail\Out\foundation_test.pdb _binaries\test\foundation_test_x86.pdb
+copy _artifacts\x86_64\LLVM_Retail\Out\core_test.exe _binaries\test\core_test_LLVM.exe
+copy _artifacts\x86_64\LLVM_Retail\Out\core_test.pdb _binaries\test\core_test_LLVM.pdb
+copy _artifacts\x86_64\LLVM_Retail\Out\foundation_test.exe _binaries\test\foundation_test_LLVM.exe
+copy _artifacts\x86_64\LLVM_Retail\Out\foundation_test.pdb _binaries\test\foundation_test_LLVM.pdb
+copy _artifacts\x86_32\LLVM_Retail\Out\core_test.exe _binaries\test\core_test_LLVM32.exe
+copy _artifacts\x86_32\LLVM_Retail\Out\core_test.pdb _binaries\test\core_test_LLVM32.pdb
+copy _artifacts\x86_32\LLVM_Retail\Out\foundation_test.exe _binaries\test\foundation_test_LLVM32.exe
+copy _artifacts\x86_32\LLVM_Retail\Out\foundation_test.pdb _binaries\test\foundation_test_LLVM32.pdb
 
-copy _artifacts\x64\Retail\Out\core_test.pdb _binaries\core_test_x64.pdb
-copy _artifacts\Win32\Retail\Out\core_test.pdb _binaries\core_test_x86.pdb
-copy _artifacts\LLVM_x64\Retail\Out\core_test.pdb _binaries\core_test_LLVM.pdb
-
-copy _artifacts\x64\Retail\Out\foundation_test.exe _binaries\foundation_test_x64.exe
-copy _artifacts\Win32\Retail\Out\foundation_test.exe _binaries\foundation_test_x86.exe
-copy _artifacts\LLVM_x64\Retail\Out\foundation_test.exe _binaries\foundation_test_LLVM.exe
-
-copy _artifacts\x64\Retail\Out\foundation_test.pdb _binaries\foundation_test_x64.pdb
-copy _artifacts\Win32\Retail\Out\foundation_test.pdb _binaries\foundation_test_x86.pdb
-copy _artifacts\LLVM_x64\Retail\Out\foundation_test.pdb _binaries\foundation_test_LLVM.pdb
-
-copy _artifacts\x64\Retail\Out\retrofit.exe _binaries\retrofit_x64.exe
-copy _artifacts\Win32\Retail\Out\retrofit.exe _binaries\retrofit_x86.exe
-copy _artifacts\LLVM_x64\Retail\Out\retrofit.exe _binaries\retrofit_LLVM.exe
-
-copy _artifacts\x64\Retail\Out\retrofit.pdb _binaries\retrofit_x64.pdb
-copy _artifacts\Win32\Retail\Out\retrofit.pdb _binaries\retrofit_x86.pdb
-copy _artifacts\LLVM_x64\Retail\Out\retrofit.pdb _binaries\retrofit_LLVM.pdb
-
-copy _artifacts\x64\Retail\Out\FramePackEditor.exe _binaries\FramePackEditor_x64.exe
-copy _artifacts\Win32\Retail\Out\FramePackEditor.exe _binaries\FramePackEditor_x86.exe
-copy _artifacts\LLVM_x64\Retail\Out\FramePackEditor.exe _binaries\FramePackEditor_LLVM.exe
-
-copy _artifacts\x64\Retail\Out\FramePackEditor.pdb _binaries\FramePackEditor_x64.pdb
-copy _artifacts\Win32\Retail\Out\FramePackEditor.pdb _binaries\FramePackEditor_x86.pdb
-copy _artifacts\LLVM_x64\Retail\Out\FramePackEditor.pdb _binaries\FramePackEditor_LLVM.pdb
-
-copy _artifacts\x64\Retail\Out\cc3o3.exe _binaries\cc3o3_x64.exe
-copy _artifacts\Win32\Retail\Out\cc3o3.exe _binaries\cc3o3_x86.exe
-copy _artifacts\LLVM_x64\Retail\Out\cc3o3.exe _binaries\cc3o3_LLVM.exe
-
-copy _artifacts\x64\Retail\Out\cc3o3.pdb _binaries\cc3o3_x64.pdb
-copy _artifacts\Win32\Retail\Out\cc3o3.pdb _binaries\cc3o3_x86.pdb
-copy _artifacts\LLVM_x64\Retail\Out\cc3o3.pdb _binaries\cc3o3_LLVM.pdb
+copy _artifacts\x86_64\MSVC_Retail\Out\retrofit.dll _binaries\retrofit.dll
+copy _artifacts\x86_64\MSVC_Retail\Out\retrofit.pdb _binaries\retrofit.pdb
+copy _artifacts\x86_64\MSVC_Retail\Out\retrofit_exe.exe _binaries\retrofit.exe
+copy _artifacts\x86_64\MSVC_Retail\Out\retrofit_exe.pdb _binaries\retrofit_exe.pdb
+copy _artifacts\x86_64\MSVC_Retail\Out\FramePackEditor.exe _binaries\FramePackEditor.exe
+copy _artifacts\x86_64\MSVC_Retail\Out\FramePackEditor.pdb _binaries\FramePackEditor.pdb
+copy _artifacts\x86_64\MSVC_Retail\Out\cc3o3.dll _binaries\cc3o3.dll
+copy _artifacts\x86_64\MSVC_Retail\Out\cc3o3.pdb _binaries\cc3o3.pdb
+copy _artifacts\x86_64\MSVC_Retail\Out\cc3o3_exe.exe _binaries\cc3o3.exe
+copy _artifacts\x86_64\MSVC_Retail\Out\cc3o3_exe.pdb _binaries\cc3o3_exe.pdb
