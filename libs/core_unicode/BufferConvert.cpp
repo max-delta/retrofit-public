65647ce6abe81fde5c646dd5f0d19daa0a706d46
Max Delta <54338561+max-delta@users.noreply.github.com>
2021-03-08 00:35:30 +0000

    Cleanup unicode buffer conversion

--- libs/core_unicode/BufferConvert.cpp
+++ libs/core_unicode/BufferConvert.cpp
@@ -7,6 +7,7 @@
 
 namespace RF { namespace unicode {
 ///////////////////////////////////////////////////////////////////////////////
+namespace details {
 
 // NOTE: Not currently a fully STL-compliant InsertIterator
 template<typename CharT>
@@ -56,124 +57,116 @@ struct BufferInsertIterator
 	size_t mNextInsert = 0;
 };
 
+
+template<typename DestT, typename SourceT>
+using ConvertToInsertSig = void( SourceT const*, size_t, BufferInsertIterator<DestT>&& );
+
+template<typename DestT, typename CallableT, typename SourceT>
+size_t ConvertTo( CallableT const& callable, DestT* dest, size_t destLen, SourceT const* source, size_t numElements )
+{
+	size_t writeAttempts = 0;
+	BufferInsertIterator<DestT> iter( dest, destLen, &writeAttempts );
+	callable( source, numElements, rftl::move( iter ) );
+	return writeAttempts;
+}
+
+
+
+template<typename SourceT>
+size_t ConvertToASCII( char* dest, size_t destLen, SourceT const* source, size_t numElements )
+{
+	ConvertToInsertSig<char, SourceT>* const call = unicode::ConvertToASCII;
+	return ConvertTo<char>( call, dest, destLen, source, numElements );
+}
+
+template<typename SourceT>
+size_t ConvertToUtf8( char* dest, size_t destLen, SourceT const* source, size_t numElements )
+{
+	ConvertToInsertSig<char, SourceT>* const call = unicode::ConvertToUtf8;
+	return ConvertTo<char>( call, dest, destLen, source, numElements );
+}
+
+template<typename SourceT>
+size_t ConvertToUtf16( char16_t* dest, size_t destLen, SourceT const* source, size_t numElements )
+{
+	ConvertToInsertSig<char16_t, SourceT>* const call = unicode::ConvertToUtf16;
+	return ConvertTo<char16_t>( call, dest, destLen, source, numElements );
+}
+
+template<typename SourceT>
+size_t ConvertToUtf32( char32_t* dest, size_t destLen, SourceT const* source, size_t numElements )
+{
+	ConvertToInsertSig<char32_t, SourceT>* const call = unicode::ConvertToUtf32;
+	return ConvertTo<char32_t>( call, dest, destLen, source, numElements );
+}
+
+}
 ///////////////////////////////////////////////////////////////////////////////
 
 size_t ConvertToASCII( char* dest, size_t destLen, char const* source, size_t numBytes )
 {
-	size_t writeAttempts = 0;
-	BufferInsertIterator iter( dest, destLen, &writeAttempts );
-	ConvertToASCII( source, numBytes, iter );
-	return writeAttempts;
+	return details::ConvertToASCII( dest, destLen, source, numBytes );
 }
-
-
 
 size_t ConvertToASCII( char* dest, size_t destLen, char16_t const* source, size_t numPairs )
 {
-	size_t writeAttempts = 0;
-	BufferInsertIterator iter( dest, destLen, &writeAttempts );
-	ConvertToASCII( source, numPairs, iter );
-	return writeAttempts;
+	return details::ConvertToASCII( dest, destLen, source, numPairs );
 }
-
-
 
 size_t ConvertToASCII( char* dest, size_t destLen, char32_t const* source, size_t numCodePoints )
 {
-	size_t writeAttempts = 0;
-	BufferInsertIterator iter( dest, destLen, &writeAttempts );
-	ConvertToASCII( source, numCodePoints, iter );
-	return writeAttempts;
+	return details::ConvertToASCII( dest, destLen, source, numCodePoints );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
 size_t ConvertToUtf8( char* dest, size_t destLen, char const* source, size_t numBytes )
 {
-	size_t writeAttempts = 0;
-	BufferInsertIterator iter( dest, destLen, &writeAttempts );
-	ConvertToUtf8( source, numBytes, iter );
-	return writeAttempts;
+	return details::ConvertToUtf8( dest, destLen, source, numBytes );
 }
-
-
 
 size_t ConvertToUtf8( char* dest, size_t destLen, char16_t const* source, size_t numPairs )
 {
-	size_t writeAttempts = 0;
-	BufferInsertIterator iter( dest, destLen, &writeAttempts );
-	ConvertToUtf8( source, numPairs, iter );
-	return writeAttempts;
+	return details::ConvertToUtf8( dest, destLen, source, numPairs );
 }
-
-
 
 size_t ConvertToUtf8( char* dest, size_t destLen, char32_t const* source, size_t numCodePoints )
 {
-	size_t writeAttempts = 0;
-	BufferInsertIterator iter( dest, destLen, &writeAttempts );
-	ConvertToUtf8( source, numCodePoints, iter );
-	return writeAttempts;
+	return details::ConvertToUtf8( dest, destLen, source, numCodePoints );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
 size_t ConvertToUtf16( char16_t* dest, size_t destLen, char const* source, size_t numBytes )
 {
-	size_t writeAttempts = 0;
-	BufferInsertIterator iter( dest, destLen, &writeAttempts );
-	ConvertToUtf16( source, numBytes, iter );
-	return writeAttempts;
+	return details::ConvertToUtf16( dest, destLen, source, numBytes );
 }
-
-
 
 size_t ConvertToUtf16( char16_t* dest, size_t destLen, char16_t const* source, size_t numPairs )
 {
-	size_t writeAttempts = 0;
-	BufferInsertIterator iter( dest, destLen, &writeAttempts );
-	ConvertToUtf16( source, numPairs, iter );
-	return writeAttempts;
+	return details::ConvertToUtf16( dest, destLen, source, numPairs );
 }
-
-
 
 size_t ConvertToUtf16( char16_t* dest, size_t destLen, char32_t const* source, size_t numCodePoints )
 {
-	size_t writeAttempts = 0;
-	BufferInsertIterator iter( dest, destLen, &writeAttempts );
-	ConvertToUtf16( source, numCodePoints, iter );
-	return writeAttempts;
+	return details::ConvertToUtf16( dest, destLen, source, numCodePoints );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
 size_t ConvertToUtf32( char32_t* dest, size_t destLen, char const* source, size_t numBytes )
 {
-	size_t writeAttempts = 0;
-	BufferInsertIterator iter( dest, destLen, &writeAttempts );
-	ConvertToUtf32( source, numBytes, iter );
-	return writeAttempts;
+	return details::ConvertToUtf32( dest, destLen, source, numBytes );
 }
-
-
 
 size_t ConvertToUtf32( char32_t* dest, size_t destLen, char16_t const* source, size_t numPairs )
 {
-	size_t writeAttempts = 0;
-	BufferInsertIterator iter( dest, destLen, &writeAttempts );
-	ConvertToUtf32( source, numPairs, iter );
-	return writeAttempts;
+	return details::ConvertToUtf32( dest, destLen, source, numPairs );
 }
-
-
 
 size_t ConvertToUtf32( char32_t* dest, size_t destLen, char32_t const* source, size_t numCodePoints )
 {
-	size_t writeAttempts = 0;
-	BufferInsertIterator iter( dest, destLen, &writeAttempts );
-	ConvertToUtf32( source, numCodePoints, iter );
-	return writeAttempts;
+	return details::ConvertToUtf32( dest, destLen, source, numCodePoints );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
