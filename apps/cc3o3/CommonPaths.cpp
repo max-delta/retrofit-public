4a3a3c245f5579c1a68f05d332a627448ec42e6f
Max Delta <54338561+max-delta@users.noreply.github.com>
2021-04-10 21:14:09 +0000

    Path cleanup

--- apps/cc3o3/CommonPaths.cpp
+++ apps/cc3o3/CommonPaths.cpp
@@ -14,89 +14,122 @@ file::VFSPath VfsConfig()
 
 
 
-file::VFSPath FontsRoot()
-{
-	return file::VFS::kRoot.GetChild( "assets", "fonts" );
-}
-file::VFSPath TilesetsRoot()
-{
-	return file::VFS::kRoot.GetChild( "assets", "tilesets" );
-}
-file::VFSPath TilemapsRoot()
-{
-	return file::VFS::kRoot.GetChild( "assets", "tilemaps" );
-}
-file::VFSPath FramepacksRoot()
-{
-	return file::VFS::kRoot.GetChild( "assets", "framepacks" );
-}
-file::VFSPath TablesRoot()
-{
-	return file::VFS::kRoot.GetChild( "assets", "tables" );
-}
 file::VFSPath UserSavesRoot()
 {
 	return file::VFS::kRoot.GetChild( "saves" );
 }
 
+
+
+file::VFSPath AssetsRoot()
+{
+	return file::VFS::kRoot.GetChild( "assets" );
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+file::VFSPath FontsRoot()
+{
+	return AssetsRoot().GetChild( "fonts" );
+}
+file::VFSPath TilesetsRoot()
+{
+	return AssetsRoot().GetChild( "tilesets" );
+}
+file::VFSPath TilemapsRoot()
+{
+	return AssetsRoot().GetChild( "tilemaps" );
+}
+file::VFSPath FramepacksRoot()
+{
+	return AssetsRoot().GetChild( "framepacks" );
+}
+file::VFSPath TexturesRoot()
+{
+	return AssetsRoot().GetChild( "textures" );
+}
+file::VFSPath TablesRoot()
+{
+	return AssetsRoot().GetChild( "tables" );
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+
 file::VFSPath CommonFonts()
 {
-	return file::VFS::kRoot.GetChild( "assets", "fonts", "common" );
+	return FontsRoot().GetChild( "common" );
 }
+
+///////////////////////////////////////////////////////////////////////////////
+
 file::VFSPath CommonTilesets()
 {
-	return file::VFS::kRoot.GetChild( "assets", "tilesets", "common" );
+	return TilesetsRoot().GetChild( "common" );
 }
-
-
-
 file::VFSPath BackgroundTilesets()
 {
-	return file::VFS::kRoot.GetChild( "assets", "tilesets", "backgrounds" );
-}
-file::VFSPath BackgroundTilemaps()
-{
-	return file::VFS::kRoot.GetChild( "assets", "tilemaps", "backgrounds" );
+	return TilesetsRoot().GetChild( "backgrounds" );
 }
 file::VFSPath UITilesets()
 {
-	return file::VFS::kRoot.GetChild( "assets", "tilesets", "ui" );
+	return TilesetsRoot().GetChild( "ui" );
 }
 file::VFSPath DialogFrameTilesets()
 {
-	return file::VFS::kRoot.GetChild( "assets", "tilesets", "frames" );
+	return TilesetsRoot().GetChild( "frames" );
 }
+
+///////////////////////////////////////////////////////////////////////////////
+
+file::VFSPath BackgroundTilemaps()
+{
+	return TilemapsRoot().GetChild( "backgrounds" );
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
 file::VFSPath LogoFramepacks()
 {
-	return file::VFS::kRoot.GetChild( "assets", "framepacks", "logos" );
+	return FramepacksRoot().GetChild( "logos" );
 }
-file::VFSPath CharacterTables()
-{
-	return file::VFS::kRoot.GetChild( "assets", "tables", "char" );
-}
+
+///////////////////////////////////////////////////////////////////////////////
+
 file::VFSPath CharacterPieces()
 {
-	return file::VFS::kRoot.GetChild( "assets", "textures", "char" );
+	return TexturesRoot().GetChild( "char" );
 }
-file::VFSPath Localizations()
+
+///////////////////////////////////////////////////////////////////////////////
+
+file::VFSPath CharacterTables()
 {
-	return file::VFS::kRoot.GetChild( "assets", "localizations" );
+	return TablesRoot().GetChild( "char" );
 }
 file::VFSPath BaseCharacters()
 {
-	return file::VFS::kRoot.GetChild( "assets", "tables", "world", "chars" );
+	return TablesRoot().GetChild( "world", "chars" );
+}
+file::VFSPath ElementDescTables()
+{
+	return TablesRoot().GetChild( "elements", "descs" );
+}
+file::VFSPath ElementTierUnlockTables()
+{
+	return TablesRoot().GetChild( "elements", "tier_unlocks" );
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+file::VFSPath Localizations()
+{
+	return AssetsRoot().GetChild( "localizations" );
 }
 file::VFSPath CompositeCharacters()
 {
 	return file::VFS::kRoot.GetChild( "scratch", "composite_char" );
-}
-file::VFSPath ElementDescTables()
-{
-	return file::VFS::kRoot.GetChild( "assets", "tables", "elements", "descs" );
-}
-file::VFSPath ElementTierUnlockTables()
-{
-	return file::VFS::kRoot.GetChild( "assets", "tables", "elements", "tier_unlocks" );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
--- apps/cc3o3/CommonPaths.h
+++ apps/cc3o3/CommonPaths.h
@@ -9,28 +9,44 @@ namespace RF::cc::paths {
 
 file::VFSPath VfsConfig();
 
+file::VFSPath UserSavesRoot();
+file::VFSPath AssetsRoot();
+
+// Asset roots
 file::VFSPath FontsRoot();
 file::VFSPath TilesetsRoot();
 file::VFSPath TilemapsRoot();
 file::VFSPath FramepacksRoot();
+file::VFSPath TexturesRoot();
 file::VFSPath TablesRoot();
-file::VFSPath UserSavesRoot();
 
+// Fonts
 file::VFSPath CommonFonts();
-file::VFSPath CommonTilesets();
 
+// Tilesets
+file::VFSPath CommonTilesets();
 file::VFSPath BackgroundTilesets();
-file::VFSPath BackgroundTilemaps();
 file::VFSPath UITilesets();
 file::VFSPath DialogFrameTilesets();
+
+// Tilemaps
+file::VFSPath BackgroundTilemaps();
+
+// Framepacks
 file::VFSPath LogoFramepacks();
-file::VFSPath CharacterTables();
+
+// Textures
 file::VFSPath CharacterPieces();
-file::VFSPath Localizations();
+
+// Tables
+file::VFSPath CharacterTables();
 file::VFSPath BaseCharacters();
-file::VFSPath CompositeCharacters();
 file::VFSPath ElementDescTables();
 file::VFSPath ElementTierUnlockTables();
+
+// Misc
+file::VFSPath Localizations();
+file::VFSPath CompositeCharacters();
 
 ///////////////////////////////////////////////////////////////////////////////
 }
