959c5aabffd559a84087f5b39121ac621caca6bb
Max Delta <54338561+max-delta@users.noreply.github.com>
2021-05-01 19:38:47 +0000

    Added primitive collisions

---
+++ libs/GamePixelPhysics/PrimitiveCollision.cpp
@@ -0,0 +1,31 @@
+#include "stdafx.h"
+#include "PrimitiveCollision.h"
+
+#include "core_math/AABB4.h"
+
+
+namespace RF::phys {
+///////////////////////////////////////////////////////////////////////////////
+
+bool PrimitiveCollision::HasCollision(
+	AABB const& aabb,
+	PhysCoord const& pos )
+{
+	RF_ASSERT( aabb.Left() <= aabb.Right() );
+	RF_ASSERT( aabb.Top() <= aabb.Bottom() );
+
+	if( pos.x < aabb.Left() || pos.x > aabb.Right() )
+	{
+		return false;
+	}
+
+	if( pos.y < aabb.Top() || pos.y > aabb.Bottom() )
+	{
+		return false;
+	}
+
+	return true;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+}
---
+++ libs/GamePixelPhysics/PrimitiveCollision.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "project.h"
+
+#include "GamePixelPhysics/PhysicsFwd.h"
+
+#include "core/macros.h"
+
+
+namespace RF::phys {
+///////////////////////////////////////////////////////////////////////////////
+
+// NOTE: General ordering of parameters is that more complex types are first
+class GAMEPIXELPHYSICS_API PrimitiveCollision
+{
+	RF_NO_INSTANCE( PrimitiveCollision );
+
+	//
+	// Public methods
+public:
+	static bool HasCollision(
+		AABB const& aabb,
+		PhysCoord const& pos );
+};
+
+///////////////////////////////////////////////////////////////////////////////
+}
