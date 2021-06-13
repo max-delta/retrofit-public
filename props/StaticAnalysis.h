0ceb738b8f82f753f80dc9fbdbe8a44c3cc1ae82
Max Delta <54338561+max-delta@users.noreply.github.com>
2021-06-12 19:03:15 -0700

    Static analysis fix

--- props/StaticAnalysis.h
+++ props/StaticAnalysis.h
@@ -14,11 +14,14 @@
 #if _MSC_FULL_VER >= 192829910 && _MSC_FULL_VER <= 192930037
 	#pragma warning( disable : 26457 )
 #endif
+#if _MSC_FULL_VER >= 192930037 && _MSC_FULL_VER <= 192930037
+	#pragma warning( disable : 26467 )
+#endif
 
 // False positives about locks being dropped in non-sensical locations
 // NOTE: Specifically observed with rftl::lock_guard<rftl::mutex> not
 //  understanding where scope ends, and thinking the destructor would happen
 //  in the same line as the constructor
-#if _MSC_FULL_VER >= 192729111 && _MSC_FULL_VER <= 192829914
+#if _MSC_FULL_VER >= 192729111 && _MSC_FULL_VER <= 192930037
 	#pragma warning( disable : 26110 )
 #endif
