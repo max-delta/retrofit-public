0b5bad3201e30e67972fa0310866fd5f0a2580cf
Max Delta <54338561+max-delta@users.noreply.github.com>
2023-08-06 16:02:50 -0700

    Serialize element grid, required accessor->accessor support

--- apps/cc3o3/char/ElementSlots.cpp
+++ apps/cc3o3/char/ElementSlots.cpp
@@ -3,16 +3,13 @@
 
 #include "RFType/CreateClassInfoDefinition.h"
 
-// TODO: Add an extension for this
-//#include "core_rftype/stl_extensions/array.h"
-#include "core_rftype/stl_extensions/string.h"
+#include "core_rftype/stl_extensions/array.h"
 
 
 RFTYPE_CREATE_META( RF::cc::character::ElementSlots )
 {
 	using namespace RF::cc::character;
-	// TODO: Add an extension for this (rftl::array<...>)
-	//RFTYPE_META().ExtensionProperty( "mGrid", &ElementSlots::mGrid );
+	RFTYPE_META().ExtensionProperty( "mGrid", &ElementSlots::mGrid );
 	RFTYPE_REGISTER_BY_NAME( "ElementSlots" );
 	RFTYPE_REGISTER_BY_QUALIFIED_NAME( RF::cc::character::ElementSlots );
 }
