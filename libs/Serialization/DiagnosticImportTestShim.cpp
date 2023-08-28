2ea709f127850e955dc0eb22b5dcf1346a33c671
Max Delta <54338561+max-delta@users.noreply.github.com>
2023-08-27 18:28:49 -0700

    Added helper shim for importer diagnostics

---
+++ libs/Serialization/DiagnosticImportTestShim.cpp
@@ -0,0 +1,106 @@
+#include "stdafx.h"
+#include "DiagnosticImportTestShim.h"
+
+#include "Serialization/Importer.h"
+#include "Serialization/DiagnosticExporter.h"
+
+#include "core_math/math_casts.h"
+#include "core_reflect/Value.h"
+
+RF_TODO_ANNOTATION( "Remove c_str usages, revise API" );
+#include "rftl/extension/c_str.h"
+
+#include "rftl/string"
+
+
+namespace RF::serialization {
+///////////////////////////////////////////////////////////////////////////////
+
+bool DiagnosticProcessImport( Importer& importer )
+{
+	DiagnosticExporter diagExporter{ false };
+
+	using InstanceID = Importer::InstanceID;
+	using TypeID = Importer::TypeID;
+	using IndirectionID = Importer::IndirectionID;
+	using ExternalReferenceID = Importer::ExternalReferenceID;
+
+	Importer::Callbacks callbacks = {};
+
+	callbacks.mRoot_BeginNewInstanceFunc =
+		[&diagExporter]() -> bool
+	{
+		return diagExporter.Root_BeginNewInstance();
+	};
+
+	callbacks.mRoot_RegisterLocalIndirectionFunc =
+		[&diagExporter]( IndirectionID const& indirectionID, InstanceID const& instanceID ) -> bool
+	{
+		return diagExporter.Root_RegisterLocalIndirection( indirectionID, instanceID );
+	};
+
+	callbacks.mRoot_RegisterExternalIndirectionFunc =
+		[&diagExporter]( IndirectionID const& indirectionID, ExternalReferenceID const& referenceID ) -> bool
+	{
+		return diagExporter.Root_RegisterExternalIndirection( indirectionID, referenceID );
+	};
+
+	callbacks.mInstance_AddInstanceIDAttributeFunc =
+		[&diagExporter]( InstanceID const& instanceID ) -> bool
+	{
+		return diagExporter.Instance_AddInstanceIDAttribute( instanceID );
+	};
+
+	callbacks.mInstance_AddTypeIDAttributeFunc =
+		[&diagExporter]( TypeID const& typeID, char const* debugName ) -> bool
+	{
+		return diagExporter.Instance_AddTypeIDAttribute( typeID, debugName );
+	};
+
+	callbacks.mInstance_BeginNewPropertyFunc =
+		[&diagExporter]() -> bool
+	{
+		return diagExporter.Instance_BeginNewProperty();
+	};
+
+	callbacks.mProperty_AddNameAttributeFunc =
+		[&diagExporter]( rftl::string_view const& name ) -> bool
+	{
+		return diagExporter.Property_AddNameAttribute( RFTLE_CSTR( name ) );
+	};
+
+	callbacks.mProperty_AddValueAttributeFunc =
+		[&diagExporter]( reflect::Value const& value ) -> bool
+	{
+		return diagExporter.Property_AddValueAttribute( value );
+	};
+
+	callbacks.mProperty_AddIndirectionAttributeFunc =
+		[&diagExporter]( IndirectionID const& indirectionID ) -> bool
+	{
+		return diagExporter.Property_AddIndirectionAttribute( indirectionID );
+	};
+
+	callbacks.mProperty_IndentFromCurrentPropertyFunc =
+		[&diagExporter]() -> bool
+	{
+		return diagExporter.Property_IndentFromCurrentProperty();
+	};
+
+	callbacks.mProperty_OutdentFromLastIndentFunc =
+		[&diagExporter]() -> bool
+	{
+		return diagExporter.Property_OutdentFromLastIndent();
+	};
+
+	bool const success = importer.ImportAndFinalize( callbacks );
+	if( success == false )
+	{
+		return false;
+	}
+
+	return diagExporter.Root_FinalizeExport();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+}
