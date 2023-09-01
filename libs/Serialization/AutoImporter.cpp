aca217d8b6bb544164e8372aacbb2cde59daee2f
Max Delta <54338561+max-delta@users.noreply.github.com>
2023-09-01 14:49:59 -0700

    Header peek helper for auto importer

--- libs/Serialization/AutoImporter.cpp
+++ libs/Serialization/AutoImporter.cpp
@@ -21,6 +21,23 @@ AutoImporter::~AutoImporter() = default;
 
 
 
+bool AutoImporter::LooksLikeSupportedType( rftl::string_view const& string )
+{
+	if( string.size() < kLongestPeekMagicBytes )
+	{
+		RF_DBGFAIL_MSG( "Not enough bytes to check all possible types" );
+	}
+
+	if( rftl::bagins_with( string, "<?xml" ) )
+	{
+		return true;
+	}
+
+	return false;
+}
+
+
+
 bool AutoImporter::ReadFromString( rftl::string_view const& string )
 {
 	if( mUnderlying != nullptr )
--- libs/Serialization/AutoImporter.h
+++ libs/Serialization/AutoImporter.h
@@ -17,8 +17,16 @@ class SERIALIZATION_API AutoImporter final : public Importer
 	RF_NO_COPY( AutoImporter );
 
 public:
+	// The longest byte sequence needed to peek for all supported types
+	static constexpr size_t kLongestPeekMagicBytes = 5; // '<?xml'
+
+
+public:
 	AutoImporter();
 	~AutoImporter() override;
+
+	// Check initial bytes to see if it looks like it would be supported
+	static bool LooksLikeSupportedType( rftl::string_view const& string );
 
 	bool ReadFromString( rftl::string_view const& string );
 
