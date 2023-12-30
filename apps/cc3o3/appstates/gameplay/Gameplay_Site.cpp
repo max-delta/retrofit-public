cac31dc3cb371ea62dd107ee2aaff9f5e2ab74df
Max Delta <54338561+max-delta@users.noreply.github.com>
2023-12-30 15:29:16 -0800

    Lots of hacky site logic, mostly take from the game menus as a starting point

--- apps/cc3o3/appstates/gameplay/Gameplay_Site.cpp
+++ apps/cc3o3/appstates/gameplay/Gameplay_Site.cpp
@@ -12,9 +12,21 @@
 #include "GameAppState/AppStateTickContext.h"
 #include "GameAppState/AppStateManager.h"
 
-#include "GameInput/GameController.h"
+#include "GameUI/ContainerManager.h"
+#include "GameUI/FocusEvent.h"
+#include "GameUI/FocusManager.h"
+#include "GameUI/UIContext.h"
+#include "GameUI/controllers/BorderFrame.h"
+#include "GameUI/controllers/ColumnSlicer.h"
+#include "GameUI/controllers/Floater.h"
+#include "GameUI/controllers/ListBox.h"
+#include "GameUI/controllers/MultiPassthrough.h"
+#include "GameUI/controllers/Passthrough.h"
+#include "GameUI/controllers/RowSlicer.h"
+#include "GameUI/controllers/TextLabel.h"
 
 #include "PPU/PPUController.h"
+#include "PPU/TilesetManager.h"
 
 #include "core/ptr/default_creator.h"
 
@@ -25,22 +37,247 @@ namespace RF::cc::appstate {
 struct Gameplay_Site::InternalState
 {
 	RF_NO_COPY( InternalState );
+
+public:
+	struct TopLevelSections
+	{
+		enum Section : size_t
+		{
+			kVista = 0,
+			kHACKBattle,
+
+			kNumSections
+		};
+
+		static constexpr char const* kLabels[kNumSections] = {
+			"H_vista",
+			"H_hackbattle",
+		};
+	};
+
+
+public:
 	InternalState() = default;
+
+	void SwitchTopLevelSection( TopLevelSections::Section section );
+	bool IsActiveSection( TopLevelSections::Section section ) const;
+	void ShowSelector( ui::UIContext& context );
+	void HideSelector( ui::UIContext& context, TopLevelSections::Section chosenSection );
+
+
+public:
+	WeakPtr<ui::controller::TextLabel> mMainHeader;
+
+	WeakPtr<ui::controller::InstancedController> mSectionSelectorFloater;
+	WeakPtr<ui::controller::ListBox> mSectionSelector;
+
+	using TopLevelControllers = rftl::array<WeakPtr<ui::controller::InstancedController>, TopLevelSections::kNumSections>;
+	TopLevelControllers mTopLevelControllers;
 };
+
+
+
+void Gameplay_Site::InternalState::SwitchTopLevelSection( TopLevelSections::Section section )
+{
+	// Only show current section
+	for( WeakPtr<ui::controller::InstancedController> const& controller : mTopLevelControllers )
+	{
+		controller->SetChildRenderingBlocked( true );
+	}
+	mTopLevelControllers.at( section )->SetChildRenderingBlocked( false );
+}
+
+
+
+bool Gameplay_Site::InternalState::IsActiveSection( TopLevelSections::Section section ) const
+{
+	return mTopLevelControllers.at( section )->IsChildRenderingBlocked() == false;
+}
+
+
+
+void Gameplay_Site::InternalState::ShowSelector( ui::UIContext& context )
+{
+	mSectionSelectorFloater->SetChildRenderingBlocked( false );
+	context.GetMutableFocusManager().GetMutableFocusTree().SetRootFocusToSpecificChild(
+		mSectionSelectorFloater->GetMutableFocusTreeNode( context ) );
+}
+
+
+
+void Gameplay_Site::InternalState::HideSelector( ui::UIContext& context, TopLevelSections::Section chosenSection )
+{
+	SwitchTopLevelSection( chosenSection );
+
+	mSectionSelectorFloater->SetChildRenderingBlocked( true );
+	context.GetMutableFocusManager().GetMutableFocusTree().SetRootFocusToSpecificChild(
+		mTopLevelControllers.at( chosenSection )->GetMutableFocusTreeNode( context ) );
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 
 void Gameplay_Site::OnEnter( AppStateChangeContext& context )
 {
 	mInternalState = DefaultCreator<InternalState>::Create();
+	InternalState& internalState = *mInternalState;
 
-	campaign::CampaignManager& campaign = *gCampaignManager;
+	gfx::ppu::PPUController const& ppu = *app::gGraphics;
+	gfx::TilesetManager const& tsetMan = *ppu.GetTilesetManager();
 
-	// Load site
-	site::Site const site = campaign.LoadDataForSite();
+	// Setup site
+	{
+		campaign::CampaignManager& campaign = *gCampaignManager;
 
-	// TODO
-	( (void)site );
+		// Load site
+		site::Site const site = campaign.LoadDataForSite();
+
+		// TODO
+		( (void)site );
+	}
+
+	// Setup UI
+	{
+		ui::ContainerManager& uiManager = *app::gUiManager;
+		ui::FocusManager& focusMan = uiManager.GetMutableFocusManager();
+		ui::UIContext uiContext( uiManager );
+		uiManager.RecreateRootContainer();
+
+		// Cut the whole screen into columns
+		ui::controller::ColumnSlicer::Ratios const rootColumnRatios = {
+			{ 1.f / 20.f, false },
+			{ 18.f / 20.f, true },
+			{ 1.f / 20.f, false },
+		};
+		WeakPtr<ui::controller::ColumnSlicer> const rootColumnSlicer =
+			uiManager.AssignStrongController(
+				ui::kRootContainerID,
+				DefaultCreator<ui::controller::ColumnSlicer>::Create(
+					rootColumnRatios ) );
+
+		// Cut the middle in 3
+		ui::controller::RowSlicer::Ratios const middleRowRatios = {
+			{ 1.f / 14.f, true },
+			{ 12.f / 14.f, true },
+			{ 1.f / 14.f, true },
+		};
+		WeakPtr<ui::controller::RowSlicer> const middleRowSlicer =
+			uiManager.AssignStrongController(
+				rootColumnSlicer->GetChildContainerID( 1 ),
+				DefaultCreator<ui::controller::RowSlicer>::Create(
+					middleRowRatios ) );
+
+		// Header in top center
+		WeakPtr<ui::controller::TextLabel> const mainHeader =
+			uiManager.AssignStrongController(
+				middleRowSlicer->GetChildContainerID( 0 ),
+				DefaultCreator<ui::controller::TextLabel>::Create() );
+		mainHeader->SetJustification( ui::Justification::BottomCenter );
+		mainHeader->SetFont( ui::font::LargeMenuHeader );
+		mainHeader->SetText( "UNSET" );
+		mainHeader->SetColor( math::Color3f::kWhite );
+		mainHeader->SetBorder( true );
+		internalState.mMainHeader = mainHeader;
+
+		// Controls in bottom right
+		WeakPtr<ui::controller::TextLabel> const mainFooter =
+			uiManager.AssignStrongController(
+				middleRowSlicer->GetChildContainerID( 2 ),
+				DefaultCreator<ui::controller::TextLabel>::Create() );
+		mainFooter->SetJustification( ui::Justification::TopRight );
+		mainFooter->SetFont( ui::font::SmallMenuSelection );
+		mainFooter->SetText( "UNSET" );
+		mainFooter->SetColor( math::Color3f::kWhite );
+		mainFooter->SetBorder( true );
+
+		// Main sections are in the center
+		using TopLevelSections = InternalState::TopLevelSections;
+		WeakPtr<ui::controller::MultiPassthrough> const sectionPassthroughs =
+			uiManager.AssignStrongController(
+				middleRowSlicer->GetChildContainerID( 1 ),
+				DefaultCreator<ui::controller::MultiPassthrough>::Create( TopLevelSections::kNumSections + 1 ) );
+
+		// Section selector floats above the screen
+		WeakPtr<ui::controller::Floater> const sectionSelectorFloater =
+			uiManager.AssignStrongController(
+				sectionPassthroughs->GetChildContainerID( TopLevelSections::kNumSections ),
+				DefaultCreator<ui::controller::Floater>::Create(
+					math::integer_cast<gfx::ppu::CoordElem>( gfx::ppu::kTileSize * 2 ),
+					math::integer_cast<gfx::ppu::CoordElem>( gfx::ppu::kTileSize ),
+					ui::Justification::MiddleCenter ) );
+		uiManager.AdjustRecommendedRenderDepth( sectionSelectorFloater->GetContainerID(), -20 );
+		sectionSelectorFloater->AddAsChildToFocusTreeNode( uiContext, focusMan.GetMutableFocusTree().GetMutableRootNode() );
+		internalState.mSectionSelectorFloater = sectionSelectorFloater;
+
+		// Sections each get a passthrough that can be used to shut off all
+		//  children when switching sections
+		WeakPtr<ui::controller::Passthrough> const vistaPassthrough =
+			uiManager.AssignStrongController(
+				sectionPassthroughs->GetChildContainerID( TopLevelSections::kVista ),
+				DefaultCreator<ui::controller::Passthrough>::Create() );
+		WeakPtr<ui::controller::Passthrough> const HACKBattlePassthrough =
+			uiManager.AssignStrongController(
+				sectionPassthroughs->GetChildContainerID( TopLevelSections::kHACKBattle ),
+				DefaultCreator<ui::controller::Passthrough>::Create() );
+		internalState.mTopLevelControllers.at( TopLevelSections::kVista ) = vistaPassthrough;
+		internalState.mTopLevelControllers.at( TopLevelSections::kHACKBattle ) = HACKBattlePassthrough;
+		for( WeakPtr<ui::controller::InstancedController> const& controller : internalState.mTopLevelControllers )
+		{
+			controller->AddAsSiblingAfterFocusTreeNode( uiContext, sectionSelectorFloater->GetMutableFocusTreeNode( uiContext ) );
+		}
+
+		// Section selector
+		{
+			// Frame
+			WeakPtr<ui::controller::BorderFrame> const frame =
+				uiManager.AssignStrongController(
+					sectionSelectorFloater->GetChildContainerID(),
+					DefaultCreator<ui::controller::BorderFrame>::Create() );
+			frame->SetTileset( uiContext, tsetMan.GetManagedResourceIDFromResourceName( "retro1_8_48" ), { 8, 8 }, { 48, 48 }, { 0, 0 } );
+
+			// Implement selector as horizontal list
+			// TODO: Image support in list boxes
+			WeakPtr<ui::controller::ListBox> const selector =
+				uiManager.AssignStrongController(
+					frame->GetChildContainerID(),
+					DefaultCreator<ui::controller::ListBox>::Create(
+						ui::Orientation::Horizontal,
+						TopLevelSections::kNumSections,
+						ui::font::NarrowHalfTileMono ) );
+			for( size_t i = 0; i < TopLevelSections::kNumSections; i++ )
+			{
+				WeakPtr<ui::controller::TextLabel> const slotController = selector->GetMutableSlotController( i );
+				{
+					// TODO: Image support
+					rftl::string temp = "0";
+					temp.at( 0 ) += math::integer_cast<char>( i );
+					slotController->SetText( rftl::move( temp ) );
+				}
+				uiManager.AssignLabel( slotController->GetContainerID(), TopLevelSections::kLabels[i] );
+			}
+			selector->AddAsChildToFocusTreeNode( uiContext, *sectionSelectorFloater->GetMutableFocusTreeNode( uiContext ) );
+			internalState.mSectionSelector = selector;
+		}
+
+		// Vista section
+		{
+			// Placeholder
+			WeakPtr<ui::controller::BorderFrame> const frame =
+				uiManager.AssignStrongController(
+					vistaPassthrough->GetChildContainerID(),
+					DefaultCreator<ui::controller::BorderFrame>::Create() );
+			frame->SetTileset( uiContext, tsetMan.GetManagedResourceIDFromResourceName( "wood_8_48" ), { 8, 8 }, { 48, 48 }, { 0, 0 } );
+		}
+
+		// HACK battle section
+		{
+			// Placeholder
+			WeakPtr<ui::controller::BorderFrame> const frame =
+				uiManager.AssignStrongController(
+					HACKBattlePassthrough->GetChildContainerID(),
+					DefaultCreator<ui::controller::BorderFrame>::Create() );
+			frame->SetTileset( uiContext, tsetMan.GetManagedResourceIDFromResourceName( "wood_8_48" ), { 8, 8 }, { 48, 48 }, { 0, 0 } );
+		}
+	}
 }
 
 
@@ -54,26 +291,152 @@ void Gameplay_Site::OnExit( AppStateChangeContext& context )
 
 void Gameplay_Site::OnTick( AppStateTickContext& context )
 {
+	using TopLevelSections = InternalState::TopLevelSections;
+
+	InternalState& internalState = *mInternalState;
+	ui::ContainerManager& uiManager = *app::gUiManager;
+	ui::FocusManager& focusMan = uiManager.GetMutableFocusManager();
 	campaign::CampaignManager& campaign = *gCampaignManager;
 
-	app::gGraphics->DebugDrawText( gfx::ppu::Coord( 32, 32 ), "TODO: Site" );
+	ui::UIContext uiContext( uiManager );
+	focusMan.UpdateHardFocus( uiContext );
 
 	// Process menu actions
-	rftl::vector<input::GameCommand> const menuCommands =
-		InputHelpers::GetGameplayInputToProcess( InputHelpers::GetSinglePlayer(), input::layer::GameMenu );
-	for( input::GameCommand const& menuCommand : menuCommands )
+	rftl::vector<ui::FocusEventType> const focusEvents = InputHelpers::GetGameMenuInputToProcess( InputHelpers::GetSinglePlayer() );
+	for( ui::FocusEventType const& focusEvent : focusEvents )
 	{
-		if( menuCommand.mType == input::command::game::UIActivateSelection )
+		bool const handled = focusMan.HandleEvent( uiContext, focusEvent );
+		focusMan.UpdateHardFocus( uiContext );
+
+		// Figure out the useful focus information
+		ui::ContainerID const currentFocusContainerID = focusMan.GetFocusTree().GetCurrentFocusContainerID();
+
+		if( handled )
 		{
-			// Start encounter
-			campaign.StartEncounter( context, "TODO" );
+			// Handled by the normal UI
+			RF_TODO_ANNOTATION(
+				"Check for certain selection changes and update some ancillary"
+				" displays, similar to how the section selector works in the"
+				" save/loadout menus" );
+
+			// HACK: This is taken from the pause menu logic, but that's not
+			//  how this is intended to work long-term
+			// Normally we would ignore this, but we want to check to see if
+			//  this was the section selector moving, in which case we want to
+			//  change which section is displayed behind it
+			if( internalState.mSectionSelector->SlotHasCurrentFocus( uiContext ) )
+			{
+				bool foundSection = false;
+				for( size_t i = 0; i < TopLevelSections::kNumSections; i++ )
+				{
+					if( currentFocusContainerID == uiManager.GetContainerID( TopLevelSections::kLabels[i] ) )
+					{
+						internalState.SwitchTopLevelSection( static_cast<TopLevelSections::Section>( i ) );
+						foundSection = true;
+						break;
+					}
+				}
+				RF_ASSERT( foundSection );
+			}
 		}
-		else if( menuCommand.mType == input::command::game::UICancelSelection )
+		else
 		{
-			// Exit site
-			campaign.TravelToOverworld( context );
+			// Wasn't handled by general UI
+
+			if( currentFocusContainerID != ui::kInvalidContainerID )
+			{
+				if( internalState.mSectionSelector->SlotHasCurrentFocus( uiContext ) )
+				{
+					// Section selection
+
+					if( focusEvent == ui::focusevent::Command_ActivateCurrentFocus )
+					{
+						bool foundSection = false;
+						for( size_t i = 0; i < TopLevelSections::kNumSections; i++ )
+						{
+							if( currentFocusContainerID == uiManager.GetContainerID( TopLevelSections::kLabels[i] ) )
+							{
+								TopLevelSections::Section const chosenSection = static_cast<TopLevelSections::Section>( i );
+
+								// This should've already been made active
+								RF_ASSERT( internalState.IsActiveSection( chosenSection ) );
+
+								// Hide the selector, moving focus to the
+								//  chosen section instead
+								internalState.HideSelector( uiContext, chosenSection );
+
+								foundSection = true;
+								break;
+							}
+						}
+						RF_ASSERT( foundSection );
+					}
+					else if( focusEvent == ui::focusevent::Command_CancelCurrentFocus )
+					{
+						// HACK: Just return to the overworld
+						// TODO: Only allow this through an explicit 'Exit'
+						//  choice, that might not always be allowed if story
+						//  progression blocks it
+						// Exit site
+						campaign.TravelToOverworld( context );
+					}
+				}
+				else
+				{
+					// Sections
+					TopLevelSections::Section currentSection = TopLevelSections::kNumSections;
+					for( size_t i = 0; i < TopLevelSections::kNumSections; i++ )
+					{
+						if( internalState.mTopLevelControllers.at( i )->IsInCurrentFocusStack( uiContext ) )
+						{
+							currentSection = math::enum_bitcast<TopLevelSections::Section>( i );
+						}
+					}
+					RF_ASSERT( currentSection != TopLevelSections::kNumSections );
+
+					if( currentSection == TopLevelSections::kVista )
+					{
+						// Vista
+
+						if( focusEvent == ui::focusevent::Command_ActivateCurrentFocus )
+						{
+							// TODO
+						}
+						else if( focusEvent == ui::focusevent::Command_CancelCurrentFocus )
+						{
+							// HACK: Always pop back up to section selector
+							// TODO: Proper logic
+							internalState.ShowSelector( uiContext );
+						}
+					}
+					else if( currentSection == TopLevelSections::kHACKBattle )
+					{
+						// HACK battle
+
+						if( focusEvent == ui::focusevent::Command_ActivateCurrentFocus )
+						{
+							// Start encounter
+							campaign.StartEncounter( context, "TODO" );
+						}
+						else if( focusEvent == ui::focusevent::Command_CancelCurrentFocus )
+						{
+							// HACK: Always pop back up to section selector
+							// TODO: Proper logic
+							internalState.ShowSelector( uiContext );
+						}
+					}
+					else
+					{
+						// Uknown/unhandled section
+						RF_DBGFAIL();
+					}
+				}
+			}
 		}
+		focusMan.UpdateHardFocus( uiContext );
 	}
+
+	app::gGraphics->DebugDrawText( gfx::ppu::Coord( 32, 32 ), "TODO: Site" );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
