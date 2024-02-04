523917292411d82caa1444042a983ddb1201244f
Max Delta <54338561+max-delta@users.noreply.github.com>
2024-02-04 14:42:44 -0800

    Added basic logging to bindump

--- apps/bindump/main.cpp
+++ apps/bindump/main.cpp
@@ -1,9 +1,37 @@
 #include "stdafx.h"
 
+#include "Logging/Logging.h"
+#include "Logging/AssertLogger.h"
+
+#include "PlatformUtils_win32/loggers/DebuggerLogger.h"
+
+#include "core_math/math_bits.h"
+
 
 ///////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
+	using namespace RF;
+
+	// Need to set up a minimum logger before logging anything, so that it
+	//  doesn't use the fallback logger and pollute the standard output pipes
+	{
+		logging::HandlerDefinition def;
+		def.mSupportedSeverities = math::GetAllBitsSet<logging::SeverityMask>();
+		def.mUtf8HandlerFunc = platform::debugging::DebuggerLogger;
+		logging::RegisterHandler( def );
+	}
+
+	RFLOG_MILESTONE( nullptr, RFCAT_BINDUMP, "Initializing assert logging..." );
+	{
+		logging::HandlerDefinition def;
+		def.mSupportedSeverities = logging::Severity::RF_SEV_USER_ATTENTION_REQUESTED;
+		def.mUtf8HandlerFunc = logging::AssertLogger;
+		logging::RegisterHandler( def );
+	}
+
+	RFLOG_MILESTONE( nullptr, RFCAT_BINDUMP, "Initializing bin dump..." );
+
 	return 0;
 }
