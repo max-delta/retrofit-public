3b044dae988e0ac6423192de9b2ecf8933dcc00e
Max Delta <54338561+max-delta@users.noreply.github.com>
2024-02-17 14:13:19 -0800

    Update to VS 17.9.0

--- libs/rftl/extension/integer_literals.h
+++ libs/rftl/extension/integer_literals.h
@@ -5,28 +5,28 @@
 namespace rftl::literals {
 ///////////////////////////////////////////////////////////////////////////////
 
-inline constexpr uint8_t operator"" _u8( unsigned long long arg )
+inline constexpr uint8_t operator""_u8( unsigned long long arg )
 {
 	return static_cast<uint8_t>( arg );
 }
 
 
 
-inline constexpr uint16_t operator"" _u16( unsigned long long arg )
+inline constexpr uint16_t operator""_u16( unsigned long long arg )
 {
 	return static_cast<uint16_t>( arg );
 }
 
 
 
-inline constexpr int8_t operator"" _s8( unsigned long long arg )
+inline constexpr int8_t operator""_s8( unsigned long long arg )
 {
 	return static_cast<int8_t>( arg );
 }
 
 
 
-inline constexpr int16_t operator"" _s16( unsigned long long arg )
+inline constexpr int16_t operator""_s16( unsigned long long arg )
 {
 	return static_cast<int16_t>( arg );
 }
