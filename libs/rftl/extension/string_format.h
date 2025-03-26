f6a234482415fca90d9f76f9919d66237a608344
Max Delta <54338561+max-delta@users.noreply.github.com>
2025-03-25 22:03:19 -0700

    Retail build fix

--- libs/rftl/extension/string_format.h
+++ libs/rftl/extension/string_format.h
@@ -7,32 +7,35 @@
 namespace rftl {
 ///////////////////////////////////////////////////////////////////////////////
 
-char format_nibble( uint8_t nibble )
+template<typename CharT = char>
+CharT format_nibble( uint8_t nibble )
 {
 	if( nibble <= 0x9 )
 	{
-		return static_cast<char>( '0' + nibble );
+		return static_cast<CharT>( '0' + nibble );
 	}
 	if( nibble <= 0xf )
 	{
-		return static_cast<char>( 'A' + ( nibble - 0xa ) );
+		return static_cast<CharT>( 'A' + ( nibble - 0xa ) );
 	}
 	return '#';
 }
 
 
 
-void format_byte( char& upper, char& lower, uint8_t byte )
+template<typename CharT>
+void format_byte( CharT& upper, CharT& lower, uint8_t byte )
 {
-	upper = format_nibble( ( byte & 0xf0u ) >> 4 );
-	lower = format_nibble( byte & 0x0fu );
+	upper = format_nibble<CharT>( ( byte & 0xf0u ) >> 4 );
+	lower = format_nibble<CharT>( byte & 0x0fu );
 }
 
 
 
 // NOTE: Max bytes specifies how many bytes to read from the view, not the size
 //  of the outputted string, which will be atleast twice the size of the data
-string to_string( byte_view const& view, size_t maxBytes )
+template<typename CharT = char>
+basic_string<CharT> to_string( byte_view const& view, size_t maxBytes )
 {
 	size_t numBytes = view.size();
 	bool truncated = false;
@@ -44,7 +47,7 @@ string to_string( byte_view const& view, size_t maxBytes )
 
 	size_t const numChars = numBytes * 2 + ( truncated ? 3 : 0 );
 
-	string retVal;
+	basic_string<CharT> retVal;
 	retVal.reserve( numChars );
 
 	for( size_t i = 0; i < numBytes; i++ )
