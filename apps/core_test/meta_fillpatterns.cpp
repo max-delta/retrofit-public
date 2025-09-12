ab40ab0ae65a5bfe36688a1f822b5dfa82d656e9
Max Delta <54338561+max-delta@users.noreply.github.com>
2025-09-11 18:22:32 -0700

    Replaced fill patterns with even better ones, and more of them

--- apps/core_test/meta_fillpatterns.cpp
+++ apps/core_test/meta_fillpatterns.cpp
@@ -11,25 +11,43 @@ namespace RF::meta::fill {
 
 TEST( FillPatterns, Bad4Bytes )
 {
-	uint32_t const memory[2] = { kBad4Bytes, kBad4Bytes };
-	uint32_t const& asInt = memory[0];
-	float const& asFloat = *reinterpret_cast<float const*>( &asInt );
-	float const& asMisalignedFloat = *reinterpret_cast<float const*>( reinterpret_cast<uint16_t const*>( &asInt ) + 1 );
-	char const( &asChars )[4] = *reinterpret_cast<char const( * )[4]>( &asInt );
+	uint32_t const patterns[] = {
+		kBadPattern4_0,
+		kBadPattern4_A,
+		kBadPattern4_B,
+		kBadPattern4_8,
+		kBadPattern4_9 };
 
-	// Can't be interpreted as null
-	ASSERT_NE( asInt, static_cast<uintptr_t>( 0 ) );
-
-	// Can't be interpreted as a good float
-	ASSERT_TRUE( math::SoftFloat<float>::IsNaN( asFloat ) );
-	ASSERT_TRUE( math::SoftFloat<float>::IsSignalingNaN( asFloat ) );
-	ASSERT_TRUE( math::SoftFloat<float>::IsNaN( asMisalignedFloat ) );
-	ASSERT_TRUE( math::SoftFloat<float>::IsSignalingNaN( asMisalignedFloat ) );
-
-	// Can't be interpreted as ASCII
-	for( char const& ch : asChars )
+	for( uint32_t const& pattern : patterns )
 	{
-		ASSERT_FALSE( unicode::IsValidAscii( ch ) );
+		// Stack up the pattern twice in a row, to allow for misalignment tests
+		struct Memory
+		{
+			uint32_t m1;
+			uint32_t m2;
+		};
+		Memory const memory = { pattern, pattern };
+
+		// Can't be interpreted as null
+		uint32_t const& asInt = memory.m1;
+		ASSERT_NE( asInt, static_cast<uintptr_t>( 0 ) );
+
+		// Can't be interpreted as a good float
+		float const& asFloat = *reinterpret_cast<float const*>( &asInt );
+		ASSERT_TRUE( math::SoftFloat<float>::IsNaN( asFloat ) );
+		ASSERT_TRUE( math::SoftFloat<float>::IsSignalingNaN( asFloat ) );
+
+		// ... even if misaligned
+		float const& asMisalignedFloat = *reinterpret_cast<float const*>( reinterpret_cast<uint16_t const*>( &asInt ) + 1 );
+		ASSERT_TRUE( math::SoftFloat<float>::IsNaN( asMisalignedFloat ) );
+		ASSERT_TRUE( math::SoftFloat<float>::IsSignalingNaN( asMisalignedFloat ) );
+
+		// Can't be interpreted as ASCII
+		char const( &asChars )[4] = *reinterpret_cast<char const( * )[4]>( &asInt );
+		for( char const& ch : asChars )
+		{
+			ASSERT_FALSE( unicode::IsValidAscii( ch ) );
+		}
 	}
 }
 
--- libs/core/meta/FillPatterns.h
+++ libs/core/meta/FillPatterns.h
@@ -6,6 +6,7 @@
 namespace RF::meta::fill {
 ///////////////////////////////////////////////////////////////////////////////
 namespace details {
+
 // To be a NaN, the exponent must be all ones (B), the sign bit can be
 //  anything (A), the highest fraction bit should be zero to make it a
 //  signalling NaN (C), and the fraction must be non-zero (D)
@@ -19,33 +20,58 @@ static constexpr uint32_t kNonASCIIMask4Bytes = 0b10000000'10000000'10000000'100
 // In Windows, pointers with the top bit set are reserved for kernel addresses
 static constexpr uint32_t kBadWin32PointerMask4Bytes = 0b10000000'00000000'00000000'00000000;
 
-// Try to make it stand out more to human readers
-static constexpr uint32_t kHumanFudgeMask4Bytes = 0x00'bbbbbb;
-
 // An ideal bad fill pattern will cause issues in several different ways of
-//  interpreting it, but also be quickly recognizable to a human in hexadecimal
-static constexpr uint32_t kAlignedBad4Bytes =
+//  interpreting it
+static constexpr uint32_t kBasePatternBad4Bytes =
 	kNanPartialExponentMask4Bytes |
 	kBadWin32PointerMask4Bytes |
-	kNonASCIIMask4Bytes |
-	kHumanFudgeMask4Bytes;
-static_assert( kAlignedBad4Bytes == 0b1'11111111'0'1110111011101110111011 );
-static_assert( kAlignedBad4Bytes == 0xffbbbbbb );
+	kNonASCIIMask4Bytes;
+static_assert( kBasePatternBad4Bytes == 0b1'11111111'0'0000001000000010000000 );
+static_assert( kBasePatternBad4Bytes == 0xff808080 );
 
 // A good 4-byte pattern is even better if it is still bad when offset slightly
-static constexpr uint32_t kUnalignedBad4Bytes = kAlignedBad4Bytes >> 16 | kAlignedBad4Bytes;
-static_assert( kUnalignedBad4Bytes == 0xffbbffbb );
+static constexpr uint32_t kUnalignedBaseBad4Bytes = kBasePatternBad4Bytes >> 16 | kBasePatternBad4Bytes;
+static_assert( kUnalignedBaseBad4Bytes == 0xff80ff80 );
+
+// Try to make it stand out more to human readers, and provide variants
+static constexpr uint32_t kHumanFudgeMask4Bytes_0 = 0x00'00'00'00;
+static constexpr uint32_t kHumanFudgeMask4Bytes_A = 0x00'aa'00'aa;
+static constexpr uint32_t kHumanFudgeMask4Bytes_B = 0x00'bb'00'bb;
+static constexpr uint32_t kHumanFudgeMask4Bytes_8 = 0x00'88'00'88;
+static constexpr uint32_t kHumanFudgeMask4Bytes_9 = 0x00'99'00'99;
+
+// The final patterns
+static constexpr uint32_t kBadPattern4_0 = kUnalignedBaseBad4Bytes | kHumanFudgeMask4Bytes_0;
+static constexpr uint32_t kBadPattern4_A = kUnalignedBaseBad4Bytes | kHumanFudgeMask4Bytes_A;
+static constexpr uint32_t kBadPattern4_B = kUnalignedBaseBad4Bytes | kHumanFudgeMask4Bytes_B;
+static constexpr uint32_t kBadPattern4_8 = kUnalignedBaseBad4Bytes | kHumanFudgeMask4Bytes_8;
+static constexpr uint32_t kBadPattern4_9 = kUnalignedBaseBad4Bytes | kHumanFudgeMask4Bytes_9;
+static_assert( kBadPattern4_0 == 0xff80ff80 );
+static_assert( kBadPattern4_A == 0xffAAffAA );
+static_assert( kBadPattern4_B == 0xffBBffBB );
+static_assert( kBadPattern4_8 == 0xff88ff88 );
+static_assert( kBadPattern4_9 == 0xff99ff99 );
+
 }
 ///////////////////////////////////////////////////////////////////////////////
 
-static constexpr uint32_t kBad4Bytes = details::kUnalignedBad4Bytes;
-static_assert( kBad4Bytes == 0xffbb'ffbb );
+static constexpr uint16_t kBadPattern2_0 = details::kBadPattern4_0 >> 16;
+static constexpr uint16_t kBadPattern2_A = details::kBadPattern4_A >> 16;
+static constexpr uint16_t kBadPattern2_B = details::kBadPattern4_B >> 16;
+static constexpr uint16_t kBadPattern2_8 = details::kBadPattern4_8 >> 16;
+static constexpr uint16_t kBadPattern2_9 = details::kBadPattern4_9 >> 16;
 
-static constexpr uint64_t kBad8Bytes = static_cast<uint64_t>( kBad4Bytes ) << 32 | kBad4Bytes;
-static_assert( kBad8Bytes == 0xffbb'ffbb'ffbb'ffbb );
+static constexpr uint32_t kBadPattern4_0 = details::kBadPattern4_0;
+static constexpr uint32_t kBadPattern4_A = details::kBadPattern4_A;
+static constexpr uint32_t kBadPattern4_B = details::kBadPattern4_B;
+static constexpr uint32_t kBadPattern4_8 = details::kBadPattern4_8;
+static constexpr uint32_t kBadPattern4_9 = details::kBadPattern4_9;
 
-static constexpr uint16_t kBad2Bytes = kBad4Bytes >> 16;
-static_assert( kBad2Bytes == 0xffbb );
+static constexpr uint64_t kBadPattern8_0 = static_cast<uint64_t>( details::kBadPattern4_0 ) << 32 | details::kBadPattern4_0;
+static constexpr uint64_t kBadPattern8_A = static_cast<uint64_t>( details::kBadPattern4_A ) << 32 | details::kBadPattern4_A;
+static constexpr uint64_t kBadPattern8_B = static_cast<uint64_t>( details::kBadPattern4_B ) << 32 | details::kBadPattern4_B;
+static constexpr uint64_t kBadPattern8_8 = static_cast<uint64_t>( details::kBadPattern4_8 ) << 32 | details::kBadPattern4_8;
+static constexpr uint64_t kBadPattern8_9 = static_cast<uint64_t>( details::kBadPattern4_9 ) << 32 | details::kBadPattern4_9;
 
 ///////////////////////////////////////////////////////////////////////////////
 }
