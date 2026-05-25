428d347b3374cd08f696826cc17d37d77b42bb66
Max Delta <54338561+max-delta@users.noreply.github.com>
2026-05-25 14:00:12 -0700

    Fix for terminal exception warning

--- libs/rftl/extension/fatal_exception.h
+++ libs/rftl/extension/fatal_exception.h
@@ -8,7 +8,7 @@ namespace rftl {
 // At time of writing, LLVM doesn't support throws, which is absolutely
 //  infuriating, so we're just aborting here
 template<typename ExceptionT>
-void fatal_exception( ExceptionT const& exception )
+[[noreturn]] void fatal_exception( ExceptionT const& exception )
 {
 	( (void)exception );
 	rftl::abort();
