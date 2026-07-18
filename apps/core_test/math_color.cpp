a0827d3afb635c7b221e5dc3c5b111bbff811f40
Max Delta <54338561+max-delta@users.noreply.github.com>
2026-07-18 13:21:03 -0700

    Added some color conversion tests

---
+++ apps/core_test/math_color.cpp
@@ -0,0 +1,49 @@
+#include "stdafx.h"
+
+#include "core_math/Color3f.h"
+#include "core_math/Color3u8.h"
+#include "core_math/Color4u8.h"
+
+#include "rftl/extension/array_convert.h"
+
+
+namespace RF::math {
+///////////////////////////////////////////////////////////////////////////////
+
+TEST( Color, BasicConversions )
+{
+#define RF_ARR( TYPE ) { TYPE::kRed, TYPE::kGreen, TYPE::kBlue }
+	rftl::array const kArr3f = RF_ARR( Color3f );
+	rftl::array const kArr3u8 = RF_ARR( Color3u8 );
+	rftl::array const kArr4u8 = RF_ARR( Color4u8 );
+#undef RF_ARR
+
+	static constexpr auto convert =
+		[] //
+		<typename ToT, typename FromT, size_t CountT> //
+		( rftl::array<FromT, CountT> const& arr ) -> //
+		rftl::array<ToT, CountT>
+	{
+		rftl::array<ToT, CountT> retVal = arr;
+		return retVal;
+	};
+
+	{
+		ASSERT_EQ( kArr3u8, rftl::array_convert<Color3u8>( kArr3u8 ) );
+		ASSERT_EQ( kArr3u8, rftl::array_convert<Color3u8>( kArr3f ) );
+		ASSERT_EQ( kArr3u8, rftl::array_convert<Color3u8>( kArr4u8 ) );
+	}
+	{
+		ASSERT_EQ( kArr3f, rftl::array_convert<Color3f>( kArr3u8 ) );
+		ASSERT_EQ( kArr3f, rftl::array_convert<Color3f>( kArr3f ) );
+		ASSERT_EQ( kArr3f, rftl::array_convert<Color3f>( kArr4u8 ) );
+	}
+	{
+		ASSERT_EQ( kArr4u8, rftl::array_convert<Color4u8>( kArr3u8 ) );
+		ASSERT_EQ( kArr4u8, rftl::array_convert<Color4u8>( kArr3f ) );
+		ASSERT_EQ( kArr4u8, rftl::array_convert<Color4u8>( kArr4u8 ) );
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+}
